a64bad734300747821da0d6e07632f14bf8bb4de
Dmitrii <dmitrii.konevnin@gmail.com>
2025-12-20 19:09:05 +0200

    add view for current page

--- app/components/links.tsx
+++ app/components/links.tsx
@@ -1,14 +1,34 @@
 'use client'
 
-import { font } from "../globalConsts/globalStyles";
+import { font, rounded, THEME_COLOR_SCHEME } from "../globalConsts/globalStyles";
 import { useGlobalStore } from "../store/globalStore"
 import { LINKS } from "../template/text"
+import { usePathname } from 'next/navigation';
 import Link from "next/link";
 
 export default function ListLinks() {
     const currentLanguage = useGlobalStore((state) => state.currentLanguage)
+    const currentTheme = useGlobalStore((state) => state.currentTheme)
+    const pathname = usePathname();
 
     return (
-        <div className={`flex gap-4 items-center justify-center`}>{LINKS.map((link) => <Link className={`${font.text.size.medium} w-20`} key={link.label} href={link.path}>{link.translate[currentLanguage]}</Link>)}</div>
-    )
+        <div className="flex gap-4 items-center justify-center">
+            {LINKS.map((link) => {
+                const isActive = pathname === link.path;
+
+                return (
+                    <Link
+                        key={link.label}
+                        href={link.path}
+                        className={`
+                            ${font.text.size.medium} text-center ${rounded.medium} p-2 hover:underline hover:underline-offset-4
+                            ${isActive ? `${THEME_COLOR_SCHEME[currentTheme].activeElement} font-bold ` : 'text-gray-500'}
+                        `}
+                    >
+                        {link.translate[currentLanguage]}
+                    </Link>
+                );
+            })}
+        </div>
+    );
 }
