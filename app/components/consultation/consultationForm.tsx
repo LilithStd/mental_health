945b4807cd796403077ca3165ccc83714d238e7e
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-01-17 21:38:01 +0200

    updated styles in consultation page

--- app/components/consultation/consultationForm.tsx
+++ app/components/consultation/consultationForm.tsx
@@ -4,12 +4,18 @@ export default function ConsultationForm() {
     return (
         <div>
             <h2>Consultation Form</h2>
-            <form>
-                <input placeholder="name" type="text" className={`border-2`} />
-                <input placeholder="email" type="text" className={`border-2`} />
-                <input placeholder="phone" type="text" className={`border-2`} />
-                <input placeholder="date" type="text" className={`border-2`} />
-                <input placeholder="time" type="text" className={`border-2`} />
+            <form className={`flex gap-4 border-2 p-4 rounded-md`}>
+                <div className={`flex flex-col gap-2 w-1/2`}>
+                    <input placeholder="name" type="text" className={`border-2 w-full p-2`} />
+                    <input placeholder="email" type="text" className={`border-2 w-full p-2`} />
+                    <input placeholder="phone" type="text" className={`border-2 w-full p-2`} />
+                    <input placeholder="date" type="text" className={`border-2 w-full p-2`} />
+                </div>
+                <div className={`flex flex-col gap-2 w-1/2`}>
+                    <textarea placeholder="message" className={`border-2 w-full p-2`} />
+                    <button className={`border-2 rounded-md p-2`}>Submit</button>
+                </div>
+
             </form>
         </div>
     )
