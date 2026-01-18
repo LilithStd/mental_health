12e044ca9969a0171dd135b82acc66ac55d24831
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-01-18 19:30:03 +0200

    update styles for consultation page

--- app/components/consultation/consultationForm.tsx
+++ app/components/consultation/consultationForm.tsx
@@ -1,6 +1,13 @@
+'use client'
 
+import { rounded, THEME_COLOR_SCHEME } from "@/app/globalConsts/globalStyles";
+import { useGlobalStore } from "@/app/store/globalStore"
 
 export default function ConsultationForm() {
+    // stores
+    const currentTheme = useGlobalStore((state) => state.currentTheme);
+    const currentLanguage = useGlobalStore((state) => state.currentLanguage);
+    // 
     return (
         <div className={`flex flex-col items-center justify-center w-full mt-8 mb-8`}>
             <form className={`flex flex-col items-center justify-center gap-4 border-2 p-4 rounded-md `}>
@@ -8,17 +15,17 @@ export default function ConsultationForm() {
                 <div className={`flex gap-4 w-full`}>
 
                     <div className={`flex flex-col gap-2 w-1/2`}>
-                        <input placeholder="name" type="text" className={`border-2 w-full p-2`} />
-                        <input placeholder="email" type="text" className={`border-2 w-full p-2`} />
-                        <input placeholder="phone" type="text" className={`border-2 w-full p-2`} />
-                        <input placeholder="date" type="text" className={`border-2 w-full p-2`} />
+                        <input placeholder="name" type="text" className={`border-2 w-full p-2 ${THEME_COLOR_SCHEME[currentTheme].input} ${rounded.medium}`} />
+                        <input placeholder="email" type="text" className={`border-2 w-full p-2 ${THEME_COLOR_SCHEME[currentTheme].input} ${rounded.medium}`} />
+                        <input placeholder="phone" type="text" className={`border-2 w-full p-2 ${THEME_COLOR_SCHEME[currentTheme].input} ${rounded.medium}`} />
+                        <input placeholder="date" type="text" className={`border-2 w-full p-2 ${THEME_COLOR_SCHEME[currentTheme].input} ${rounded.medium}`} />
                     </div>
                     <div className={`flex flex-col gap-2 w-1/2`}>
-                        <textarea placeholder="message" className={`border-2 w-full p-2`} />
+                        <textarea placeholder="message" className={`border-2 w-full p-2 ${THEME_COLOR_SCHEME[currentTheme].input} ${rounded.medium}`} />
                     </div>
                 </div>
 
-                <button className={`border-2 w-1/2 rounded-md p-2`}>Submit</button>
+                <button className={`border-2 w-1/2 rounded-md p-2 ${THEME_COLOR_SCHEME[currentTheme].buttonContainer} ${rounded.medium}`}>Submit</button>
             </form>
         </div>
     )
