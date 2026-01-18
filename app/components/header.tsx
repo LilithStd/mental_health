eaa0ed7a49a37dda5ee43cb611cb81d7abcd288b
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-01-18 21:02:16 +0200

    fix max width at header component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -10,20 +10,23 @@ export default function Header() {
     const currentTheme = useGlobalStore((state) => state.currentTheme)
 
     return (
-        <nav className={`flex ${rounded.medium}  justify-center items-center ${indents.container.main} ${THEME_COLOR_SCHEME[currentTheme].container}`}>
-            <div className={`flex`}>
-                <h1 className={`${titleBaseStyles} text-center`}>Mental Health</h1>
+        <nav className={`flex ${rounded.medium}  justify-center items-center ${indents.container.main} ${THEME_COLOR_SCHEME[currentTheme].container} items-center text-center`}>
+            <div className={`flex w-full justify-between items-center max-w-6xl`}>
+                <div className={`flex`}>
+                    <h1 className={`${titleBaseStyles} text-center`}>Mental Health</h1>
 
+                </div>
+                <div className={`flex justify-center lg:w-1/2  xl:w-1/2 md:w-2/5 sm:w-3/5 w-2/5`}>
+                    <ListLinks />
+                </div>
+                <div className={`flex justify-end w-1/4 md:w-1/5 lg:w-1/7`}>
+                    <AdditionalPanel />
+                </div>
+                <div className={`flex justify-end w-1/4 md:w-1/5 lg:w-1/7`}>
+                    <Authorization />
+                </div>
             </div>
-            <div className={`flex justify-center lg:w-1/2  xl:w-1/2 md:w-2/5 sm:w-3/5 w-2/5`}>
-                <ListLinks />
-            </div>
-            <div className={`flex justify-end w-1/4 md:w-1/5 lg:w-1/7`}>
-                <AdditionalPanel />
-            </div>
-            <div className={`flex justify-end w-1/4 md:w-1/5 lg:w-1/7`}>
-                <Authorization />
-            </div>
+
         </nav>
     )
 }
