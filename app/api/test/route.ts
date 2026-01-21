ad0f333e344ecf28cc7cc4b117c628e015909765
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-01-21 23:10:42 +0200

    delete test api route

--- app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET(
-  _req: Request,
-  { params }: { params: { id: string } }
-) {
-  console.log('🔥 TEST API', params.id)
-  return NextResponse.json({ ok: true })
-}
