60461e37edc1fc1852d4d391acebc3354814cece
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-01-28 02:01:21 +0200

    added methods to auth store (now template)

--- app/store/authorizationStore.ts
+++ app/store/authorizationStore.ts
@@ -1,31 +1,54 @@
-// store/auth.store.ts
-import { create } from 'zustand';
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import { ROLE_AUTHORIZED_USER, ROLE_AUTH_USER_PRIVILEGE, UPDATE_USER_DATA_TYPE, USER_FAVORITES_ACTION } from "../globalConsts/globalEnum";
 
-type User = {
-  id: string;
-  email: string;
-};
+export type User = {
+  id: string
+  email: string
+  password: string
+  role: ROLE_AUTHORIZED_USER
+  privilege: ROLE_AUTH_USER_PRIVILEGE
+  favorites: USER_FAVORITES
+}
 
-type AuthState = {
-  user: User | null;
-  isAuth: boolean;
-  setUser: (user: User | null) => void;
-  logout: () => void;
-};
+type USER_FAVORITES = {
+  ARTICLES:string[],
+  TESTS:string[],
+  NEWS:string[],
 
-export const useAuthStore = create<AuthState>((set) => ({
-  user: null,
-  isAuth: false,
+}
 
-  setUser: (user) =>
-    set({
-      user,
-      isAuth: !!user,
+type UserUpdateData = {
+  id: string,
+  typeUpdate: UPDATE_USER_DATA_TYPE
+  dataUpdate:string,
+  favoriteAction?: USER_FAVORITES_ACTION
+
+}
+
+interface useAuthorizationStoreInterface {
+  authUsers: User[]
+  currentAuthUser: User | null
+  setCurrentAuthUser: (userData: User) => void
+  logoutUser: (id: string) => void
+}
+
+export const useAuthorizationStore = create<useAuthorizationStoreInterface>()(
+  persist(
+    (set, get) => ({
+      authUsers: [],
+      currentAuthUser: null,
+      setCurrentAuthUser: (userData: User) => {
+        set(() => ({ currentAuthUser: userData }))
+      },
+      logoutUser: (id: string) => {
+        const updatedAuthUsers = get().authUsers.filter(user => user.id !== id)
+        set(() => ({ authUsers: updatedAuthUsers, currentAuthUser: null }))
+    },
     }),
-
-  logout: () =>
-    set({
-      user: null,
-      isAuth: false,
-    }),
-}));
+    {
+      name: 'authorization-storage', // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+    },
+  ),
+)
