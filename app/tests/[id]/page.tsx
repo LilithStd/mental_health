07524c7c10370d1cc14854e4cd59b6bb8daaadd7
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-01-31 19:51:28 +0200

    updated logic to redirect for current selected test (by id)

--- app/tests/[id]/page.tsx
+++ app/tests/[id]/page.tsx
@@ -1,10 +1,12 @@
 'use client'
+import Test from "@/app/components/tests/test";
+import { TEST_TYPE } from "@/app/globalConsts/globalEnum";
 import { THEME_COLOR_SCHEME, rounded, indents } from "@/app/globalConsts/globalStyles"
 import { useGlobalStore } from "@/app/store/globalStore";
 import { use } from "react"
 
 
-export default function Test({
+export default function TestCurrent({
     params,
 }: {
     params: Promise<{ id: string }>
@@ -12,6 +14,11 @@ export default function Test({
     const currentTheme = useGlobalStore((state) => state.currentTheme);
     const { id } = use(params)
     return (
-        <div className={`flex flex-col ${THEME_COLOR_SCHEME[currentTheme].container} ${rounded.medium} flex-1 ${indents.container.main} items-center text-center`}>test page {id}</div>
+        <div className={`flex flex-col ${THEME_COLOR_SCHEME[currentTheme].container} ${rounded.medium} flex-1 ${indents.container.main} items-center text-center`}>
+            test page {id}
+            <div>
+                <Test id={id} type="1212" name="Test Name" group="sdsd" testType={TEST_TYPE.FULL} />
+            </div>
+        </div>
     )
 }
