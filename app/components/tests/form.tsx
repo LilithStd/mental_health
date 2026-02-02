5fd597f2513c26e7e7f00d2f581be921c880439f
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-02 22:12:47 +0200

    updated styles in test component

--- app/components/tests/form.tsx
+++ app/components/tests/form.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { rounded, THEME_COLOR_SCHEME } from "@/app/globalConsts/globalStyles"
+import { useGlobalStore } from "@/app/store/globalStore";
 import { TestType } from "@/app/tests/page"
 
 interface FormProps {
@@ -7,22 +9,26 @@ interface FormProps {
 }
 
 export default function Form({ test }: FormProps) {
+    // stores
+    const currentTheme = useGlobalStore((state) => state.currentTheme);
+    // components
     async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault()
     }
     return (
 
-        <form onSubmit={handleSubmit} className={`flex flex-col w-full gap-4 `}>
+        <form onSubmit={handleSubmit} className={`flex flex-col justify-center items-center w-full gap-4 `}>
             <h1 className="text-lg font-bold">Выберите вариант:</h1>
             {test.questions.map((question) => (
-                <div key={question.title} className="flex w-full gap-2">
+                <div key={question.title} className="flex justify-center items-center flex-col gap-2">
                     <h2>{question.title}:</h2>
-                    <div className={``}>
+                    <div className={`flex items-center justify-center ${THEME_COLOR_SCHEME[currentTheme].subContainer} ${rounded.medium} p-2 gap-2`}>
                         {question.variants.map((variant) => (
                             <label
                                 key={variant.id}
-                                className={`flex`}
+                                className={`flex flex-col ${THEME_COLOR_SCHEME[currentTheme].elementAccent} p-2 ${rounded.medium} items-center cursor-pointer`}
                             >
+                                <span>{variant.title}</span>
                                 <input
                                     type="radio"
                                     name={question.title}
@@ -30,7 +36,7 @@ export default function Form({ test }: FormProps) {
                                     required
                                     className={``}
                                 />
-                                <span>{variant.title}</span>
+
                             </label>
 
                         ))}
@@ -40,7 +46,7 @@ export default function Form({ test }: FormProps) {
             ))}
             <button
                 type="submit"
-                className="bg-black text-white px-4 py-2 rounded"
+                className={`${THEME_COLOR_SCHEME[currentTheme].buttonContainer} px-4 py-2 mt-4 rounded-md cursor-pointer`}
             >
                 Отправить
             </button>
