27c3489bdfc022e2649a808694c1d5ad147d540e
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-02 21:31:44 +0200

    updated styles in test

--- app/components/tests/form.tsx
+++ app/components/tests/form.tsx
@@ -12,17 +12,30 @@ export default function Form({ test }: FormProps) {
     }
     return (
 
-        <form onSubmit={handleSubmit} className={`flex flex-col gap-4 `}>
+        <form onSubmit={handleSubmit} className={`flex flex-col w-full gap-4 `}>
             <h1 className="text-lg font-bold">Выберите вариант:</h1>
             {test.questions.map((question) => (
-                <div key={question.title} className="flex flex-col gap-2">
-                    <h2>{question.title}</h2>
-                    {question.variants.map((variant) => (
-                        <label key={variant.id}>
-                            <input type="radio" name={question.title} value={variant.count} required />
-                            {variant.title}
-                        </label>
-                    ))}
+                <div key={question.title} className="flex w-full gap-2">
+                    <h2>{question.title}:</h2>
+                    <div className={``}>
+                        {question.variants.map((variant) => (
+                            <label
+                                key={variant.id}
+                                className={`flex`}
+                            >
+                                <input
+                                    type="radio"
+                                    name={question.title}
+                                    value={variant.count}
+                                    required
+                                    className={``}
+                                />
+                                <span>{variant.title}</span>
+                            </label>
+
+                        ))}
+                    </div>
+
                 </div>
             ))}
             <button
