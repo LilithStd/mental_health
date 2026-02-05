06ab8249954fc93f1540a62ac67a038c1169ae34
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-05 21:49:35 +0200

    added method to check privilileges for current auth user

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -6,12 +6,18 @@ import { THEME_COLOR_SCHEME, rounded, indents } from "../globalConsts/globalStyl
 import { useGlobalStore } from "../store/globalStore";
 import News, { NewsType } from "../components/news/news";
 import { NEWS_TYPE } from "../globalConsts/globalEnum";
+import { canEditContent } from "../serverActions/permissions";
+import { useAuthorizationStore } from "../store/authorizationStore";
+import CreateNews from "../components/news/createNews";
 
 
 export default function AllNews() {
     // stores
     const currentTheme = useGlobalStore((state) => state.currentTheme);
+    const currentAuthUser = useAuthorizationStore((state) => state.currentAuthUser);
     // state
+    const [userPrivilege, setUserPrivilege] = useState(false);
+    const [isCreateArticleVisible, setIsCreateArticleVisible] = useState(false);
     const [news, setNews] = useState<NewsType[]>([])
     const [loading, setLoading] = useState(true)
     // functions
@@ -24,13 +30,27 @@ export default function AllNews() {
                 setLoading(false)
             })
     }, [])
-
+    useEffect(() => {
+        const checkPrivilege = async () => {
+            const privilege = await canEditContent(currentAuthUser);
+            setUserPrivilege(privilege);
+        };
+        checkPrivilege();
+    }, [currentAuthUser]);
     return (
         <div className={`flex flex-col ${THEME_COLOR_SCHEME[currentTheme].container} ${rounded.medium} flex-1 ${indents.container.main}  items-center text-center`}>
             {/* <Search /> */}
             {loading ? (<div>Loading...</div>
             ) : (
                 <div>
+                    <div>
+                        {userPrivilege && (
+                            <button className={`p-2 mb-4 ${THEME_COLOR_SCHEME[currentTheme].buttonContainer} ${rounded.medium}`} onClick={() => setIsCreateArticleVisible(true)}>
+                                Create News
+                            </button>
+                        )}
+                        {isCreateArticleVisible && <CreateNews />}
+                    </div>
                     {news.map((newsItem) => (
                         <News key={newsItem.id} news={newsItem} typeNews={NEWS_TYPE.PREVIEW} />
                     ))}
