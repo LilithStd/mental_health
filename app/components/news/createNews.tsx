35fad2139520590d00047d852ec83cd802e2cd07
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-06 16:43:54 +0200

    updated create news component (style, consts and etc.)

--- app/components/news/createNews.tsx
+++ app/components/news/createNews.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { rounded, THEME_COLOR_SCHEME } from "@/app/globalConsts/globalStyles";
+import { rounded, sizes, THEME_COLOR_SCHEME } from "@/app/globalConsts/globalStyles";
 import { useGlobalStore } from "@/app/store/globalStore";
 
 export default function CreateNews() {
@@ -11,15 +11,23 @@ export default function CreateNews() {
     // handlers
     //
     // components 
+    const previewNewsComponent = <div>
+
+    </div>
+
     return (
-        <div className={`flex flex-col mb-4 p-2 w-full  ${rounded.medium} ${THEME_COLOR_SCHEME[currentTheme].subContainer}`}>
+        <div className={`flex flex-col mb-4 p-2 w-full  ${rounded.medium} ${THEME_COLOR_SCHEME[currentTheme].subContainer} ${sizes.width.maxWidth} gap-4`}>
             <h2>Create News</h2>
             <form action="">
                 <input type="text" placeholder="Title" className="w-full mb-2 p-2 rounded-md border border-gray-300" />
-                <textarea placeholder="Content" className="w-full mb-2 p-2 rounded-md border border-gray-300"></textarea>
+                <textarea placeholder="Content" className="w-full min-h-40 mb-2 p-2 rounded-md border border-gray-300"></textarea>
                 <input type="text" placeholder="Link" className="w-full mb-2 p-2 rounded-md border border-gray-300" />
             </form>
-            <button className={`p-2 ${THEME_COLOR_SCHEME[currentTheme].buttonContainer} ${rounded.medium}`}>Create</button>
+            <div className={`flex w-full gap-4 justify-center`}>
+                <button className={`p-2 ${THEME_COLOR_SCHEME[currentTheme].buttonContainer} ${rounded.medium} w-1/5`}>Cancel</button>
+                <button className={`p-2 ${THEME_COLOR_SCHEME[currentTheme].buttonContainer} ${rounded.medium} w-1/5`}>Create</button>
+            </div>
+
         </div>
     )
 }
