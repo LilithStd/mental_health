efb405f31233ef83ef2052a7a25cfbb02a3f526b
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-06 16:26:10 +0200

    created createNewsPage

--- app/news/create/page.tsx
+++ app/news/create/page.tsx
@@ -1,13 +1,16 @@
 'use client'
-import { useGlobalStore } from "@/app/store/globalStore";
-import CreateArticle from "@/app/components/articles/createArticle";
-import { THEME_COLOR_SCHEME, rounded, indents } from "@/app/globalConsts/globalStyles";
 
-export default function CreateArticlePage() {
+import { THEME_COLOR_SCHEME } from "@/app/globalConsts/globalStyles";
+import { useGlobalStore } from "@/app/store/globalStore";
+import { indents, rounded } from "@/app/globalConsts/globalStyles";
+import CreateNews from "@/app/components/news/createNews";
+
+export default function CreateNewsPage() {
     const currentTheme = useGlobalStore((state) => state.currentTheme);
     return (
         <div className={`flex flex-col  ${THEME_COLOR_SCHEME[currentTheme].container} ${rounded.medium} flex-1 ${indents.container.main} gap-2 items-center `}>
-            <CreateArticle />
+            <CreateNews />
+
         </div>
     )
 }
