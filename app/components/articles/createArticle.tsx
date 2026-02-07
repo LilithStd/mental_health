7c7655d89d60e2aa263c531374712dd52014f4c3
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-07 14:15:02 +0200

    added redirect to preview page after push cancel create artcle button

--- app/components/articles/createArticle.tsx
+++ app/components/articles/createArticle.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { THEME_COLOR_SCHEME, rounded } from "@/app/globalConsts/globalStyles";
+import { THEME_COLOR_SCHEME, rounded, sizes } from "@/app/globalConsts/globalStyles";
+import { routes } from "@/app/helpers/helpersFunctions";
 import { useGlobalStore } from "@/app/store/globalStore";
+import { useRouter } from "next/navigation";
 
 // interface CreateArticleProps {
 
@@ -9,7 +11,12 @@ import { useGlobalStore } from "@/app/store/globalStore";
 export default function CreateArticle() {
     //stores
     const currentTheme = useGlobalStore((state) => state.currentTheme);
-    // 
+    // consts
+    const router = useRouter()
+    // handles
+    const handleCancel = () => {
+        router.push(routes.articles.root)
+    }
     async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault()
 
@@ -31,7 +38,7 @@ export default function CreateArticle() {
     return (
         <form
             onSubmit={handleSubmit}
-            className="flex flex-col gap-4 max-w-md"
+            className={`flex flex-col gap-4 p-4 w-full ${sizes.width.maxWidth} ${rounded.medium} ${THEME_COLOR_SCHEME[currentTheme].subContainer} ${THEME_COLOR_SCHEME[currentTheme].text}`}
         >
             <h1 className="text-xl font-bold">Create Article</h1>
 
@@ -54,7 +61,7 @@ export default function CreateArticle() {
                 className={`border p-2 h-32 ${THEME_COLOR_SCHEME[currentTheme].input} ${rounded.medium}`}
             />
             <div className={`flex w-full gap-4`}>
-                <button className={`${THEME_COLOR_SCHEME[currentTheme].buttonContainer} py-2 ${rounded.medium} flex-1`}>Cancel</button>
+                <button type="button" onClick={handleCancel} className={`${THEME_COLOR_SCHEME[currentTheme].buttonContainer} py-2 ${rounded.medium} flex-1`}>Cancel</button>
                 <button
                     type="submit"
                     className={`${THEME_COLOR_SCHEME[currentTheme].buttonContainer} py-2 ${rounded.medium} flex-1`}
