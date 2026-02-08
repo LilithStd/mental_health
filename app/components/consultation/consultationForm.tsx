69a7bbeea0b87945c9885fa74e38f8602f310ee7
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-08 20:35:47 +0200

    updated styles (consultation end etc)

--- app/components/consultation/consultationForm.tsx
+++ app/components/consultation/consultationForm.tsx
@@ -22,31 +22,31 @@ export default function ConsultationForm() {
 
     return (
         <div className={`flex flex-col p-4 gap-4 w-full ${rounded.medium}`}>
-            <form ref={ref} action={action} className={`w-full max-w-6xl mx-auto  border-2 p-4 items-center rounded-md bg-subContainer ${sizes.width.maxWidth} w-full `}>
+            <form ref={ref} action={action} className={`mx-auto  border-2 p-4 items-center rounded-md bg-subContainer max-content-main-container`}>
                 <label className={`text-center mb-2`}>Contact Information</label>
                 <div className={`flex gap-4 flex-col w-full items-center  h-full`}>
                     <div className={`flex items-center gap-2 w-1/2`}>
                         <div className={`flex flex-col  w-1/2`}>
-                            <label htmlFor="name" className={`border-2 p-2`}>Your Name:</label>
-                            <label htmlFor="subject" className={`border-2 p-2`}>Subject:</label>
-                            <label htmlFor="email" className={`border-2 p-2`}>Email:</label>
-                            <label htmlFor="phone" className={`border-2 p-2`}>Phone:</label>
-                            <label htmlFor="date" className={`border-2 p-2`}>Date:</label>
+                            <label htmlFor="name" className={`border-2 p-2 rounded-medium`}>Your Name:</label>
+                            <label htmlFor="subject" className={`border-2 p-2 rounded-medium`}>Subject:</label>
+                            <label htmlFor="email" className={`border-2 p-2 rounded-medium`}>Email:</label>
+                            <label htmlFor="phone" className={`border-2 p-2 rounded-medium`}>Phone:</label>
+                            <label htmlFor="date" className={`border-2 p-2 rounded-medium`}>Date:</label>
                         </div>
                         <div className={`flex flex-col  w-1/2`}>
-                            <input id="name" name="name" placeholder="name" type="text" className={`border-2 p-2 ${THEME_COLOR_SCHEME[currentTheme].input} ${rounded.medium}`} />
-                            <input id="subject" name="subject" placeholder="subject" type="text" className={`border-2 p-2 ${THEME_COLOR_SCHEME[currentTheme].input} ${rounded.medium}`} />
-                            <input id="email" name="email" placeholder="email" type="text" className={`border-2 p-2 ${THEME_COLOR_SCHEME[currentTheme].input} ${rounded.medium}`} />
-                            <input id="phone" name="phone" placeholder="phone" type="text" className={`border-2  p-2 ${THEME_COLOR_SCHEME[currentTheme].input} ${rounded.medium}`} />
-                            <input id="date" name="date" placeholder="date" type="text" className={`border-2  p-2 ${THEME_COLOR_SCHEME[currentTheme].input} ${rounded.medium}`} />
+                            <input id="name" name="name" placeholder="name" type="text" className={`border-2 p-2 ${THEME_COLOR_SCHEME[currentTheme].input} rounded-medium`} />
+                            <input id="subject" name="subject" placeholder="subject" type="text" className={`border-2 p-2 ${THEME_COLOR_SCHEME[currentTheme].input} rounded-medium`} />
+                            <input id="email" name="email" placeholder="email" type="text" className={`border-2 p-2 ${THEME_COLOR_SCHEME[currentTheme].input} rounded-medium`} />
+                            <input id="phone" name="phone" placeholder="phone" type="text" className={`border-2  p-2 ${THEME_COLOR_SCHEME[currentTheme].input} rounded-medium`} />
+                            <input id="date" name="date" placeholder="date" type="text" className={`border-2  p-2 ${THEME_COLOR_SCHEME[currentTheme].input} rounded-medium`} />
                         </div>
                     </div>
                     <div className={`flex flex-col gap-2 w-1/2`}>
-                        <textarea id="message" name="message" placeholder="message" className={`border-2 min-h-40 w-full h-full p-2 ${THEME_COLOR_SCHEME[currentTheme].input} ${rounded.medium}`} />
+                        <textarea id="message" name="message" placeholder="message" className={`border-2 min-h-40 w-full h-full p-2 ${THEME_COLOR_SCHEME[currentTheme].input} rounded-medium`} />
                     </div>
                 </div>
                 <div className={`flex justify-center w-full`}>
-                    <button type="submit" className={`border-2  items-center mb-2 mt-2  w-1/2 rounded-md p-2 ${THEME_COLOR_SCHEME[currentTheme].buttonContainer} ${rounded.medium}`}>Submit</button>
+                    <button type="submit" className={`border-2  items-center mb-2 mt-2  w-1/2 rounded-md p-2 ${THEME_COLOR_SCHEME[currentTheme].buttonContainer} rounded-medium`}>Submit</button>
                 </div>
 
             </form>
--- app/consultation/page.tsx
+++ app/consultation/page.tsx
@@ -1,15 +1,15 @@
-'use client'
+
 
 import ConsultationForm from "../components/consultation/consultationForm";
-import { THEME_COLOR_SCHEME, rounded, indents } from "../globalConsts/globalStyles";
-import { useGlobalStore } from "../store/globalStore";
+
+
 
 export default function Consultation() {
     // stores
-    const currentTheme = useGlobalStore((state) => state.currentTheme);
+
 
     return (
-        <div className={`flex flex-col bg-mainContainer ${rounded.medium} flex-1 ${indents.container.main}`}>
+        <div className={`flex flex-col bg-mainContainer indents-main-container rounded-medium flex-1 `}>
             <h2>Consultation</h2>
             <ConsultationForm />
         </div>
