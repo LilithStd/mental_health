3d0ebe8a46107a5944ef5ed008d249aaa64222b8
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-10 19:52:33 +0200

    added language icons

--- app/components/blockRandomElements/randomNewsBlock.tsx
+++ app/components/blockRandomElements/randomNewsBlock.tsx
@@ -1,8 +1,5 @@
 'use client';
-import { ArticleType } from "@/app/articles/page";
-import { THEME_COLOR_SCHEME, rounded, font, sizes } from "@/app/globalConsts/globalStyles";
 import { pickRandomUnique } from "@/app/helpers/helpersFunctions";
-import { useGlobalStore } from "@/app/store/globalStore";
 import { useEffect, useState } from "react";
 import News, { NewsType } from "../news/news";
 import { NEWS_TYPE } from "@/app/globalConsts/globalEnum";
@@ -14,7 +11,6 @@ export default function RandomNewsBlock() {
     const [news, setNews] = useState<NewsType[]>([])
     const [loading, setLoading] = useState(true)
     //stores
-    const currentTheme = useGlobalStore((state) => state.currentTheme);
 
 
     useEffect(() => {
@@ -30,7 +26,7 @@ export default function RandomNewsBlock() {
     return (
         <div className={`flex flex-col bg-subContainer indents-main-container rounded-medium max-content-main-container gap-4`}>
             <div>
-                <h2 className={`${font.title.size.medium} indents-main-container ${font.title.weigth.bold} bg-accentElement rounded-large`}>Random News</h2>
+                <h2 className={`text-3xl indents-main-container font-bold bg-accentElement rounded-large`}>Random News</h2>
             </div>
             <div className={`flex flex-col gap-4 m-4`}>
                 {loading ? (
