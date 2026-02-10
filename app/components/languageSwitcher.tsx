d5fc3eaa2489a4864ee0fa65f022fde15fb55247
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-10 21:58:04 +0200

    updated language switcher component (need some work on logic to render list language )

--- app/components/languageSwitcher.tsx
+++ app/components/languageSwitcher.tsx
@@ -1,20 +1,60 @@
 'use client'
 import { LANGUAGE_APP } from "../globalConsts/globalConsts"
-import { indents, rounded, THEME_COLOR_SCHEME } from "../globalConsts/globalStyles"
 import { useGlobalStore } from "../store/globalStore"
 import ButtonComponent from "./shared/button"
+import LanguageIcon from "../../public/icons/Language.svg"
+import { useState } from "react"
+
 
 export default function LanguageSwitcher() {
     const currentLanguage = useGlobalStore((state) => state.currentLanguage)
     const setCurrentLanguage = useGlobalStore((state) => state.setCurrentLanguage)
-    return (
-        <div className={`flex rounded-medium items-center  bg-mainContainer`}>
-            {LANGUAGE_APP.map((item) =>
-                <div key={item} className={`${currentLanguage === item ? 'bg-activeElement' : ''} rounded-medium gap-2 p-2`}>
-                    <ButtonComponent callBack={() => setCurrentLanguage(item)} content={item} />
+    // state
+    const [isOpen, setIsOpen] = useState(false)
+    // 
+    // components
+    const listLanguage = (
+        <div
+            className="
+      absolute
+      top-12
+      left-1/2 -translate-x-1/2
+      flex gap-2
+      bg-accentElement
+      rounded-medium
+      p-2
+      z-10
+    "
+        >
+            {LANGUAGE_APP.map((item) => (
+                <div
+                    key={item}
+                    className={`${currentLanguage === item ? 'bg-activeElement' : ''} rounded-medium p-2`}
+                >
+                    <button
+                        className={`text-text ${currentLanguage === item ? 'font-bold' : ''}`}
+                        onClick={() => {
+                            setCurrentLanguage(item);
+                            setIsOpen(false);
+                        }}
+                    >
+                        {item}
+                    </button>
                 </div>
-
-            )}
+            ))}
         </div>
+    );
+
+
+
+    return (
+        <div className="flex rounded-medium items-center bg-mainContainer relative">
+            <LanguageIcon
+                className="w-6 h-6 mr-2 cursor-pointer"
+                onClick={() => setIsOpen(!isOpen)}
+            />
+            {isOpen && listLanguage}
+        </div>
+
     )
 }
