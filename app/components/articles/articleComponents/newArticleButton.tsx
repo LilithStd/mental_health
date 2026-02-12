484b5752f827c745529eeba499de595c27b38d20
Dmitrii <dmitrii.konevnin@gmail.com>
2026-02-12 23:33:46 +0200

    updated articles component

--- app/components/articles/articleComponents/newArticleButton.tsx
+++ app/components/articles/articleComponents/newArticleButton.tsx
@@ -3,23 +3,26 @@ import { routes } from '@/app/helpers/helpersFunctions'
 import { canEditContent } from '@/app/serverActions/permissions';
 import { useAuthorizationStore } from '@/app/store/authorizationStore';
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-interface NewArticleButtonProps {
-    callBackCheckUserPrivilege: (privilege: boolean) => void
-}
-
-export default function NewArticleButton({ callBackCheckUserPrivilege }: NewArticleButtonProps) {
+export default function NewArticleButton() {
     const route = useRouter()
     const currentAuthUser = useAuthorizationStore((state) => state.currentAuthUser);
+    const [isAllowedToEdit, setIsAllowedToEdit] = useState(false);
     useEffect(() => {
         const checkPrivilege = async () => {
             const privilege = await canEditContent(currentAuthUser);
-            callBackCheckUserPrivilege(privilege);
+            setIsAllowedToEdit(privilege);
         };
         checkPrivilege();
     }, [currentAuthUser]);
+    console.log(isAllowedToEdit)
+    if (!isAllowedToEdit) {
+        return null; // or return a message indicating lack of permissions
+    }
     return (
-        <button className={` bg-buttonContainer p-2 rounded-medium`} onClick={() => route.push(routes.articles.create())}>New Articles</button>
+
+        <button className={` bg-buttonContainer  p-2 rounded-medium`} onClick={() => route.push(routes.articles.create())}>New Articles</button>
+
     )
 }
--- app/components/articles/articlesClients.tsx
+++ app/components/articles/articlesClients.tsx
@@ -1,16 +1,7 @@
-'use client';
 
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { useGlobalStore } from "@/app/store/globalStore";
-import { useAuthorizationStore } from "@/app/store/authorizationStore";
-import { canEditContent } from "@/app/serverActions/permissions";
-import Search from "../shared/search";
-import { routes } from "@/app/helpers/helpersFunctions";
-import Loading from "../shared/loading";
-import Article from "./article";
 import { ARTICLE_TYPE } from "@/app/globalConsts/globalEnum";
 import ArticleClient from "./articleClient";
+import NewArticleButton from "./articleComponents/newArticleButton";
 
 
 
@@ -28,35 +19,20 @@ interface ArticlesClientProps {
 }
 
 export default function ArticlesClient({ initialArticles }: ArticlesClientProps) {
-    //stores
-    const currentLanguage = useGlobalStore((state) => state.currentLanguage);
-    const currentAuthUser = useAuthorizationStore((state) => state.currentAuthUser);
-    // state
-    const [userPrivilege, setUserPrivilege] = useState(false);
-    const [isCreateArticleVisible, setIsCreateArticleVisible] = useState(false);
-    // functions
-    // const CloseFormHandler = () => {
-    //     setIsCreateArticleVisible(false);
-    // }
-    //functions
-    // const 
-
     const articles = initialArticles
-    useEffect(() => {
-        const checkPrivilege = async () => {
-            const privilege = await canEditContent(currentAuthUser);
-            setUserPrivilege(privilege);
-        };
-        checkPrivilege();
-    }, [currentAuthUser]);
 
     return (
         <div className={`flex flex-col  bg-mainContainer rounded-medium flex-1 indents-main-container  gap-2 items-center `}>
+            <div className={`flex flex-col gap-4 `}>
+                <NewArticleButton />
+            </div>
             <div className={`grid grid-cols-1 md:grid-cols-2 gap-4  mb-4 max-content-main-container`}>
-                {!isCreateArticleVisible && articles.map((article) =>
-                    // <ArticleServerLikesWrapper key={article.id} article={article} type={ARTICLE_TYPE.MEDIUM} />
+
+                {articles.map((article) =>
                     <ArticleClient key={article.id} article={article} type={ARTICLE_TYPE.MEDIUM} />
                 )}
+
+
             </div>
 
         </div>
