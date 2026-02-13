9bbc792b4e31c5cfe6a864643ad91e0bb10e8463
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-13 23:06:46 +0200

    added logout method current auth user

--- app/components/authorization/authUserComponent.tsx
+++ app/components/authorization/authUserComponent.tsx
@@ -2,16 +2,20 @@ import AuthorisationIcon from "@/public/icons/user/UserLogout.svg"
 import Link from "next/link"
 import { APP_PATH_ROUTER } from "@/app/globalConsts/globalEnum"
 import { UserAuthType } from "@/app/types/types"
+import { logoutAction } from "@/app/serverActions/auth/auth"
 
 interface AuthUserComponentProps {
     authUser: UserAuthType,
     callBackModal: () => void
 }
 
-export default function AuthUserComponent({ authUser, callBackModal }: AuthUserComponentProps) {
+export default function AuthUserComponent({ authUser }: AuthUserComponentProps) {
+    const logoutCurrentAuthUserHandler = async () => {
+        await logoutAction()
+    }
     return (
         <div className={`flex flex-col items-center justify-center gap-2`} >
-            <AuthorisationIcon width={48} height={48} onClick={callBackModal} />
+            <AuthorisationIcon width={48} height={48} onClick={logoutCurrentAuthUserHandler} />
             <div className={`cursor-pointer`}>
                 <Link href={`${APP_PATH_ROUTER.USERS}/${authUser.id}`} className={`hover:bg-hover cursor-pointer`} >{authUser.email}</Link>
             </div>
