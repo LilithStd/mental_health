b137d7519c1d3ec323c14b2d5f2c2cb0945f4294
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-13 23:24:30 +0200

    create redirec to main page if do logout current auth user on auth user page

--- app/components/currentUser.tsx
+++ app/components/currentUser.tsx
@@ -1,28 +1,23 @@
 import { getCurrentUser } from "../serverActions/auth/auth";
+import { redirect } from "next/navigation";
 
 interface UserProps {
     id: string
 }
+
 export default async function CurrentUser({ id }: UserProps) {
-    // stores
-    const currentAuthUser = await getCurrentUser()
-    // 
+    const currentAuthUser = await getCurrentUser();
 
-    // components
-    const userTestResults = <div>
-        <h2>User Test Results:</h2>
-    </div>
-    // functions
-
+    // если нет пользователя — редирект
+    if (!currentAuthUser) {
+        redirect("/");
+    }
 
     return (
         <div>
-            {currentAuthUser && currentAuthUser.id ? (
-                <div>
-                    <p>Current Auth User Email: {currentAuthUser ? currentAuthUser.email : 'No user authenticated'}</p>
-                </div>
-            ) : <p>No user authenticated with this ID.</p>}
-
+            <p>
+                Current Auth User Email: {currentAuthUser.email}
+            </p>
         </div>
     )
 }
