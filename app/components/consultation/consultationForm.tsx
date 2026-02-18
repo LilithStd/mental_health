54ad4982a6a02f1d7a96dadc8af64630cfa83ee6
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-18 22:18:59 +0200

    updated form in consultation page

--- app/components/consultation/consultationForm.tsx
+++ app/components/consultation/consultationForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useGlobalStore } from "@/app/store/globalStore"
+
 import { saveConsultation } from '@/app/serverActions/consultationStorage';
 import { useRef } from "react";
 
@@ -21,29 +21,38 @@ export default function ConsultationForm() {
         <div className={`flex flex-col w-full  max-w-6xl  rounded-large bg-mainContainer `}>
             <form ref={ref} action={action} className={`mx-auto  p-4 items-center rounded-large bg-subContainer max-content-main-container`}>
                 <label className={`flex justify-center text-center mb-2`}>Contact Information</label>
-                <div className={`flex gap-4 flex-col w-full items-center  h-full`}>
+                <div className={`flex gap-4 flex-col w-full items-center h-full`}>
                     <div className={`flex items-center gap-2 w-1/2`}>
-                        <div className={`flex flex-col  w-1/2`}>
-                            <label htmlFor="name" className={`border-2 p-2 rounded-large`}>Your Name:</label>
-                            <label htmlFor="subject" className={`border-2 p-2 rounded-large`}>Subject:</label>
-                            <label htmlFor="email" className={`border-2 p-2 rounded-large`}>Email:</label>
-                            <label htmlFor="phone" className={`border-2 p-2 rounded-large`}>Phone:</label>
-                            <label htmlFor="date" className={`border-2 p-2 rounded-large`}>Date:</label>
-                        </div>
-                        <div className={`flex flex-col  w-1/2`}>
-                            <input id="name" name="name" placeholder="name" type="text" className={`border-2 p-2 bg-input rounded-medium`} />
-                            <input id="subject" name="subject" placeholder="subject" type="text" className={`border-2 p-2 bg-input rounded-medium`} />
-                            <input id="email" name="email" placeholder="email" type="text" className={`border-2 p-2 bg-input rounded-medium`} />
-                            <input id="phone" name="phone" placeholder="phone" type="text" className={`border-2  p-2 bg-input rounded-medium`} />
-                            <input id="date" name="date" placeholder="date" type="text" className={`border-2  p-2 bg-input rounded-medium`} />
+                        <div className={`flex flex-col gap-4 w-full`}>
+                            <label className={` p-2 rounded-large flex flex-col`}>
+                                <span>Name:</span>
+                                <input name="name" placeholder="John Doe" type="text" className={`p-2 bg-input rounded-large`} />
+                            </label>
+                            <label className={` p-2 rounded-large flex flex-col`}>
+                                <span>Subject:</span>
+                                <input name="subject" placeholder="Subject" type="text" className={` p-2 bg-input rounded-large`} />
+                            </label>
+                            <label className={` p-2 rounded-large flex flex-col`}>
+                                <span>Email:</span>
+                                <input name="email" placeholder="email@example.com" type="text" className={` p-2 bg-input rounded-large`} />
+                            </label>
+                            <label className={` p-2 rounded-large flex flex-col`}>
+                                <span>Phone:</span>
+                                <input name="phone" placeholder="(123) 456-7890" type="text" className={` p-2 bg-input rounded-large`} />
+                            </label>
+                            <label className={` p-2 rounded-large flex flex-col`}>
+                                <span>Date:</span>
+                                <input name="date" placeholder="MM/DD/YYYY" type="text" className={` p-2 bg-input rounded-large`} />
+                            </label>
                         </div>
                     </div>
-                    <div className={`flex flex-col gap-2 w-1/2`}>
-                        <textarea id="message" name="message" placeholder="message" className={`border-2 min-h-40 w-full h-full p-2 bg-input rounded-medium`} />
-                    </div>
+                    <label className={`flex flex-col gap-2 w-1/2`}>
+                        <span>Message*:</span>
+                        <textarea id="message" name="message" placeholder="message" className={` min-h-40 w-full h-full p-2 bg-input rounded-large`} />
+                    </label>
                 </div>
-                <div className={`flex justify-center w-full`}>
-                    <button type="submit" className={`border-2  items-center mb-2 mt-2  w-1/2 rounded-medium p-2 bg-buttonContainer`}>Submit</button>
+                <div className={`flex justify-center m-4`}>
+                    <button type="submit" className={` items-center mb-2 mt-2  w-1/4  rounded-large p-4 bg-buttonContainer`}>Submit</button>
                 </div>
 
             </form>
