34e9f1e870281fbc0ca39f28428f9950dca7f4f2
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-20 22:53:47 +0200

    fix button to create news

--- app/components/news/newsComponents/createNewsButtonComponent.tsx
+++ app/components/news/newsComponents/createNewsButtonComponent.tsx
@@ -12,6 +12,7 @@ export default function CreateNewsButtonComponent() {
     // functions
     // handlers
     if (!currentAuthUser) return null
+
     const handleReturnToNews = () => {
         router.push(routes.news.root)
     }
@@ -51,19 +52,6 @@ export default function CreateNewsButtonComponent() {
     </div>
 
     return (
-        <div className={`flex flex-col mb-4 p-2 rounded-medium bg-subContainer max-content-main-container gap-4`}>
-            <h2>Create News</h2>
-            <form action="" onSubmit={createNews} className={`flex flex-col gap-4 w-full`}>
-                <input type="text" name="title" placeholder="Title" className="w-full mb-2 p-2 rounded-md border border-gray-300" />
-                <textarea name="content" placeholder="Content" className="w-full min-h-40 mb-2 p-2 rounded-md border border-gray-300"></textarea>
-                <input type="text" name="link" placeholder="Link" className="w-full mb-2 p-2 rounded-md border border-gray-300" />
-                <div className={`flex w-full gap-4 justify-center`}>
-                    <button type="button" onClick={handleReturnToNews} className={`p-2 bg-buttonContainer rounded-medium w-1/5`}>Cancel</button>
-                    <button type="submit" className={`p-2 bg-buttonContainer rounded-medium w-1/5`}>Create</button>
-                </div>
-            </form>
-
-
-        </div>
+        <button className={` bg-buttonContainer mb-4 w-fit  p-2 rounded-circle`} onClick={() => router.push(routes.news.create())}>Create News</button>
     )
 }
