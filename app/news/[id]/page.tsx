8cfd1ae1c62b6906e8afbc5494c7f1c66dfa47b6
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-24 21:32:27 +0200

    updated news[id] component for new method to get news by id

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -1,44 +1,28 @@
-'use client';
 
 
-import News, { NewsType } from "@/app/components/news/news";
+import News from "@/app/components/news/news";
 import ReturnButton from "@/app/components/returnButton";
 import { NEWS_TYPE } from "@/app/globalConsts/globalEnum";
 import { routes } from "@/app/helpers/helpersFunctions";
-import { use, useEffect, useState } from "react";
+import { getNewsById } from "@/app/serverActions/newsStorage";
 
 
 
-export default function CurrentNews({
+
+export default async function CurrentNews({
     params,
 }: {
     params: Promise<{ id: string }>
 }) {
-    const { id } = use(params)
+    const { id } = await params
     // stores
     // state
-    const [currentNews, setCurrentNews] = useState<NewsType | null>(null)
-    const [error, setError] = useState(false)
-    //
-    useEffect(() => {
-        fetch(`/api/news?id=${id}`)
-            .then(r => {
-                if (!r.ok) {
-                    setError(true)
-                    return null
-                }
-                return r.json()
-            })
-            .then(data => {
-                if (data) {
-                    setCurrentNews(data.news)
-                }
-            })
-    }, [id])
+    // const [currentNews, setCurrentNews] = useState<NewsType | null>(null)
+    const currentNews = await getNewsById(Number(id))
+
     return (
         <div className={`flex  flex-col indents-main-container  flex-1 items-center`}>
-            {/* <Search /> */}
-            {currentNews && !error && (
+            {currentNews && (
                 <div className={`flex  gap-4 p-4 flex-col flex-1 max-w-6xl items-center  rounded-large bg-mainContainer `}>
                     <ReturnButton pathToReturn={routes.news.root} />
                     <News news={currentNews} typeNews={NEWS_TYPE.FULL} />
