37c40667f2ef60e4b17f80af295569f37662f8e3
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-27 17:55:32 +0200

    added template for welcome block

--- app/components/mainPageComponent/welcomeBlock.tsx
+++ app/components/mainPageComponent/welcomeBlock.tsx
@@ -2,13 +2,40 @@
 import DoctorImage from '@/public/images/doctor/doctor_2.png'
 import Image from 'next/image'
 
+const listPriviliges = [
+    'confidentiality and privacy',
+    'personalized care',
+    'access to resources',
+    'supportive community',
+]
+
 export default function WelcomeBlock() {
+
+
     return (
         <div className={`grid grid-cols-1 md:grid-cols-2 gap-6   w-full p-4 `}>
             <Image src={DoctorImage} alt="Doctor Image" className='h-150 rounded-large' />
             <div>
                 <h2 className={`text-2xl font-bold `}>Welcome to Our Mental Health Platform</h2>
                 <p className={``}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptas corporis explicabo eius facilis sunt, dolor laudantium dolores sapiente tempora, magnam asperiores fuga esse itaque molestias quidem beatae modi sint. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolor deserunt quos, dignissimos ipsa corrupti, expedita recusandae harum at necessitatibus atque illum voluptatibus obcaecati voluptates! Iure molestias veniam maxime accusantium. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur veniam incidunt quas alias rerum, reiciendis vero praesentium at sunt accusamus, inventore provident accusantium voluptas aliquam deserunt pariatur, ipsum asperiores dolorem!</p>
+                <div>
+                    <h3 className={`text-xl font-semibold mt-4`}>Already know  your problem?</h3>
+                    <button className={`mt-4 px-4 py-2 bg-buttonContainer rounded-large`}>Sign Up for a Consultation</button>
+                </div>
+                <div>
+                    <h3 className={`text-xl font-semibold mt-4`}>Need some guidance?</h3>
+                    <button className={`mt-4 px-4 py-2 bg-buttonContainer rounded-large`}>Go to Tests</button>
+                </div>
+                <div>
+                    <h3 className={`text-xl font-semibold mt-4`}>Our Privileges:</h3>
+                    {listPriviliges.map((privilege, index) => (
+                        <div key={index} className={`flex items-center mt-2`}>
+                            <span className={`w-2 h-2 bg-activeElement rounded-full mr-2`}></span>
+                            <span>{privilege}</span>
+                        </div>
+                    ))}
+                </div>
+
             </div>
 
         </div>
