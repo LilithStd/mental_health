516c2ed9e6c6f5e364ffb15af04ddc60cced37ae
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-27 17:35:54 +0200

    created welcome block for main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import RandomArticleBlock from "./components/blockRandomElements/randomArticleBl
 import RandomNewsBlock from "./components/blockRandomElements/randomNewsBlock";
 import MainPageBackgroundImage from "@/public/images/background/butterfly.png"
 import BackgroundImages from "./components/shared/backgroundImages";
+import WelcomeBlock from "./components/mainPageComponent/welcomeBlock";
 
 
 export default function Main() {
@@ -10,12 +11,13 @@ export default function Main() {
   // 
   return (
     <main className={`flex flex-col indents-main-container  flex-1 items-center`}>
-      <BackgroundImages imageSrc={MainPageBackgroundImage}>
-        <div className={`flex flex-col  max-w-6xl  rounded-large bg-mainContainer `}>
-          <RandomArticleBlock />
-          <RandomNewsBlock />
-        </div>
-      </BackgroundImages>
+      {/* <BackgroundImages imageSrc={MainPageBackgroundImage}> */}
+      <div className={`flex flex-col w-full  max-w-6xl  rounded-large bg-subContainer `}>
+        <WelcomeBlock />
+        {/* <RandomArticleBlock />
+          <RandomNewsBlock /> */}
+      </div>
+      {/* </BackgroundImages> */}
     </main>
   );
 }
