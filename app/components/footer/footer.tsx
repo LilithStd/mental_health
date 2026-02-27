74ece3c9789835d78073f867945b2f8c570cb304
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-27 19:24:00 +0200

    added footer in layout app

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -2,6 +2,12 @@
 
 export default function Footer() {
     return (
-        <div className={`flex rounded-medium  justify-center  indents-main-container `}>Footer</div>
+        <div className={`flex rounded-medium  justify-center  indents-main-container `}>
+            <div className={`flex flex-col max-w-6xl justify-between items-center w-full bg-subContainer rounded-large min-h-50 p-6`}>
+                <h2 className={`text-2xl font-bold `}>Footer</h2>
+                <h3 className={`text-lg font-medium `}>2026</h3>
+            </div>
+
+        </div>
     )
 }
