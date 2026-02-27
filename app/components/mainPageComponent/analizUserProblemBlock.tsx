69410aeaa51c6000b869b080d9d7271e8c042d89
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-02-27 19:13:17 +0200

    updated styles in analize user problem

--- app/components/mainPageComponent/analizUserProblemBlock.tsx
+++ app/components/mainPageComponent/analizUserProblemBlock.tsx
@@ -14,17 +14,22 @@ const problemsList = [
 export default function AnalizUserProblemBlock() {
     return (
         <div>
-            <div className={`grid grid-cols-2 rounded-large bg-subContainer p-6`}>
-                <div className={`flex flex-col gap-4`}>
-                    <h2 className={`text-2xl font-bold `}>When should you see a psychiatrist?</h2>
+            <div className={`grid grid-cols-1 gap-4 justify-center items-center text-center rounded-large bg-subContainer p-6`}>
+
+                {/* <div className={`flex flex-col gap-4`}> */}
+                <h2 className={`text-2xl font-bold `}>When should you see a psychiatrist?</h2>
+                <div className={`flex gap-2`}>
                     {problemsList.map((problem, index) => (
-                        <button key={index} className={`p-2 bg-buttonContainer rounded-large cursor-pointer`}>{problem}</button>
+                        <button key={index} className={`p-2 w-fit bg-buttonContainer rounded-large cursor-pointer`}>{problem}</button>
                     ))}
                 </div>
-                <div className={`flex justify-center items-center`}>
-                    <Image src={HumanWithProblem} alt="Human with Problem" className=' rounded-large' />
-                </div>
 
+                {/* </div> */}
+
+
+            </div>
+            <div className={`flex justify-center items-center`}>
+                <Image src={HumanWithProblem} alt="Human with Problem" className=' rounded-large' />
             </div>
             <div className={`flex w-full flex-col gap-4 p-6 justify-center items-center`}>
                 <h2 className={`text-2xl font-bold `}>Did you recognize yourself?</h2>
