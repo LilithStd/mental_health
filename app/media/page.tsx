7fe536b8ac9e0a536ce1c256d3c1e9a5b248eb1a
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-03-01 23:34:25 +0200

    added test path for redirect to news and articles

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { routes } from "../helpers/helpersFunctions";
 
 
 export default function MediaPage() {
@@ -5,6 +7,8 @@ export default function MediaPage() {
         <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
             <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-mainContainer p-4 `}>
                 <h2>Media Page</h2>
+                <Link href={routes.news.root} className={`text-blue-500 underline`}>News</Link>
+                <Link href={routes.articles.root} className={`text-blue-500 underline`}>Articles</Link>
             </div>
         </div>
     )
