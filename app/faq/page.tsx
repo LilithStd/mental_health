b6f7c26fd178989691c89f307fc56a19306225ed
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-03-01 19:45:47 +0200

    updated styles in faq component

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,8 +1,10 @@
 
 export default function FaqPage() {
     return (
-        <div>
-            <h2>Frequently Asked Questions</h2>
+        <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
+            <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-mainContainer p-4 `}>
+                <h2>Frequently Asked Questions</h2>
+            </div>
         </div>
     )
 }
