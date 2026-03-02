4cf42b5310a1f1f14907dc622a5359946963139b
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-03-02 19:44:51 +0200

    updated styles in media page

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
 import { routes } from "../helpers/helpersFunctions";
+import RandomArticleBlock from "../components/blockRandomElements/randomArticleBlock";
+import RandomNewsBlock from "../components/blockRandomElements/randomNewsBlock";
 
 
 export default function MediaPage() {
     return (
         <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
+
             <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-mainContainer p-4 `}>
-                <h2>Media Page</h2>
-                <Link href={routes.news.root} className={`text-blue-500 underline`}>News</Link>
-                <Link href={routes.articles.root} className={`text-blue-500 underline`}>Articles</Link>
+                <div className={`flex w-full justify-center gap-4 mb-4`}>
+                    <Link href={routes.news.root} className={`bg-buttonContainer p-4 rounded-large`}>News</Link>
+                    <Link href={routes.articles.root} className={`bg-buttonContainer p-4 rounded-large`}>Articles</Link>
+                </div>
+
+                <RandomArticleBlock />
+                <RandomNewsBlock />
+
             </div>
         </div>
     )
