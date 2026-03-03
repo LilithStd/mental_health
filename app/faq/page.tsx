7cf0e568686decac710ae06b59ebbc6babd9cff9
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-03-03 19:19:59 +0200

    updated styles

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -3,65 +3,65 @@ export default function FaqPage() {
     return (
         <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
             <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-mainContainer p-4 `}>
-                <h2>Frequently Asked Questions</h2>
-                <details className={`list-disc list-inside mb-4`}>
+                <div className={`flex flex-col items-center mb-8`}>
+                    <h2>F.A.Q </h2>
+                    <h2>(Frequently Asked Questions)</h2>
+                </div>
+
+                <details className={`list-disc list-inside mb-4 bg-subContainer p-4 rounded-large hover:bg-hover cursor-pointer `}>
                     <summary className={``}>What is the difference between a psychiatrist and a psychologist or psychotherapist?</summary>
                     <p>
-                        1. Whats the difference between a psychiatrist and a psychologist or psychotherapist?
+                        Whats the difference between a psychiatrist and a psychologist or psychotherapist?
 
                         A psychiatrist is a physician with a medical degree. They can diagnose, prescribe medication, and write prescriptions.
                         A psychologist is not a physician and does not prescribe medication.
                         A psychotherapist is a specialist who practices psychotherapy; in some countries, they are considered physicians, but in others, they are not.
                     </p>
                 </details>
-                <details className={`list-disc list-inside mb-4`}>
+                <details className={`list-disc list-inside mb-4 bg-subContainer p-4 rounded-large hover:bg-hover cursor-pointer `}>
                     <summary className={``}>What conditions do psychiatrists treat?</summary>
                     <p>
-                        2. Will I necessarily be prescribed medication?
+                        Will I necessarily be prescribed medication?
 
                         No. Medication is prescribed only when indicated.
                         In many cases, psychotherapy, observation, or a comprehensive approach may be sufficient. The decision is made jointly with the patient.
                     </p>
                 </details>
-                <details>
+                <details className={`list-disc list-inside mb-4 bg-subContainer p-4 rounded-large hover:bg-hover cursor-pointer `}>
                     <summary className={``}>Will I necessarily be prescribed medication?</summary>
                     <p>
-                        3. Is the consultation confidential?
+                        Will I necessarily be prescribed medication?
 
+                        No. Medication is prescribed only when indicated.
+                        In many cases, psychotherapy, observation, or a comprehensive approach may be sufficient. The decision is made jointly with the patient.
+                    </p>
+                </details>
+                <details className={`list-disc list-inside mb-4 bg-subContainer p-4 rounded-large hover:bg-hover cursor-pointer `}>
+                    <summary className={``}>Is the consultation confidential?</summary>
+                    <p>
                         Yes. All information is strictly confidential and will not be shared with third parties, except in cases required by law (for example, in cases of life-threatening situations).
                     </p>
                 </details>
-                <details>
-                    <summary className={``}>Is the consultation confidential?</summary>
+                <details className={`list-disc list-inside mb-4 bg-subContainer p-4 rounded-large hover:bg-hover cursor-pointer `}>
+                    <summary className={``}>What does the initial consultation involve?</summary>
                     <p>
-                        4. What does the initial consultation involve?
-
                         The initial consultation usually lasts 50–60 minutes. We discuss symptoms, medical history, lifestyle, and, if necessary, develop a treatment or monitoring plan.
                     </p>
                 </details>
-                <details>
-                    <summary className={``}>What does the initial consultation involve?</summary>
+                <details className={`list-disc list-inside mb-4 bg-subContainer p-4 rounded-large hover:bg-hover cursor-pointer `}>
+                    <summary className={``}>How long does treatment last?</summary>
                     <p>
-                        5. How long does treatment last?
-
                         The duration depends on the diagnosis and individual circumstances.
                         Some conditions require short-term support, while others require longer-term monitoring.
                     </p>
                 </details>
-                <details>
-                    <summary className={``}>How long does treatment last?</summary>
-                    <p> 6. Is it possible to get help online?
-
-                        Yes, consultations are available online (video).
-                        Online consultations are suitable for most conditions if an in-person examination is not necessary.</p>
-                </details>
-                <details>
+                <details className={`list-disc list-inside mb-4 bg-subContainer p-4 rounded-large hover:bg-hover cursor-pointer `}>
                     <summary className={``}>Is it possible to get help online?</summary>
                     <p>
                         No. If you feel distressed, anxious, or if your condition is interfering with your life, that is sufficient reason to contact us. A diagnosis is made during the consultation.
                     </p>
                 </details>
-                <details>
+                <details className={`list-disc list-inside mb-4 bg-subContainer p-4 rounded-large hover:bg-hover cursor-pointer `}>
                     <summary className={``}>What if I need urgent help?</summary>
                     <p>
                         If you are experiencing an acute crisis, suicidal thoughts, or a life-threatening situation, you must immediately contact emergency services or the nearest medical facility.
