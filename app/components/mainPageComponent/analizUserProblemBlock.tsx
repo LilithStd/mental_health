894398bfe90aebeda1d7224d10a9af0c9dbc32c5
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-03-04 20:52:49 +0200

    updated link to routes

--- app/components/mainPageComponent/analizUserProblemBlock.tsx
+++ app/components/mainPageComponent/analizUserProblemBlock.tsx
@@ -1,6 +1,8 @@
 
+import { routes } from '@/app/helpers/helpersFunctions'
 import HumanWithProblem from '@/public/images/problems/sad_human_with_problem.png'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const problemsList = [
     'Persistent anxiety for more than 2 weeks',
@@ -15,25 +17,19 @@ export default function AnalizUserProblemBlock() {
     return (
         <div>
             <div className={`grid grid-cols-1 gap-4 justify-center items-center text-center rounded-large bg-subContainer p-6`}>
-
-                {/* <div className={`flex flex-col gap-4`}> */}
                 <h2 className={`text-2xl font-bold `}>When should you see a psychiatrist?</h2>
                 <div className={`flex gap-2`}>
                     {problemsList.map((problem, index) => (
                         <button key={index} className={`p-2 w-fit bg-buttonContainer rounded-large cursor-pointer`}>{problem}</button>
                     ))}
                 </div>
-
-                {/* </div> */}
-
-
             </div>
             <div className={`flex justify-center items-center`}>
                 <Image src={HumanWithProblem} alt="Human with Problem" className=' rounded-large' />
             </div>
             <div className={`flex w-full flex-col gap-4 p-6 justify-center items-center`}>
                 <h2 className={`text-2xl font-bold `}>Did you recognize yourself?</h2>
-                <button className={`p-4 justify-center items-center bg-buttonContainer rounded-large cursor-pointer w-fit`}>Sign Up for a Consultation</button>
+                <Link href={routes.consultation.root} className={`p-4 justify-center items-center bg-buttonContainer rounded-large cursor-pointer w-fit`}>Sign Up for a Consultation</Link>
 
             </div>
         </div>
--- app/components/mainPageComponent/pricesBlock.tsx
+++ app/components/mainPageComponent/pricesBlock.tsx
@@ -1,5 +1,7 @@
+import { routes } from '@/app/helpers/helpersFunctions'
 import MedicineWomen from '@/public/images/doctor/doctor_5.png'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function PricesBlock() {
     return (
@@ -8,7 +10,7 @@ export default function PricesBlock() {
                 <div className={`flex flex-col gap-4`}>
                     <h2 className={`text-2xl font-bold `}>Prices Block</h2>
                     <p className={`mt-4`}>Discover our affordable pricing options for mental health consultations and services.</p>
-                    <button className={`mt-4 px-4 py-2 bg-buttonContainer w-fit rounded-large`}>View Pricing</button>
+                    <Link href={routes.pricing.root} className={`mt-4 px-4 py-2 bg-buttonContainer w-fit rounded-large`}>View Pricing</Link>
                 </div>
                 <div className={`flex justify-center items-center`}>
                     <Image src={MedicineWomen} alt="Medicine Women" />
