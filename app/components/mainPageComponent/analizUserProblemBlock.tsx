14ccd1e1e3fb8ecf90c48a82c6e1de271134b133
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-03-08 21:42:36 +0200

    created analize user problem block content and updated component for use this content

--- app/components/mainPageComponent/analizUserProblemBlock.tsx
+++ app/components/mainPageComponent/analizUserProblemBlock.tsx
@@ -1,6 +1,9 @@
-
+'use client'
 import { routes } from '@/app/helpers/helpersFunctions'
+import { useLocale } from '@/app/hooks/useLocale'
+import { LocaleType } from '@/app/types/types'
 import HumanWithProblem from '@/public/images/problems/sad_human_with_problem.png'
+import { AnalizeUserProblemContent } from '@/translate/mainPage/analiazeUserProblemBlock'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -13,14 +16,15 @@ const problemsList = [
     'Thoughts of self-harm or suicide'
 ]
 
-export default function AnalizUserProblemBlock({ locale }: { locale: string }) {
+export default function AnalizUserProblemBlock() {
+    const locale = useLocale() as LocaleType
     const routesAdaptive = routes(locale)
     return (
         <div>
             <div className={`grid grid-cols-1 gap-4 justify-center items-center text-center rounded-large bg-subContainer p-6`}>
-                <h2 className={`text-2xl font-bold `}>When should you see a psychiatrist?</h2>
+                <h2 className={`text-2xl font-bold `}>{AnalizeUserProblemContent[locale].TITLE}</h2>
                 <div className={`flex gap-2`}>
-                    {problemsList.map((problem, index) => (
+                    {AnalizeUserProblemContent[locale].PROBLEMS.map((problem, index) => (
                         <button key={index} className={`p-2 w-fit bg-buttonContainer rounded-large cursor-pointer`}>{problem}</button>
                     ))}
                 </div>
@@ -29,8 +33,8 @@ export default function AnalizUserProblemBlock({ locale }: { locale: string }) {
                 <Image src={HumanWithProblem} alt="Human with Problem" className=' rounded-large' />
             </div>
             <div className={`flex w-full flex-col gap-4 p-6 justify-center items-center`}>
-                <h2 className={`text-2xl font-bold `}>Did you recognize yourself?</h2>
-                <Link href={routesAdaptive.consultation.root} className={`p-4 justify-center items-center bg-buttonContainer rounded-large cursor-pointer w-fit`}>Sign Up for a Consultation</Link>
+                <h2 className={`text-2xl font-bold `}>{AnalizeUserProblemContent[locale].TITLE_2}</h2>
+                <Link href={routesAdaptive.consultation.root} className={`p-4 justify-center items-center bg-buttonContainer rounded-large cursor-pointer w-fit`}>{AnalizeUserProblemContent[locale].SIGN_UP_BUTTON}</Link>
 
             </div>
         </div>
---
+++ translate/mainPage/analiazeUserProblemBlock.ts
@@ -0,0 +1,41 @@
+export const AnalizeUserProblemContent = {
+    en: {
+        TITLE: 'When should you see a psychiatrist?',
+        TITLE_2: 'Did you recognize yourself?',
+        SIGN_UP_BUTTON: 'Sign Up for a Consultation',
+        PROBLEMS:[
+            'Persistent anxiety for more than 2 weeks',
+            'Depressive symptoms lasting more than 2 weeks',
+            'Significant changes in sleep or appetite',
+            'Difficulty concentrating or making decisions',
+            'Feelings of hopelessness or worthlessness',
+            'Thoughts of self-harm or suicide'
+        ]
+    },
+    lv: {
+        TITLE: 'Kad jums vajadzētu apmeklēt psihiatru?',
+        TITLE_2: 'Vai jūs atpazināt sevi?',
+        SIGN_UP_BUTTON: 'Pierakstīties konsultācijai',
+        PROBLEMS:[
+            'Pastāvīga trauksme ilgāk par 2 nedēļām',
+            'Depresijas simptomi ilgāk par 2 nedēļām',
+            'Būtiskas izmaiņas miega vai apetītes paradumos',
+            'Grūtības koncentrēties vai pieņemt lēmumus',
+            'Beznākuma vai nevērtības sajūtas',
+            'Pašiznīcinošas vai pašnāvības domas'
+        ]
+    },
+    ru: {
+        TITLE: 'Когда следует обратиться к психиатру?',
+        TITLE_2: 'Вы узнали себя?',
+        SIGN_UP_BUTTON: 'Записаться на консультацию',
+        PROBLEMS:[
+            'Постоянная тревога более 2 недель',
+            'Депрессивные симптомы более 2 недель',
+            'Значительные изменения сна или аппетита',
+            'Трудности с концентрацией или принятием решений',
+            'Чувство безнадежности или никчемности',
+            'Мысли о самоповреждении или суициде'
+        ]
+    }
+} as const
