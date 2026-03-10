c3ae351277ed3bf64aa63015ab1eb041ffada241
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-03-10 21:26:46 +0200

    created consultation page content

---
+++ translate/consultationPage/consultationPage.ts
@@ -0,0 +1,32 @@
+export const ConsultationPageContent = {
+    en: {
+        title: 'Contact Information',
+        name: 'Name',
+        subject: 'Subject',
+        email: 'Email',
+        phone: 'Phone',
+        date: 'Date',
+        message: 'Message',
+        buttonText: 'Submit',
+    },
+    lv: {
+        title: 'Kontakti',
+        name: 'Vārds',
+        subject: 'Tēma',
+        email: 'E-pasts',
+        phone: 'Telefons',
+        date: 'Datums',
+        message: 'Ziņa',
+        buttonText: 'Iesniegt',
+    },
+    ru: {
+        title: 'Контактная Информация',
+        name: 'Имя',
+        subject: 'Тема',
+        email: 'Электронная почта',
+        phone: 'Телефон',   
+        date: 'Дата',
+        message: 'Сообщение',
+        buttonText: 'Отправить',
+    }
+} as const
