4875174f1d06acad92791b1a86d3256dedc3bddd
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-03-10 21:20:07 +0200

    created price page content

---
+++ translate/pricesPage/pricePage.ts
@@ -0,0 +1,56 @@
+export const PricesPageContent = {
+    en: {
+        title: 'Our Pricing',
+        description: 'We offer affordable pricing plans to make mental health care accessible to everyone. Choose the plan that best suits your needs and start your journey towards better mental well-being today.',
+        list: [
+            {
+                title: 'Basic Plan',
+                description: 'Our Basic Plan offers essential mental health support, including access to a licensed therapist for one session per month. This plan is ideal for individuals seeking occasional guidance and support for managing stress, anxiety, or other mental health concerns.',
+            },
+            {
+                title: 'Premium Plan',
+                description: 'Our Premium Plan provides comprehensive mental health support, including access to a licensed therapist for weekly sessions, personalized treatment plans, and 24/7 support. This plan is designed for individuals seeking ongoing support and guidance for managing more complex mental health issues.',
+            },
+            {
+                title: 'Consultation Services',
+                description: 'In addition to our Basic and Premium Plans, we also offer consultation services for individuals seeking specific advice or guidance on mental health concerns. Our consultations are conducted by licensed therapists and can be scheduled on an as-needed basis.',
+            },
+        ],
+    },
+    lv: {
+        title: 'Mūsu Cenas',
+        description: 'Mēs piedāvājam pieejamas cenu plānus, lai padarītu garīgās veselības aprūpi pieejamu ikvienam. Izvēlieties plānu, kas vislabāk atbilst jūsu vajadzībām, un sāciet savu ceļojumu uz labāku garīgo labklājību jau šodien.',
+        list: [
+            {
+                title: 'Pamata Plāns',
+                description: 'Mūsu Pamata Plāns piedāvā būtisku garīgās veselības atbalstu, tostarp piekļuvi licencētam terapeitam vienai sesijai mēnesī. Šis plāns ir ideāls cilvēkiem, kuri meklē ikreizēju vadību un atbalstu stresa, trauksmes vai citu garīgās veselības problēmu pārvaldībā.',
+            },
+            {
+                title: 'Premium Plāns',
+                description: 'Mūsu Premium Plāns nodrošina visaptverošu garīgās veselības atbalstu, tostarp piekļuvi licencētam terapeitam iknedēļas sesijām, personalizētiem ārstēšanas plāniem un 24/7 atbalstu. Šis plāns ir paredzēts cilvēkiem, kuri meklē pastāvīgu atbalstu un vadību sarežģītāku garīgās veselības problēmu pārvaldībā.',
+            },
+            {
+                title: 'Konsultāciju Pakalpojumi',
+                description: 'Papildus mūsu Pamata un Premium Plāniem mēs piedāvājam arī konsultāciju pakalpojumus cilvēkiem, kuri meklē konkrētus padomus vai vadību par garīgās veselības problēmām. Mūsu konsultācijas tiek veiktas licencētu terapeitu vadībā un var tikt ieplānotas pēc nepieciešamības.',
+            },
+        ],
+    },
+    ru: {
+        title: 'Наши Цены',
+        description: 'Мы предлагаем доступные тарифные планы, чтобы сделать психиатрическую помощь доступной для всех. Выберите план, который лучше всего соответствует вашим потребностям, и начните свой путь к лучшему психическому благополучию уже сегодня.',
+        list: [
+            {
+                title: 'Базовый План',
+                description: 'Наш Базовый План предлагает основную поддержку психического здоровья, включая доступ к лицензированному терапевту для одной сессии в месяц. Этот план идеально подходит для людей, которые ищут периодическое руководство и поддержку в управлении стрессом, тревогой или другими проблемами психического здоровья.',
+            },
+            {
+                title: 'Премиум План',
+                description: 'Наш Премиум План предоставляет комплексную поддержку психического здоровья, включая доступ к лицензированному терапевту для еженедельных сессий, персонализированные планы лечения и круглосуточную поддержку. Этот план предназначен для людей, которые ищут постоянную поддержку и руководство в управлении более сложными проблемами психического здоровья.',
+            },
+            {
+                title: 'Консультационные Услуги',
+                description: 'В дополнение к нашим Базовому и Премиум Планам, мы также предлагаем консультационные услуги для людей, которые ищут конкретные советы или руководство по проблемам психического здоровья. Наши консультации проводятся лицензированными терапевтами и могут быть запланированы по мере необходимости.',
+            },
+        ],
+    }
+} as const
