477e33e6b631620ddf1050935198180f8b255879
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-19 20:07:56 +0200

    added fonts for app (need some test)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poiret_One } from "next/font/google";
+import { Geist, Geist_Mono, Poiret_One, Jura, Alumni_Sans_Pinstripe } from "next/font/google";
 import "./globals.css";
 
 
@@ -25,6 +25,17 @@ const poiret = Poiret_One({
   variable: '--font-poiret',
 });
 
+const jura = Jura({
+  subsets: ['latin', 'cyrillic'],
+  weight: '400',
+  variable: '--font-jura',
+});
+const alumniSansPinstripe = Alumni_Sans_Pinstripe({
+  subsets: ['latin', 'cyrillic'],
+  weight: '400',
+  variable: '--font-alumni-sans-pinstripe',
+});
+
 export const metadata: Metadata = {
   title: "Mental health",
   description: "App about mental health",
@@ -40,7 +51,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${poiret.variable} bg-background text-foreground antialiased `}
+        className={`${geistSans.variable} ${geistMono.variable} ${poiret.variable} ${jura.variable} ${alumniSansPinstripe.variable} bg-background text-foreground antialiased `}
       >
         <Providers>
           <AuthProvider user={currentAuthUser}>
