c25e95cefa22f002ffd0b8eeb6c145dc5ad07b12
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-19 22:34:21 +0200

    updated styled in preces blocks (main page)

--- app/components/mainPageComponent/pricesBlock.tsx
+++ app/components/mainPageComponent/pricesBlock.tsx
@@ -14,9 +14,9 @@ export default function PricesBlock() {
         <div className={`flex gap-4 p-6 justify-center items-center`}>
             <div className={`grid grid-cols-2 p-6 justify-center items-center`}>
                 <div className={`flex flex-col gap-4`}>
-                    <h2 className={`text-2xl font-bold `}>{PricesBlockContent[locale].title}</h2>
-                    <p className={`mt-4`}>{PricesBlockContent[locale].description}</p>
-                    <Link href={routesAdaptive.pricing.root} className={`mt-4 px-4 py-2 bg-buttonContainer w-fit rounded-large`}>{PricesBlockContent[locale].buttonText}</Link>
+                    <h2 className={`text-5xl font-pattaya font-bold `}>{PricesBlockContent[locale].title}</h2>
+                    <p className={`font-poiret font-bold italic text-lg`}>{PricesBlockContent[locale].description}</p>
+                    <Link href={routesAdaptive.pricing.root} className={`mt-4 px-4 py-2 bg-buttonContainer w-fit rounded-large font-bold font-jura`}>{PricesBlockContent[locale].buttonText}</Link>
                 </div>
                 <div className={`flex justify-center items-center`}>
                     <Image src={MedicineWomen} alt="Medicine Women" />
