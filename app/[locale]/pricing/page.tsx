d8580a026803d0fe20214acdb78455588ec2009c
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-20 21:56:24 +0200

    updated styles in prices page

--- app/[locale]/pricing/page.tsx
+++ app/[locale]/pricing/page.tsx
@@ -10,25 +10,21 @@ export default function PricingPage() {
     return (
         <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
             <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-mainContainer p-4 `}>
-                <h2 className={`text-2xl justify-center text-center font-bold`}>{PricesPageContent[locale].title}</h2>
-                <p className={`text-center mt-2`}>{PricesPageContent[locale].description}</p>
+                <h2 className={`text-5xl justify-center text-center font-pattaya font-bold`}>{PricesPageContent[locale].title}</h2>
+                <p className={`text-center font-jura mt-2`}>{PricesPageContent[locale].description}</p>
                 <div className={`grid grid-cols-2 gap-4 mt-4 justify-center`}>
-
-                    <div>
-                        <Image src={DoctorPricing} alt="Doctor Pricing" className='w-100 p-4 rounded-large' />
+                    <div className={`flex justify-center items-center`}>
+                        <Image src={DoctorPricing} alt="Doctor Pricing" className='w-200 p-4 rounded-large' />
                     </div>
                     <div className={`flex flex-col gap-4`}>
                         {PricesPageContent[locale].list.map((item, index) => (
                             <div key={index}>
-                                <h2 className={`text-xl font-bold`}>{item.title}</h2>
-                                <p>{item.description}</p>
+                                <h2 className={`text-xl font-pattaya font-bold`}>{item.title}</h2>
+                                <p className={`font-jura`}>{item.description}</p>
                             </div>
                         ))}
                     </div>
                 </div>
-
-
-
             </div>
         </div>
     )
