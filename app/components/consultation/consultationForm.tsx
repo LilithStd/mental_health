f7d70f9b7a1866430c5450b65bd0f4c0d8bf2380
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-20 21:52:05 +0200

    updated styles

--- app/components/consultation/consultationForm.tsx
+++ app/components/consultation/consultationForm.tsx
@@ -23,39 +23,39 @@ export default function ConsultationForm() {
     return (
         <div className={`flex flex-col w-full  max-w-6xl  rounded-large `}>
             <form ref={ref} action={action} className={`mx-auto  p-4 items-center rounded-large bg-mainContainer max-content-main-container`}>
-                <label className={`flex justify-center text-center mb-2`}>{ConsultationPageContent[locale].title}</label>
+                <label className={`flex justify-center text-3xl font-pattaya text-center mb-2`}>{ConsultationPageContent[locale].title}</label>
                 <div className={`flex gap-4 flex-col w-full items-center h-full`}>
                     <div className={`flex items-center gap-2 w-1/2`}>
                         <div className={`flex flex-col gap-4 w-full`}>
                             <label className={` p-2 rounded-large flex flex-col`}>
-                                <span className={``}>{ConsultationPageContent[locale].name}:</span>
+                                <span className={`font-jura font-bold`}>{ConsultationPageContent[locale].name}:</span>
                                 <input name="name" required placeholder="John Doe" type="text" className={`p-4 bg-input rounded-large h-15`} />
                             </label>
                             <label className={` p-2 rounded-large flex flex-col`}>
-                                <span className={``}>{ConsultationPageContent[locale].subject}:</span>
+                                <span className={`font-jura font-bold`}>{ConsultationPageContent[locale].subject}:</span>
                                 <input name="subject" required placeholder="Subject" type="text" className={` p-4 bg-input rounded-large h-15`} />
                             </label>
                             <label className={` p-2 rounded-large flex flex-col`}>
-                                <span className={``}>{ConsultationPageContent[locale].email}:</span>
+                                <span className={`font-jura font-bold`}>{ConsultationPageContent[locale].email}:</span>
                                 <input name="email" required placeholder="email@example.com" type="email" className={` p-4 bg-input rounded-large h-15`} />
                             </label>
                             <label className={` p-2 rounded-large flex flex-col`}>
-                                <span className={``}>{ConsultationPageContent[locale].phone}:</span>
+                                <span className={`font-jura font-bold`}>{ConsultationPageContent[locale].phone}:</span>
                                 <input name="phone" required placeholder="(123) 456-7890" type="tel" className={` p-4 bg-input rounded-large h-15`} />
                             </label>
                             <label className={` p-2 rounded-large flex flex-col`}>
-                                <span className={``}>{ConsultationPageContent[locale].date}:</span>
+                                <span className={`font-jura font-bold`}>{ConsultationPageContent[locale].date}:</span>
                                 <input name="date" required placeholder="MM/DD/YYYY" type="date" className={` p-4 bg-input rounded-large h-15`} />
                             </label>
                         </div>
                     </div>
                     <label className={`flex flex-col gap-2 w-1/2`}>
-                        <span className={``}>{ConsultationPageContent[locale].message}*:</span>
+                        <span className={`font-jura font-bold`}>{ConsultationPageContent[locale].message}*:</span>
                         <textarea id="message" required name="message" placeholder={ConsultationPageContent[locale].message} className={` min-h-40 w-full h-full p-4 bg-input rounded-large`} />
                     </label>
                 </div>
                 <div className={`flex justify-center m-4`}>
-                    <button type="submit" className={` items-center mb-2 mt-2  w-1/4  rounded-large p-4 bg-buttonContainer`}>{ConsultationPageContent[locale].buttonText}</button>
+                    <button type="submit" className={`font-bold font-jura items-center mb-2 mt-2  w-1/4  rounded-large p-4 bg-buttonContainer`}>{ConsultationPageContent[locale].buttonText}</button>
                 </div>
 
             </form>
