b814e34aa1585ecbf4f66a9b7fd3c51a061f0afd
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-24 16:58:45 +0200

    updated logic to modal window

--- app/components/mainPageComponent/analizUserProblemBlock.tsx
+++ app/components/mainPageComponent/analizUserProblemBlock.tsx
@@ -7,15 +7,35 @@ import CloudCartoonVisualizationProblem from '@/public/images/problems/cloudCart
 import { AnalizeUserProblemContent } from '@/translate/mainPage/analiazeUserProblemBlock'
 import Image from 'next/image'
 import Link from 'next/link'
+import ModalWindowMain from '../modalWindowMain'
+import ModalWindowUserProblemDescription from '../modalWindows/modalWindowUserProblemDescription'
+import { useState } from 'react'
 
 
 export default function AnalizUserProblemBlock() {
     const locale = useLocale() as LocaleType
     const routesAdaptive = routes(locale)
+    // state
+    const [isOpenModalWindow, setIsOpenModalWindow] = useState(false);
+    const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+
+    //
+    // functions
+    // handlers
+    const openModalWindowHandler = (x: number, y: number) => {
+        setCoordinates({ x, y });
+        // console.log(coordinates);
+        setIsOpenModalWindow(true);
+    }
+    const closeModalWindowHandler = () => {
+        setIsOpenModalWindow(false);
+    }
     return (
         <div className={`flex flex-col gap-4 justify-center relative items-center`}>
             <div className={`grid grid-cols-1 gap-2 justify-center items-center text-center rounded-large p-6`}>
-
+                {isOpenModalWindow &&
+                    <ModalWindowUserProblemDescription coordinates={coordinates} closeCallback={closeModalWindowHandler} />
+                }
                 <h2 className={`text-5xl p-4 font-pattaya font-bold `}>{AnalizeUserProblemContent[locale].TITLE}</h2>
                 <div className="flex gap-2 w-full justify-center items-center flex-wrap">
                     {AnalizeUserProblemContent[locale].PROBLEMS.map((problem, index) => (
@@ -23,6 +43,7 @@ export default function AnalizUserProblemBlock() {
                         <div
                             key={index}
                             className="relative w-60 min-h-40 p-4 rounded-large flex justify-center items-center"
+                            onClick={(e) => openModalWindowHandler(e.clientX, e.clientY)}
                         >
 
                             <Image
@@ -43,7 +64,6 @@ export default function AnalizUserProblemBlock() {
             </div>
             <div className={`flex justify-center items-center`}>
                 <Image src={HumanWithProblem} alt="Human with Problem" className=' rounded-large' />
-
             </div>
             <div className={`flex w-full flex-col gap-4 p-6 justify-center items-center`}>
                 <h2 className={`text-4xl font-pattaya font-bold `}>{AnalizeUserProblemContent[locale].TITLE_2}</h2>
--- app/components/modalWindows/modalWindowUserProblemDescription.tsx
+++ app/components/modalWindows/modalWindowUserProblemDescription.tsx
@@ -1,12 +1,30 @@
 'use client'
+
+
+
 interface ModalWindowUserProblemDescriptionProps {
     closeCallback: () => void,
+    coordinates: {
+        x: number,
+        y: number
+    }
 }
-export default function ModalWindowUserProblemDescription({ closeCallback }: ModalWindowUserProblemDescriptionProps) {
-
+export default function ModalWindowUserProblemDescription({ closeCallback, coordinates }: ModalWindowUserProblemDescriptionProps) {
+    const Modal_Size = {
+        width: 200,
+        height: 100
+    }
+    const OffSet = 8
+    const safeXcoordinate = Math.min(coordinates.x, window.innerWidth - Modal_Size.width - OffSet);
+    const safeYcoordinate = Math.min(coordinates.y, window.innerHeight - Modal_Size.height - OffSet);
+    console.log(coordinates);
     return (
-        <div className={`flex flex-col gap-4 justify-center relative items-center`}>
+        <div className={`flex flex-col gap-4 justify-center z-10 items-center bg-activeElement absolute`}
+            style={{ top: `${safeYcoordinate}px`, left: `${safeXcoordinate}px` }}
+        >
             <h2>modalWindowUserProblemDescription</h2>
+            <button onClick={closeCallback} className={`px-4 py-2 bg-blue-500 text-white rounded-md`}>Close Modal</button>
         </div>
     )
 }
+
