f22f3af2284dfa63cecfbfd3ddcac5734f1cb3b3
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-25 20:00:35 +0200

    updated styles in header(now header position sticky)

--- app/components/mainPageComponent/problemSolvingBlock.tsx
+++ app/components/mainPageComponent/problemSolvingBlock.tsx
@@ -8,30 +8,54 @@ import Image from 'next/image'
 export default function ProblemSolvingBlock() {
     const localeAdapted = useLocale() as LocaleType
     // components
+    // const probmlemSolvingBlock = () => {
+    //     return (
+    //         <div className={`grid grid-cols-2 gap-4 items-start`}>
+    //             {ProblemSolvingBlockContent[localeAdapted].problems.map((problem, index) => (
+    //                 <details key={index} className={`font-jura font-bold mb-4 bg-buttonContainer p-4 rounded-large hover:bg-hover cursor-pointer `}>
+    //                     <summary className={`text-lg font-jura font-bold mb-2 cursor-pointer `}>
+    //                         <span>{problem.problem}</span>
+    //                     </summary>
+    //                     <p>{problem.description}</p>
+    //                 </details>
+    //             ))}
+    //         </div>
+    //     )
+    // }
+
     const probmlemSolvingBlock = () => {
         return (
-            <div>
+            <div className={`grid grid-cols-2 gap-4 items-start`}>
                 {ProblemSolvingBlockContent[localeAdapted].problems.map((problem, index) => (
-                    <details key={index} className={`list-disc list-inside font-jura font-bold mb-4 bg-buttonContainer p-4 rounded-large hover:bg-hover cursor-pointer `}>
-                        <summary>
+                    <div key={index} className={` font-bold mb-4 bg-buttonContainer p-4 rounded-large hover:bg-hover cursor-pointer min-h-40`}>
+                        <p className={`text-lg font-bold  cursor-pointer `}>
                             <span>{problem.problem}</span>
-                        </summary>
-                        <p>{problem.description}</p>
-                    </details>
+                        </p>
+                    </div>
                 ))}
             </div>
         )
     }
 
+
     return (
-        <div className={`grid grid-cols-2 w-full rounded-large p-6`}>
-            <div className={`grid items-start justify-start rounded-large p-4`}>
-                <h2 className={`text-3xl mb-4 font-pattaya font-bold `}>{ProblemSolvingBlockContent[localeAdapted].title}</h2>
-                {probmlemSolvingBlock()}
-            </div>
-            <div className={`grid items-center justify-end rounded-large p-4`}>
+        <div>
+            <div className={`flex w-full rounded-large p-6`}></div>
+            <h2 className={`text-3xl mb-4 font-bold `}>{ProblemSolvingBlockContent[localeAdapted].title}</h2>
+            {probmlemSolvingBlock()}
+
+            {/* <div className={`grid items-center justify-end rounded-large p-4`}>
                 <Image src={HumanWithProblem} alt="Human with Problem" className=' p-4 rounded-large' />
-            </div>
+            </div> */}
         </div>
+        // <div className={`grid grid-cols-2 w-full rounded-large p-6`}>
+        //     <div className={`grid items-start justify-start rounded-large p-4`}>
+        //         <h2 className={`text-3xl mb-4 font-pattaya font-bold `}>{ProblemSolvingBlockContent[localeAdapted].title}</h2>
+        //         {probmlemSolvingBlock()}
+        //     </div>
+        //     <div className={`grid items-center justify-end rounded-large p-4`}>
+        //         <Image src={HumanWithProblem} alt="Human with Problem" className=' p-4 rounded-large' />
+        //     </div>
+        // </div>
     )
 }
--- app/components/mainPageComponent/welcomeBlock.tsx
+++ app/components/mainPageComponent/welcomeBlock.tsx
@@ -19,7 +19,7 @@ export default function WelcomeBlock() {
 
     const routesAdaptive = routes(localeAdapted as LocaleType)
     const listPriviliges = WelcomeBlockContent[localeAdapted].PRIVILEGES_LIST
-
+    console.log(listPriviliges)
     return (
         <div className={`grid grid-cols-1 md:grid-cols-2 gap-6 w-full p-4  `}>
             {/* <Image src={DoctorImage} alt="Doctor Image" className='rounded-large' /> */}
@@ -36,16 +36,19 @@ export default function WelcomeBlock() {
                     <Link href={routesAdaptive.tests.root} className={`px-4 w-fit py-2 bg-buttonContainer font-geistSans font-bold italic rounded-large`}>{WelcomeBlockContent[localeAdapted].TESTS_BUTTON}</Link>
                 </div> */}
                 <div className={`flex flex-col gap-4 mt-4 `}>
-                    <Link href={routesAdaptive.consultation.root} className={`p-4 w-fit bg-buttonContainer font-geistSans  font-bold italic rounded-full`}>{WelcomeBlockContent[localeAdapted].SIGN_UP_BUTTON}</Link>
+                    <Link href={routesAdaptive.consultation.root} className={`p-4 w-fit bg-buttonContainer font-geistSans shadow-lg font-bold italic rounded-full`}>{WelcomeBlockContent[localeAdapted].SIGN_UP_BUTTON}</Link>
                     <Link href={routesAdaptive.tests.root} className={`p-4 w-fit  bg-neutral-200 font-geistSans font-bold italic rounded-full`}>{WelcomeBlockContent[localeAdapted].TESTS_BUTTON}</Link>
                 </div>
                 <div className={`flex flex-col gap-2`}>
-                    <h3 className={`text-xl font-geistSans font-bold mt-2`}>{WelcomeBlockContent[localeAdapted].PRIVILEGES_TITLE}</h3>
-                    <ul className={`list-none flex flex-col gap-2 `}>
+                    {/* <h3 className={`text-xl font-geistSans font-bold mt-2`}>{WelcomeBlockContent[localeAdapted].PRIVILEGES_TITLE}</h3> */}
+                    <ul className={`list-none grid grid-cols-1 md:grid-cols-2 gap-6 mt-6`}>
                         {listPriviliges.map((privilege, index) => (
-                            <li key={index} className={`flex  items-center gap-2`}>
-                                <span className={`w-2 h-2 bg-activeElement rounded-full mr-2`}></span>
+                            <li key={index} className={`flex items-center gap-2`}>
+                                <span className={`bg-activeElement/20 rounded-full p-2 flex items-center justify-center`}>
+                                    <privilege.icon className={`w-6 h-6`} fill={'green'} />
+                                </span>
                                 <span className={`font-geistSans font-bold`}>{privilege.text}</span>
+
                             </li>
                         ))}
                     </ul>
