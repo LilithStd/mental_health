f2441588076d848a700fd2eb172f0fc709ec882f
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-26 22:21:47 +0200

    updated styles (footer)

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -6,22 +6,22 @@ export default async function Footer() {
     const locale = await getLocale() as LocaleType
     return (
         <div className={`flex rounded-medium  justify-center  indents-main-container `}>
-            <div className={`flex flex-col max-w-6xl justify-between items-center w-full bg-subContainer rounded-large min-h-20 p-6`}>
-                <div>
+            <div className={`flex  max-w-6xl justify-between items-center w-full bg-primary-color/20 rounded-large min-h-20 `}>
+                <div className={`flex flex-col gap-2 p-4`}>
                     <h2 className={`text-2xl font-bold `}>Mental Healths</h2>
                     <h3 className={`text-lg font-medium `}>2026</h3>
                 </div>
-                <div>
+                <div className={`flex flex-col gap-2 p-4`}>
                     <h2 className={`text-2xl font-bold `}>Platform</h2>
                     <h3 className={`text-lg font-medium `}>About</h3>
                     <h3 className={`text-lg font-medium `}>Contact</h3>
                 </div>
-                <div>
+                <div className={`flex flex-col gap-2 p-4`}>
                     <h2 className={`text-2xl font-bold `}>Legal</h2>
                     <h3 className={`text-lg font-medium `}>Privacy Policy</h3>
                     <h3 className={`text-lg font-medium `}>Terms of Service</h3>
                 </div>
-                <div>
+                <div className={`flex flex-col gap-2 p-4`}>
                     <h2 className={`text-2xl font-bold `}>Follow Us</h2>
                     <h3 className={`text-lg font-medium `}>Instagram</h3>
                     <h3 className={`text-lg font-medium `}>Facebook</h3>
