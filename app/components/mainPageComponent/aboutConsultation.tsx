7d101fc3fc466f3aa7da01d92deb4ea707310fc8
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-29 23:04:57 +0300

    updated styles

--- app/components/mainPageComponent/aboutConsultation.tsx
+++ app/components/mainPageComponent/aboutConsultation.tsx
@@ -21,15 +21,18 @@ export default function AboutConsultation() {
             <div className={`flex flex-col gap-4 p-6 justify-center items-center text-left`}>
                 <h2 className={`text-3xl font-bold `}>{AboutConsultationContent[locale].TITLE}</h2>
                 <p className={`italic`}>{AboutConsultationContent[locale].DESCRIPTION}</p>
-                <h3 className={`text-2xl font-semibold mt-4`}>{AboutConsultationContent[locale].TITLE_2}</h3>
-                <ul className={`list-disc list-inside font-bold`}>
+                <div className={`flex flex-col gap-4 justify-center items-start bg-primary-color/10 rounded-large p-4 border border-primary-color/30 w-full`}>
+                    <h3 className={`text-2xl font-semibold mt-4`}>{AboutConsultationContent[locale].TITLE_2}</h3>
+                    <ul className={`list-disc list-inside font-bold`}>
                     {AboutConsultationContent[locale].BENEFITS.map((benefit, index) => (
                         <li key={index} className={`flex  items-center gap-2`}>
                             <CheckIcon alt="Check Icon" className={`bg-primary-color/40 rounded-full p-2 w-8 h-8`} />
                             {benefit}
                         </li>
-                    ))}
-                </ul>
+                        ))}
+                    </ul>
+                </div>
+                
                 <Link href={routesAdaptive.consultation.root} className={`mt-4 px-4 py-2 font-bold bg-buttonContainer rounded-large`}>{AboutConsultationContent[locale].SIGN_UP_BUTTON}</Link>
             </div>
 
