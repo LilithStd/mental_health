351c0d84abbdf45fcc0c52df96f2f7abd5ca01e3
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-31 22:37:56 +0300

    added hash tags icon for visualization

--- app/components/shared/hashTags.tsx
+++ app/components/shared/hashTags.tsx
@@ -3,6 +3,7 @@
 import { useLocale } from "@/app/hooks/useLocale";
 import { LocaleType } from "@/app/types/types";
 import { HASH_TAGS } from "@/translate/shared/hashTags";
+import HashTagIcon from "@/public/icons/Tag.svg";
 
 interface HashTagsProps {
     hashTags: string[]
@@ -13,6 +14,11 @@ export default function HashTags({ hashTags }: HashTagsProps) {
   return (
     <div>
         <span className="text-primary-color font-bold">#{HASH_TAGS[locale].hashTags}</span>
+        <div>
+            {hashTags.map((tag, index) => (
+                <span key={index} className="text-primary-color font-bold"><HashTagIcon/>{tag} </span>
+            ))}
+        </div>
     </div>
   )
 }
