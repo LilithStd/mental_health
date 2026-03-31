0ce22372bdc24e3a2e8934e8642caba0f5e8178e
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-31 22:26:53 +0300

    created favorites translate

--- app/components/shared/favorites.tsx
+++ app/components/shared/favorites.tsx
@@ -3,6 +3,7 @@ import IsFavoritesIcon from "@/public/icons/HeartFull.svg"
 import WithOutFavoritesIcon from "@/public/icons/HeartEmpty.svg"
 import { LocaleType } from "@/app/types/types"
 import { useLocale } from "@/app/hooks/useLocale"
+import { FAVORITES } from "@/translate/shared/favorites"
 
 interface FavoritesProps {
     isFavorite: boolean,
@@ -16,7 +17,7 @@ export default  function Favorites({ isFavorite, counterFavorites, callBackIsFav
     return (
         <div className={`flex items-center ${"cursor-pointer"}`} onClick={callBackIsFavorite}>
             {isFavorite ? <IsFavoritesIcon width={24} height={24} /> : <WithOutFavoritesIcon width={24} height={24} />}
-            <span className="ml-2 ">{counterFavorites} {counterFavorites === 1 ? 'Like' : 'Likes'}</span>
+            <span className="ml-2 ">{counterFavorites} {counterFavorites && counterFavorites <= 1 ? FAVORITES[locale].like : FAVORITES[locale].likes}</span>
         </div>
     )
 }
---
+++ translate/shared/favorites.ts
@@ -0,0 +1,14 @@
+export const FAVORITES  = {
+    en: {
+        like: 'Like',
+        likes: 'Likes'
+    },
+    lv: {
+        like: 'Patīk',
+        likes: 'Patīk'
+    },
+    ru: {
+        like: 'Нравится',
+        likes: 'Нравится'
+    } 
+}
