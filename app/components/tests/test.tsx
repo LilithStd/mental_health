7cdebb11fca3c7c23bda33f50cb5c5e7b06f0782
Dmitrii <dmitrii.konevnin@gmail.com>
2026-04-04 20:47:37 +0300

    updated styles

--- app/components/tests/test.tsx
+++ app/components/tests/test.tsx
@@ -82,7 +82,7 @@ export default function Test({ test, testType }: TestProps) {
 
     const fullTestComponent =
 
-        <div className={`bg-primary-color/20 border border-primary-color/30 shadow-md p-4 rounded-large   grid gap-4 grid-cols-2`}>
+        <div className={`bg-primary-color/20 border border-primary-color/30 shadow-md p-4 rounded-large   grid gap-4  grid-cols-[0.5fr_1fr]`}>
             <div className={`flex flex-col  rounded-large  p-4`}>
                 <div className={`flex flex-col mb-4 bg-primary-color/30 rounded-large p-4`}>
                     <h2>{test.title[locale]}</h2>
