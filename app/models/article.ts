1d6d375b1301ebcbc8b93e8af10b35b2aae0c5d0
Dmitrii <dmitrii.konevnin@gmail.com>
2026-04-10 18:09:59 +0300

    updated scheme article

--- app/models/article.ts
+++ app/models/article.ts
@@ -3,25 +3,29 @@ import mongoose, { Schema, Document, Model } from "mongoose";
 import { MultiLanguageText } from "../types/types";
 
 
-export interface IArticle extends Document {
+export interface ArticleTypes extends Document {
   multiLanguage: boolean;
   title: MultiLanguageText | string;
   description?: MultiLanguageText | string;
   content: MultiLanguageText | string;
+  author?: string;
   tags?: string[];
   likes?: number;
   createdAt: Date;
+  updatedAt?: Date;
 }
 
-const ArticleSchema: Schema<IArticle> = new Schema({
+const ArticleSchema: Schema<ArticleTypes> = new Schema({
   multiLanguage: { type: Boolean, default: true },
   title: { en: String, ru: String, lv: String },
+  author: { type: String },
   description: { en: String, ru: String, lv: String },
   content: { en: String, ru: String, lv: String },
   tags: [String],
   likes: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
-export const Article: Model<IArticle> =
-  mongoose.models.Article || mongoose.model<IArticle>("Article", ArticleSchema);
+export const Article: Model<ArticleTypes> =
+  mongoose.models.Article || mongoose.model<ArticleTypes>("Article", ArticleSchema);
