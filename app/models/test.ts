f7c6059f5cde6968c57c97ccd3c4eb7ad83120c5
Dmitrii <dmitrii.konevnin@gmail.com>
2026-04-19 18:54:55 +0300

    updated test model

--- app/models/test.ts
+++ app/models/test.ts
@@ -28,5 +28,42 @@ export interface TestTypes extends Document {
             },
             count: number
         }[]
-    }[]
+    }[],
+    createdAt: Date,
+    updatedAt: Date
+
 }
+
+const TestSchema: Schema<TestTypes> = new Schema({
+    label: { type: String, required: true },
+    title: {
+        en: { type: String, required: true },
+        ru: { type: String, required: true },
+        lv: { type: String, required: true }
+    },
+    content: {
+        en: { type: String, required: true },
+        ru: { type: String, required: true },
+        lv: { type: String, required: true }
+    },
+    questions: [{
+        title: {
+            en: { type: String, required: true },
+            ru: { type: String, required: true },
+            lv: { type: String, required: true }
+        },
+        variants: [{
+            id:{type:String, required:true},
+            title:{
+                en:{type:String, required:true},
+                ru:{type:String, required:true},
+                lv:{type:String, required:true}
+            },
+            count:{type:Number, default:0}
+        }]
+    }],
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+})
+
+export const Test: Model<TestTypes> = mongoose.models.Test || mongoose.model<TestTypes>("Test", TestSchema)
