34412c527b2d50c1e23027f91595201f9a996819
Dmitrii <dmitrii.konevnin@gmail.com>
2026-04-22 21:33:26 +0300

    updated consultation page content

--- translate/consultationPage/consultationPage.ts
+++ translate/consultationPage/consultationPage.ts
@@ -1,3 +1,5 @@
+
+
 export const ConsultationPageContent = {
     en: {
         title: 'Contact Information',
@@ -30,3 +32,33 @@ export const ConsultationPageContent = {
         buttonText: 'Отправить',
     }
 } as const
+
+export const CONSULTATION_TYPE = {
+    MAIN:{
+        title:{
+            en: 'Main method',
+            lv: 'Galvenā metode',
+            ru: 'Основной метод'
+        }
+    },
+    ADDITIONAL:{
+        title:{
+            en: 'Additional method',
+            lv: 'Papildu metode',
+            ru: 'Дополнительный метод'
+
+        },
+        description:{
+            en: 'You can sign up for a consultation with me by following this',
+            lv: 'Jūs varat pierakstīties uz konsultāciju ar mani, sekojot šai ',
+            ru: 'Вы можете записаться на консультацию со мной, перейдя по этой '
+        },
+        linkText:{
+            en: 'link',
+            lv: 'saipei',
+            ru: 'ссылке'
+        }
+    }
+} as const
+
+const LINK_TO_CONSULTATION = "https://medon.lv/arsti/aleksandra-konevnina";
