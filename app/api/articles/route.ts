078d356d8b31847d76eedae7fb5c18e0b7a5c4c3
Dmitrii <dmitrii.konevnin@gmail.com>
2026-04-23 21:28:50 +0300

    updated api for work on cloudinary (need testing)

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -5,6 +5,8 @@ import { Article } from "@/app/models/article";
 import { ArticleType } from "@/app/types/types";
 
 import { createArticle, getAllArticles } from "@/app/service/articleService";
+import cloudinary from "@/app/lib/cloudinary";
+import { buffer } from "stream/consumers";
 
 
 // GET — возвращаем все статьи
@@ -17,11 +19,114 @@ export async function GET() {
 // app/api/articles/route.ts
 
 
+// export async function POST(req: NextRequest) {
+
+//   try {
+
+//     const formData = await req.formData()
+
+//     const title = formData.get("title") as string
+
+//     const file = formData.get("image") as File
+
+//     if (!file) {
+
+//       return NextResponse.json({ error: "No file" }, { status: 400 })
+
+//     }
+
+//     const bytes = await file.arrayBuffer()
+
+//     const buffer = Buffer.from(bytes)
+
+//     // загрузка в Cloudinary
+
+//     const uploadResult = await new Promise((resolve, reject) => {
+
+//       cloudinary.uploader
+
+//         .upload_stream(
+
+//           { folder: "articles" },
+
+//           (error, result) => {
+
+//             if (error) reject(error)
+
+//             else resolve(result)
+
+//           }
+
+//         )
+
+//         .end(buffer)
+
+//     })
+
+//     const imageUrl = (uploadResult as { secure_url: string }).secure_url
+
+//     await connectDB()
+
+//     const article = await Article.create({
+
+//       title,
+
+//       image: imageUrl,
+
+//     })
+
+//     return NextResponse.json(article)
+
+//   } catch (err) {
+
+//     return NextResponse.json({ error: "Upload failed" }, { status: 500 })
+
+//   }
+
+// }
 export async function POST(req: Request) {
   const formData = await req.formData();
 
   const image = formData.get("image") as File;
   console.log('Received form data:', formData);
+
+  
+  // 👉 обработка файла отдельно
+  if (!image || image.size === 0) {
+    return NextResponse.json({ error: "No file" }, { status: 400 })
+  }
+ 
+  const bytes = await image.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+    // сохранить (локально / cloud)
+  
+  interface CloudinaryUploadResult {
+    secure_url: string;
+  }
+
+  const uploadResult = await new Promise<CloudinaryUploadResult>((resolve, reject) => {
+
+      cloudinary.uploader
+
+        .upload_stream(
+
+          { folder: "articles" },
+
+          (error, result) => {
+
+            if (error) reject(error)
+
+            else resolve(result as CloudinaryUploadResult)
+
+          }
+
+        )
+
+        .end(buffer)
+
+  })
+  const imageUrl = image ? uploadResult.secure_url : "";
   const article = {
     multiLanguage: true,
     author: {
@@ -35,6 +140,7 @@ export async function POST(req: Request) {
       ru: (formData.get("titleRu") as string) || "",
       lv: (formData.get("titleLv") as string) || "",
     },
+    image: imageUrl,
     content: {
       en: (formData.get("contentEn") as string) || "",
       ru: (formData.get("contentRu") as string) || "",
@@ -42,14 +148,6 @@ export async function POST(req: Request) {
     },
   };
   console.log('Parsed article data:', article);
-  // 👉 обработка файла отдельно
-  if (image && image.size > 0) {
-    const bytes = await image.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-
-    // сохранить (локально / cloud)
-  }
-
   // await createArticle(article);
 
   return Response.json({ ok: true });
