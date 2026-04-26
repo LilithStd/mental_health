e2683148be00c82970db864c4d72b44e80a84a7d
Dmitrii <dmitrii.konevnin@gmail.com>
2026-04-26 21:22:35 +0300

    updated next config for get images from cloudinary

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,14 @@ const nextConfig: NextConfig = {
 		},
 	},
 	images: {
-    	domains: ["res.cloudinary.com"],
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
 
   },
 };
