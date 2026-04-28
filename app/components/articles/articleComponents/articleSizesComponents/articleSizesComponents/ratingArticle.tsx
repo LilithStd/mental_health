2f0b4a9ea75b2b8c90e7d3f81bededd2cca19f8b
Dmitrii <dmitrii.konevnin@gmail.com>
2026-04-28 18:53:47 +0300

    updated article components

--- app/components/articles/articleComponents/articleSizesComponents/articleSizesComponents/ratingArticle.tsx
+++ app/components/articles/articleComponents/articleSizesComponents/articleSizesComponents/ratingArticle.tsx
@@ -2,18 +2,20 @@ import Favorites from '@/app/components/shared/favorites'
 import HashTags from '@/app/components/shared/hashTags'
 import { SIZE_ELEMENT } from '@/app/globalConsts/globalEnum'
 import React from 'react'
+import RedirectButton from './redirectButton'
+import { ArticleType } from '@/app/types/types'
 
 interface RatingArticleProps {
-    rating: number
+    article:ArticleType
 }
 
-export default function RatingArticle({ rating }: RatingArticleProps) {
+export default function RatingArticle({ article }: RatingArticleProps) {
   return (
                 <div className={`flex items-center justify-between w-full gap-2 shadow-sm rounded-large bg-primary-color/30 border border-primary-color/30`}>
                     <Favorites isFavorite={false} type={SIZE_ELEMENT.PREVIEW} />
                     {/* {tagsComponent(type)} */}
                     <HashTags hashTags={['example', 'sample', 'test']} type={SIZE_ELEMENT.PREVIEW} />
-                    {redirectButtonComponent(type)}
+                    <RedirectButton articleId={article.id} sizeElement={SIZE_ELEMENT.PREVIEW} />
                 </div>
         )
 }
--- app/components/articles/articleComponents/articleSizesComponents/articleSizesComponents/redirectButton.tsx
+++ app/components/articles/articleComponents/articleSizesComponents/articleSizesComponents/redirectButton.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { SIZE_ELEMENT } from "@/app/globalConsts/globalEnum"
+import { routes } from "@/app/helpers/helpersFunctions"
+import { useLocale } from "@/app/hooks/useLocale"
+import { LocaleType } from "@/app/types/types"
+import { BUTTON_READ_FULL_ARTICLE } from "@/translate/mediaPage/mediaPageContent"
+import Link from "next/link"
 
-export default function redirectButton() {
-  return (
-    <div>redirectButton</div>
+
+interface RedirectButtonProps {
+    articleId: string,
+    sizeElement?: SIZE_ELEMENT
+}
+
+export default function RedirectButton({ articleId, sizeElement }: RedirectButtonProps) {
+    const locale = useLocale() as LocaleType
+    const routesAdaptive = routes(locale)
+    return (
+        <div className={`flex p-2 shadow-sm rounded-large bg-primary-color/50 border border-primary-color/30`}>
+            <Link
+                className={`flex justify-center items-center cursor-pointer`}
+                href={routesAdaptive.articles.byId(articleId)}
+            >
+                <span className={`${sizeElement === SIZE_ELEMENT.FULL ? 'text-md' : 'text-xs '}  whitespace-nowrap`}>{BUTTON_READ_FULL_ARTICLE[locale]}</span>
+            </Link>
+        </div>
   )
 }
