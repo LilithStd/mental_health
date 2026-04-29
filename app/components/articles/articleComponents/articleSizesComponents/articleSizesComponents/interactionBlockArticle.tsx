1112814cb5d0319ee326bc1174fc338f7e7795cb
Dmitrii <dmitrii.konevnin@gmail.com>
2026-04-29 21:09:41 +0300

    updated article content

--- app/components/articles/articleComponents/articleSizesComponents/articleSizesComponents/interactionBlockArticle.tsx
+++ app/components/articles/articleComponents/articleSizesComponents/articleSizesComponents/interactionBlockArticle.tsx
@@ -1,7 +1,9 @@
 import { formattedDate } from "@/app/helpers/helpersFunctions"
 import RatingArticle from "./ratingArticle"
-import { ArticleType } from "@/app/types/types"
+import { ArticleType, LocaleType } from "@/app/types/types"
 import { SIZE_ELEMENT } from "@/app/globalConsts/globalEnum"
+import { ArticleContent } from "@/translate/mediaPage/articleContent/articleContent"
+import { useLocale } from "@/app/hooks/useLocale"
 
 interface InteractionBlockArticleProps {
     typeArticle: SIZE_ELEMENT,
@@ -9,12 +11,13 @@ interface InteractionBlockArticleProps {
 }
 
 export default function InteractionBlockArticle({ typeArticle, article }: InteractionBlockArticleProps) {
-  return (
+    const locale = useLocale() as LocaleType
+    return (
         <div className={`flex flex-col  w-full gap-2 items-start mt-2`}>
             <div className={`flex flex-col w-full`}>
                 <RatingArticle article={article} typeArticle={typeArticle}/>                
-                <span className={`text-sm pl-2 pt-2 italic opacity-40`}>Published on: {formattedDate(article.createdAt)}</span>
+                <span className={`text-sm pl-2 pt-2 italic opacity-40`}>{ArticleContent[locale].publishedOn}: {formattedDate(article.createdAt)}</span>
             </div>
         </div>
-  )
+    )
 }
