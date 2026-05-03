c6e857edbaead4c5d073bf9c9a321842b668878c
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-03 20:19:47 +0300

    updated current user styles

--- app/[locale]/(protected)/user/[id]/page.tsx
+++ app/[locale]/(protected)/user/[id]/page.tsx
@@ -6,12 +6,15 @@ export default async function Users({
 }: {
     params: { id: string }
 }) {
-    const { id } = await params
-    return <div className={`flex flex-col indents-main-container rounded-medium flex-1 items-center`}>
-        <div className={`flex flex-col p-4 w-full flex-1  max-w-6xl  rounded-medium bg-mainContainer `}>
+
+    return  (
+        <div className={`flex flex-col indents-main-container flex-1 items-center`}>
+        <div className={`flex flex-col p-4 w-full flex-1  max-w-6xl  rounded-large bg-primary-color/30 items-center justify-start gap-4`}>
             <h1>User Page</h1>
-            <CurrentUser id={id} />
+            <CurrentUser />
         </div>
 
     </div>
+    )
+    
 }
