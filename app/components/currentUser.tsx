cf4a3a49a877e91bab5894ad1013fa85a003ed87
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-03 20:16:59 +0300

    updated current user component and small refactoring

--- app/components/currentUser.tsx
+++ app/components/currentUser.tsx
@@ -1,13 +1,16 @@
+'use client';
 import { getCurrentUser } from "@/app/lib/auth";
 import { redirect } from "next/navigation";
+import { useUser } from "../authClientWrapper";
+import { UserAuthType } from "../types/types";
 
 interface UserProps {
-    id: string
+    currentAuthUser: UserAuthType
 }
 
-export default async function CurrentUser({ id }: UserProps) {
-    const currentAuthUser = await getCurrentUser();
-
+export default  function CurrentUser() {
+    const currentAuthUser = useUser() as UserAuthType;
+    console.log('currentAuthUser in CurrentUser component', currentAuthUser)
     // если нет пользователя — редирект
     if (!currentAuthUser) {
         redirect("/");
