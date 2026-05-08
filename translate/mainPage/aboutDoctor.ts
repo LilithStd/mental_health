e2bf4fa70b74646afbb980ca869c8c557e53e80a
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-08 20:36:57 +0300

    updated content doctor block in main mage

--- translate/mainPage/aboutDoctor.ts
+++ translate/mainPage/aboutDoctor.ts
@@ -1,32 +1,32 @@
 export const AboutDoctorContent = {
     en:{
-        TITLE: 'About the Doctor',
-        TITLE_2: 'My Background and Experience:',
-        TITLE_3: 'My Approach to Treatment:',
-        TITLE_4: 'My Philosophy on Mental Health:',
-        DESCRIPTION: 'Hi! My name is Dr. John Smith!',
-        DESCRIPTION_2: 'I am a licensed psychiatrist with over 10 years of experience in the field of mental health. I have worked with patients of all ages and backgrounds, and I specialize in treating anxiety, depression, and trauma-related disorders. I am committed to providing compassionate and evidence-based care to my patients.',
-        DESCRIPTION_3: 'I believe in a holistic approach to mental health care, which includes a combination of therapy, medication management, and lifestyle modifications. I work closely with my patients to develop personalized treatment plans that address their unique needs and goals.',
-        DESCRIPTION_4: 'I believe that mental health is just as important as physical health, and that seeking help for mental health concerns is a sign of strength. I am passionate about reducing the stigma surrounding mental illness and promoting mental wellness for all individuals.'
+        TITLE: '',
+        TITLE_2: '',
+        TITLE_3: '',
+        TITLE_4: '',
+        DESCRIPTION: 'Hi! My name is Aleksandra!',
+        DESCRIPTION_2: 'Since 2011, I started working at the National Mental Health Centre (NPVC) as a resident physician in psychiatry. After becoming certified as a psychiatrist, I began working in my profession and additionally trained in the subspecialty of forensic psychiatry expert.',
+        DESCRIPTION_3: 'In addition to my medical work, I teach psychiatry and forensic psychiatry at the University of Latvia and the State Police College. I conduct various educational seminars on current issues in dementia and other relevant topics in psychiatry.',
+        DESCRIPTION_4: 'As a psychiatrist, I work with various patient groups, offering diagnosis of mental disorders, appropriate treatment, education of patients and their relatives, and preparation of referrals to the State Medical Commission for Health and Work Capacity Assessment. I perform dementia assessment testing and provide practical recommendations for training and maintaining cognitive (mental) functions in dementia patients.'
     },
     lv:{
-        TITLE: 'Par ārstu',
-        TITLE_2: 'Mana pieredze un izglītība:',
-        TITLE_3: 'Mans pieejas veids ārstēšanai:',
-        TITLE_4: 'Mana filozofija par garīgo veselību:',
-        DESCRIPTION: 'Sveiki! Mani sauc Dr. Džons Smits!',
-        DESCRIPTION_2: 'Esmu licencēts psihiatrs ar vairāk nekā 10 gadu pieredzi garīgās veselības jomā. Esmu strādājis ar pacientiem no visām vecuma grupām un foniem, un es specializējos trauksmes, depresijas un traumatiskiem traucējumiem ārstēšanā. Esmu apņēmies sniegt līdzjūtīgu un uz pierādījumiem balstītu aprūpi saviem pacientiem.',
-        DESCRIPTION_3: 'Es ticu holistiskai pieejai garīgās veselības aprūpei, kas ietver terapijas, medikamentu pārvaldības un dzīvesveida modifikāciju kombināciju. Es cieši sadarbojos ar saviem pacientiem, lai izstrādātu personalizētus ārstēšanas plānus, kas atbilst viņu unikālajām vajadzībām un mērķiem.',
-        DESCRIPTION_4: 'Es ticu, ka garīgā veselība ir tikpat svarīga kā fiziskā veselība, un ka meklēt palīdzību garīgās veselības problēmu gadījumā ir spēka pazīme. Es esmu kaislīgs par garīgās slimības stigmas mazināšanu un garīgās labklājības veicināšanu visiem indivīdiem.'  
+        TITLE: '',
+        TITLE_2: '',
+        TITLE_3: '',
+        TITLE_4: '',
+        DESCRIPTION: 'Sveiki! Mani sauc Aleksandra!',
+        DESCRIPTION_2: 'No 2011.gada sāku strādāt Nacionālajā psihiskās veselības centrā (NPVC) kā ārste-rezidente psihiatra specialitātē. Sertificējoties kā psihiatre, uzsāku strādāt pēc profesijas un papildus apmācījos tiesu psihiatrijas ekspertes apakšspecialitāte.',
+        DESCRIPTION_3: 'Papildus ārsta darbam, pasniedzu psihiatriju, tiesu psihiatriju Latvijas universitātē, Valsts policijas koledžā. Vadu dažādus izglītojošus seminārus par demences aktuāliem jautājumiem un citām aktuālām psihiatrijas tēmām.',
+        DESCRIPTION_4: 'Kā psihiatre, strādāju ar dažādām pacientu grupām, piedāvājot psihisko traucējumu diagnosticēšanu, atbilstošo ārstēšanu, pacientu un piederīgo izglītošanu, nosūtījumu uz Veselības un darbspēju ekspertīzes ārstu valsts komisiju sagatavošanu. Veicu demences noteikšanas testēšanu un sniedzu praktiskas rekomendācijas kognitīvo (prāta) funkciju trenēšanai, uzturēšanai demences pacientiem.'  
     },
     ru:{
-        TITLE: 'О враче',
-        TITLE_2: 'Мой опыт и образование:',
-        TITLE_3: 'Мой подход к лечению:',
-        TITLE_4: 'Моя философия по поводу психического здоровья:',
-        DESCRIPTION: 'Привет! Меня зовут доктор Джон Смит!',
-        DESCRIPTION_2: 'Я лицензированный психиатр с более чем 10-летним опытом работы в области психического здоровья. Я работал с пациентами всех возрастов и происхождения, и я специализируюсь на лечении тревоги, депрессии и травматических расстройств. Я стремлюсь предоставлять сострадательную и основанную на доказательствах помощь своим пациентам.',
-        DESCRIPTION_3: 'Я верю в целостный подход к уходу за психическим здоровьем, который включает в себя комбинацию терапии, управления медикаментами и изменений образа жизни. Я тесно сотрудничаю со своими пациентами, чтобы разработать персонализированные планы лечения, которые соответствуют их уникальным потребностям и целям.',
-        DESCRIPTION_4: 'Я считаю, что психическое здоровье так же важно, как и физическое здоровье, и что обращение за помощью при проблемах с психическим здоровьем является признаком силы. Я страстно стремлюсь снизить стигму, связанную с психическими заболеваниями, и продвигать психическое благополучие для всех людей.'
+        TITLE: '',
+        TITLE_2: '',
+        TITLE_3: '',
+        TITLE_4: '',
+        DESCRIPTION: 'Привет! Меня зовут Aлександра!',
+        DESCRIPTION_2: 'С 2011 года начала работать в Национальном центре психического здоровья (НЦПЗ) в качестве врача-резидента по специальности психиатрия. После получения сертификата психиатра начала работать по профессии и дополнительно прошла обучение по подспециальности эксперта судебной психиатрии.',
+        DESCRIPTION_3: 'Помимо врачебной работы, преподаю психиатрию, судебную психиатрию в Латвийском университете, Колледже государственной полиции. Провожу различные образовательные семинары по актуальным вопросам деменции и другим актуальным темам психиатрии.',
+        DESCRIPTION_4: 'Как психиатр, работаю с различными группами пациентов, предлагая диагностику психических расстройств, соответствующее лечение, обучение пациентов и их близких, подготовку направлений в Государственную комиссию врачей по экспертизе здоровья и трудоспособности. Провожу тестирование для определения деменции и даю практические рекомендации по тренировке, поддержанию когнитивных (умственных) функций у пациентов с деменцией.'
     }
 } as const
