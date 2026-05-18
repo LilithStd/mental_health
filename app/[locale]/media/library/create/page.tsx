edfa93b735cb75a3bf785d5f806ca6898abff4a8
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-18 18:02:42 +0300

    create form in create element library

--- app/[locale]/media/library/create/page.tsx
+++ app/[locale]/media/library/create/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
 
 export default function CreateElementLibraryPage() {
   return (
     <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
-            <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 pt-4`}>
+            <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
+            <form>
+                <input type="text" placeholder="Title" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
+                <textarea placeholder="Content" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} rows={10}></textarea>
+                <button type="submit" className={`px-4 py-2 bg-buttonContainer rounded-large font-bold`}>Create</button>
+                
+            </form>
             </div>
     </div>
   )
