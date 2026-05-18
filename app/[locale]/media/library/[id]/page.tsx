db3d2e4d546b14fe4bd7c39f76b63889c7b44cb7
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-18 16:47:25 +0300

    created library component and updated library main component

---
+++ app/[locale]/media/library/[id]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function CurrentElementLibraryPage({ params }: { params: { id: string } }) {
+    
+  return (
+    <div>Current Element Library Page</div>
+  )
+}
--- app/[locale]/media/library/page.tsx
+++ app/[locale]/media/library/page.tsx
@@ -16,17 +16,13 @@ export default async function LibraryPage() {
         <div className={`flex w-full justify-center gap-4 mb-4`}>
             <span>{LibraryContent[locale].title}</span>
         </div>
-            <div className={`flex flex-col gap-4`}>
+            <div className={`flex flex-col gap-4 p-4`}>
                 {library.map(item => (
-                    <div key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
-                        <h3 className={`text-2xl font-bold`}>{item.title[locale]}</h3>
-                        <p>{item.description?.[locale]}</p>
-                        <div className={`flex gap-2 flex-wrap`}>
-                            {item.hashTags.map((tag, index) => (
-                                <span key={index} className={`px-2 py-1 bg-secondary-color/50 rounded-full text-sm`}>#{tag}</span>
-                            ))}
-                        </div>
-                    </div>
+                    <details key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
+                        <summary className={`text-2xl font-bold cursor-pointer`}>{item.title[locale]}</summary>
+                        <p>{item.content[locale]}</p>
+                    </details>
+                    
                 ))}
             </div>
         </div>
