9d93d7ff9ddf9c907c4b62ef46b9de06639b690d
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-18 17:37:28 +0300

    updated create element library component

--- app/[locale]/media/library/create/page.tsx
+++ app/[locale]/media/library/create/page.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function CreateElementLibraryPage() {
   return (
-    <div>page</div>
+    <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
+            <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 pt-4`}>
+            </div>
+    </div>
   )
 }
--- app/components/library/createElementLibrary.tsx
+++ app/components/library/createElementLibrary.tsx
@@ -1,12 +1,17 @@
 'use client'
 import { useUser } from "@/app/authClientWrapper"
+import { routes } from "@/app/helpers/helpersFunctions";
+import { useLocale } from "@/app/hooks/useLocale";
+import { LocaleType } from "@/app/types/types";
 import Link from "next/link";
 
 
 export default function CreateElementLibrary() {
+    const locale = useLocale() as LocaleType
+    const routesAdaptive = routes(locale)
     const currentUser = useUser();
     if (!currentUser) return null;
   return (
-    <Link href="/create-element-library">CreateElementLibrary</Link>
+    <Link className={`p-2 bg-primary-color/50 rounded-large`} href={routesAdaptive.library.create()}>CreateElementLibrary</Link>
   )
 }
