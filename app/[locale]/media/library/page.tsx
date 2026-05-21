734cdc597cb57451bf6b6b518aba851eb5f364dc
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-21 20:01:36 +0300

    updated library component

--- app/[locale]/media/library/page.tsx
+++ app/[locale]/media/library/page.tsx
@@ -1,5 +1,7 @@
 
 import CreateElementLibrary from "@/app/components/library/createElementLibrary"
+import HashTags from "@/app/components/shared/hashTags"
+import { SIZE_ELEMENT } from "@/app/globalConsts/globalEnum"
 import { getLocale } from "@/app/hooks/server/getLocale"
 import { getAllLibrary } from "@/app/service/libraryService"
 import { LocaleType } from "@/app/types/types"
@@ -14,18 +16,22 @@ export default async function LibraryPage() {
   return (
     <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
         <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 pt-4`}>
-        <div>
-            <CreateElementLibrary />
-        </div>
+       
         <div className={`flex w-full justify-center gap-4 mb-4`}>
             <span>{LibraryContent[locale].title}</span>
         </div>
-            <div className={`flex flex-col gap-4 p-4`}>
+         <div className={`flex w-full justify-center pr-4 mb-4`}>
+            <CreateElementLibrary />
+        </div>
+        <div className={`flex flex-col gap-4 p-4`}>
                 {library.map(item => (
                     <div key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
-                        <span className={`text-sm italic`}>{LibraryContent[locale].type}:{item.type}</span>
+                        <span className={`text-2xl`}>{item.title[locale]}</span>                        
+                        <span className={`text-sm italic`}>{LibraryContent[locale].type}:{item.type}</span>   
+                        <HashTags hashTags={item.hashTags.length > 0 ? item.hashTags : ['depression']} type={SIZE_ELEMENT.SMALL} />
                             <details key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
-                            <summary className={`text-2xl font-bold cursor-pointer`}>{item.title[locale]}</summary>
+                            
+                            <summary className={`font-bold cursor-pointer`}>Description</summary>
                             <p>{item.content[locale]}</p>
                          </details>
                     </div>
