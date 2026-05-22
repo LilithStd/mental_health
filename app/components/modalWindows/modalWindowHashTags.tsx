894ede10e785bca28074cb4a5ed671c3c7fe7760
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-22 19:59:03 +0300

    updated hash tags component

--- app/components/modalWindows/modalWindowHashTags.tsx
+++ app/components/modalWindows/modalWindowHashTags.tsx
@@ -1,9 +1,18 @@
-
+import { useLocale } from "@/app/hooks/useLocale";
+import { LocaleType } from "@/app/types/types";
+import { HASH_TAGS } from "@/translate/shared/hashTags";
+import AddTagsIcon from "@/public/icons/PlusCircle.svg";
 
 export default function ModalWindowHashTags() {
+  const locale = useLocale() as LocaleType;
+
   return (
-    <div className={`absolute flex backdrop-blur-xl  shadow-md justify-between items-center rounded-large  bg-primary-color/10 border border-primary-color/30 p-4 left-2 min-h-30 top-18 w-[400px] z-50`}>
-        <h2 className="text-lg font-bold  ">Modal Window Hash Tags</h2>
+    <div className={`absolute flex backdrop-blur-xl  shadow-md  rounded-large  bg-primary-color/10 border border-primary-color/30 p-4 left-2 min-h-30 top-18 w-[400px] z-50`}>
+        <h2 className="text-lg font-bold">#{HASH_TAGS[locale].hashTags}</h2>
+        <div className="flex flex-col gap-2 mt-4">
+            <span>{HASH_TAGS[locale].availableHashTags}</span>
+            <AddTagsIcon className={`w-6 h-6 cursor-pointer hover:scale-110 transition-transform`} fill={'green'} onClick={() => {}} />
+        </div>
     </div>
   )
 }
--- translate/shared/hashTags.ts
+++ translate/shared/hashTags.ts
@@ -2,12 +2,21 @@
 
 export const HASH_TAGS = {
     en: {
-        hashTags: 'Hash tags'
+        hashTags: 'Hash tags',
+        availableHashTags: 'Available hash tags',
+        addHashTag: 'Add hash tag',
+        deleteHashTag: 'Delete hash tag',
     },
     lv: {
-        hashTags: 'Hashtagi'
+        hashTags: 'Hashtagi',
+        availableHashTags: 'Pieejamie hashtagi',
+        addHashTag: 'Pievienot hashtag',
+        deleteHashTag: 'Dzēst hashtag',
     },
     ru: {
-        hashTags: 'Хештеги'
-    },
-} as const 
+        hashTags: 'Хештеги',
+        availableHashTags: 'Доступные хештеги',
+        addHashTag: 'Добавить хештег',
+        deleteHashTag: 'Удалить хештег',
+    }
+} as const
