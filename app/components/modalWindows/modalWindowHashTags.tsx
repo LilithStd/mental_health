cfd2cd4a6868ac1d3db6768cccffccccb5e963a2
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-22 19:28:49 +0300

    updated modal hashtags window

--- app/components/modalWindows/modalWindowHashTags.tsx
+++ app/components/modalWindows/modalWindowHashTags.tsx
@@ -2,8 +2,8 @@
 
 export default function ModalWindowHashTags() {
   return (
-    <div className={`absolute flex flex-col gap-4 bg-primary-color/30 border border-primary-color/30 p-2 rounded-large items-center justify-center`}>
-        <h2>Modal Window Hash Tags</h2>
+    <div className={`absolute flex backdrop-blur-md  shadow-md justify-between items-center rounded-large  bg-primary-color/20 border border-primary-color/30 p-4 left-8 min-h-30 top-64 z-10`}>
+        <h2 className="text-lg font-bold  ">Modal Window Hash Tags</h2>
     </div>
   )
 }
