633fa6e5e9d4da07613e60ae1c2bc5c6196fbcc1
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-22 19:11:03 +0300

    updated hashtags component

--- app/components/modalWindows/modalWindowHashTags.tsx
+++ app/components/modalWindows/modalWindowHashTags.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+
 
 export default function ModalWindowHashTags() {
   return (
-    <div>ModalWindowHashTags</div>
+    <div className={`absolute flex flex-col gap-4 bg-primary-color/30 border border-primary-color/30 p-2 rounded-large items-center justify-center`}>
+        <h2>Modal Window Hash Tags</h2>
+    </div>
   )
 }
--- app/components/shared/createComponents/hashTagAdd.tsx
+++ app/components/shared/createComponents/hashTagAdd.tsx
@@ -1,14 +1,24 @@
-import { getLocale } from "@/app/hooks/server/getLocale";
+'use client'
+import { useLocale } from "@/app/hooks/useLocale";
 import { LocaleType } from "@/app/types/types";
 import AddTagsIcon from "@/public/icons/PlusCircle.svg";
 import { CreateArticleContent } from "@/translate/mediaPage/articleContent/articleContent";
+import { useState } from "react";
+import ModalWindowMain from "../../modalWindowMain";
+import ModalWindowHashTags from "../../modalWindows/modalWindowHashTags";
 
-export default async function HashTagAdd() {
-    const locale = await getLocale() as LocaleType;
-  return (
-    <div className={`flex w-full h-full flex-col gap-4 bg-primary-color/30 border border-primary-color/30 p-2 rounded-large`}>
-        <span>{CreateArticleContent[locale].tags}</span>
-        <AddTagsIcon className={`w-6 h-6 cursor-pointer hover:scale-110 transition-transform`} fill={'green'} />
-    </div>
+export default  function HashTagAdd() {
+    const locale =  useLocale() as LocaleType;
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const handleCloseModal = () => {
+        setIsOpenModal(false);
+    }
+    return (
+        <div className={`flex w-full h-full flex-col gap-4 bg-primary-color/30 border border-primary-color/30 p-2 rounded-large`}>
+            <span>{CreateArticleContent[locale].tags}</span>
+            <AddTagsIcon className={`w-6 h-6 cursor-pointer hover:scale-110 transition-transform`} fill={'green'} onClick={() => setIsOpenModal(!isOpenModal)} />
+            {isOpenModal && <ModalWindowHashTags />}
+                
+        </div>
   )
 }
