0e0528a74e3388c8550f254b315a73a3d521413d
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-22 20:06:19 +0300

    updated hash tags modal window

--- app/components/modalWindows/modalWindowHashTags.tsx
+++ app/components/modalWindows/modalWindowHashTags.tsx
@@ -7,11 +7,19 @@ export default function ModalWindowHashTags() {
   const locale = useLocale() as LocaleType;
 
   return (
-    <div className={`absolute flex backdrop-blur-xl  shadow-md  rounded-large  bg-primary-color/10 border border-primary-color/30 p-4 left-2 min-h-30 top-18 w-[400px] z-50`}>
+    <div className={`absolute flex flex-col backdrop-blur-xl  shadow-md  rounded-large  bg-primary-color/10 border border-primary-color/30 p-4 left-2 min-h-30 top-18 w-[400px] z-50`}>
         <h2 className="text-lg font-bold">#{HASH_TAGS[locale].hashTags}</h2>
-        <div className="flex flex-col gap-2 mt-4">
-            <span>{HASH_TAGS[locale].availableHashTags}</span>
-            <AddTagsIcon className={`w-6 h-6 cursor-pointer hover:scale-110 transition-transform`} fill={'green'} onClick={() => {}} />
+        <div className="flex gap-2 mt-4">
+            <div className={`flex items-center gap-2 bg-primary-color/30 border border-primary-color/40 p-2 rounded-large`}>
+              <span className={`text-sm`}>{HASH_TAGS[locale].availableHashTags}</span>
+            </div>          
+            <div className={`flex items-center gap-2 bg-primary-color/30 border border-primary-color/40 p-2 rounded-large`}>
+              <span className={`text-sm`}>{HASH_TAGS[locale].addHashTag}</span>
+                <AddTagsIcon className={`w-6 h-6 cursor-pointer hover:scale-110 transition-transform`} fill={'green'} onClick={() => {}} />
+            </div>
+            <div className={`flex items-center gap-2 bg-primary-color/30 border border-primary-color/40 p-2 rounded-large`}>
+              <span className={`text-sm`}>{HASH_TAGS[locale].deleteHashTag}</span>
+            </div>
         </div>
     </div>
   )
