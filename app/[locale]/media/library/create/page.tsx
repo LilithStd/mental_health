13adee87d341159301207858a827254c8d5ad600
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-28 14:04:21 +0300

    updated create library element component

--- app/[locale]/media/library/create/page.tsx
+++ app/[locale]/media/library/create/page.tsx
@@ -12,12 +12,12 @@ export default async function CreateElementLibraryPage() {
     <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
             <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg  border border-primary-color/30 p-4`}>
             <form>
-                <input name="title" type="text" placeholder="Title" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
-                <input name="title" type="text" placeholder="Title" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
-                <input name="title" type="text" placeholder="Title" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
-                <input name="description" type="text" placeholder="Description" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
-                <input name="description" type="text" placeholder="Description" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
-                <input name="description" type="text" placeholder="Description" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
+                <input name="titleen" type="text" placeholder="Title" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
+                <input name="titlelv" type="text" placeholder="Title" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
+                <input name="titleru" type="text" placeholder="Title" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
+                <input name="descriptionen" type="text" placeholder="Description" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
+                <input name="descriptionlv" type="text" placeholder="Description" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
+                <input name="descriptionru" type="text" placeholder="Description" className={`w-full p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
                 <select name="locale" className={`w-1/10 p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`}>
                     {LibraryGroupElementType.map(type => (
                         <option key={type.type} value={type.type}>{type[locale]}</option>
--- translate/mediaPage/libraryContent/libraryContent.ts
+++ translate/mediaPage/libraryContent/libraryContent.ts
@@ -4,14 +4,44 @@ export const LibraryContent = {
     en:{
         title:'Library',
         type: 'Type',
+        createElement:{
+            title: 'Title',
+    
+            description: 'Description',
+            content: 'Content',
+            hashTags: 'Hash tags',
+            image: 'Image',
+            saveButton: 'Save',
+            cancelButton: 'Cancel',
+        }
     },
     lv:{
         title:'Bibliotēka',
         type: 'Tips',
+        createElement:{
+            title: 'Nosaukums',
+    
+            description: 'Apraksts',
+            content: 'Saturs',
+            hashTags: 'Hashtagi',
+            image: 'Attēls',
+            saveButton: 'Saglabāt',
+            cancelButton: 'Atcelt',
+        }
     },
     ru:{
         title:'Библиотека',
         type: 'Тип',
+        createElement:{
+            title: 'Название',
+    
+            description: 'Описание',
+            content: 'Содержание',
+            hashTags: 'Хештеги',
+            image: 'Изображение',
+            saveButton: 'Сохранить',
+            cancelButton: 'Отмена',
+        }
     }
 }
 
