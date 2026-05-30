91d21ccb34c3e4d208f393bed3cdd60e10e92e4e
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-30 18:33:16 +0300

    updated library page

--- app/[locale]/media/library/page.tsx
+++ app/[locale]/media/library/page.tsx
@@ -1,8 +1,10 @@
 
 import CreateElementLibrary from "@/app/components/library/createElementLibrary"
+import ReturnButton from "@/app/components/returnButton"
 import HashTags from "@/app/components/shared/hashTags"
 import Search from "@/app/components/shared/search"
 import { SIZE_ELEMENT } from "@/app/globalConsts/globalEnum"
+import { routes } from "@/app/helpers/helpersFunctions"
 import { getLocale } from "@/app/hooks/server/getLocale"
 import { getAllLibrary } from "@/app/service/libraryService"
 import { LocaleType } from "@/app/types/types"
@@ -13,36 +15,39 @@ import { LibraryContent } from "@/translate/mediaPage/libraryContent/libraryCont
 export default async function LibraryPage() {
     const locale = await getLocale() as LocaleType
     const library = await getAllLibrary()
+    const routesAdaptive = routes(locale)
 
   return (
     <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
-        <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 pt-4`}>
-       
-        <div className={`flex w-full justify-center gap-4 mb-4`}>
-            <span>{LibraryContent[locale].title}</span>
-        </div>
-        <div className={`flex w-full justify-center gap-4 mb-4`}>
-           <input type="text" placeholder={`Search`} className={` p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
-        </div>
-         <div className={`flex w-full justify-center pr-4 mb-4`}>
-            <CreateElementLibrary />
-        </div>
-        <div className={`flex flex-col gap-4 p-4`}>
-                {library.map(item => (
-                    <div key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
-                        <span className={`text-2xl`}>{item.title[locale]}</span>                        
-                        <span className={`text-sm italic`}>{LibraryContent[locale].type}:{item.type}</span>   
-                        <HashTags hashTags={item.hashTags.length > 0 ? item.hashTags : ['depression']} type={SIZE_ELEMENT.SMALL} />
-                            <details key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
-                            
-                            <summary className={`font-bold cursor-pointer`}>Description</summary>
-                            <p>{item.content[locale]}</p>
-                         </details>
-                    </div>
-           
-                    
-                ))}
+        <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
+            <div className={`flex w-full justify-start mb-4`}>
+                <ReturnButton pathToReturn={routesAdaptive.media.root} />
             </div>
+            <div className={`flex w-full justify-center gap-4 mb-4`}>
+                <span>{LibraryContent[locale].title}</span>
+            </div>
+            <div className={`flex w-full justify-center gap-4 mb-4`}>
+            <input type="text" placeholder={`Search`} className={` p-2 mb-4 rounded-large bg-primary-color/50 border border-primary-color/30`} />
+            </div>
+            <div className={`flex w-full justify-center pr-4 mb-4`}>
+                <CreateElementLibrary />
+            </div>
+            <div className={`flex flex-col gap-4 `}>
+                    {library.map(item => (
+                        <div key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
+                            <span className={`text-2xl`}>{item.title[locale]}</span>                        
+                            <span className={`text-sm italic`}>{LibraryContent[locale].type}:{item.type}</span>   
+                            <HashTags hashTags={item.hashTags.length > 0 ? item.hashTags : ['depression']} type={SIZE_ELEMENT.SMALL} />
+                                <details key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
+                                
+                                <summary className={`font-bold cursor-pointer`}>Description</summary>
+                                <p>{item.content[locale]}</p>
+                            </details>
+                        </div>
+            
+                        
+                    ))}
+                </div>
         </div>
     </div>
   )
