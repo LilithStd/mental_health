99dd5de08fc66e2d6de574374bb30d52e6bf961d
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-30 19:25:44 +0300

    updated library by id page (now only temp data for testing)

--- app/[locale]/media/library/[id]/page.tsx
+++ app/[locale]/media/library/[id]/page.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 export default function CurrentElementLibraryPage({ params }: { params: { id: string } }) {
     
   return (
-    <div>Current Element Library Page</div>
+        <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
+        <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
+        </div>
+    </div>
   )
 }
