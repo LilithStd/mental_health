a3e9dc4019e1a99225727effef51de8465978419
Dmitrii <dmitrii.konevnin@gmail.com>
2026-05-31 20:48:25 +0300

    updated library by id component

--- app/[locale]/media/library/[id]/page.tsx
+++ app/[locale]/media/library/[id]/page.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
 
-export default function CurrentElementLibraryPage({ params }: { params: { id: string } }) {
-    
+
+export default async function CurrentElementLibraryPage({ params }: { params: { id: string } }) {
+  const { id } = await params;
+  console.log('CurrentElementLibraryPage id:', id);
   return (
         <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
         <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
