b4c4e40c7fc23a7554595bdbb421361ae703ae02
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-03 23:07:25 +0300

    updated librarry element component

--- app/components/library/libraryElement.tsx
+++ app/components/library/libraryElement.tsx
@@ -13,25 +13,30 @@ interface LibraryElementProps {
 export default function LibraryElement({ id, type }: LibraryElementProps) {
 const locale = useLocale() as LocaleType
 const [libraryElements, setLibraryElements] = useState<LibraryType[]>([]);
-
+console.log('LibraryElement props:', { id, type });
 
 useEffect(() => {
     console.log('LibraryElement id:', id);
-    const fetchLibraryElement = () => {
-        fetch(`/api/library/${id}`)
-            .then(response => response.json())
-            .then((data: LibraryType) => {
-                setLibraryElements(prevElements => [...prevElements, data]);
-            })
-            .catch(error => {
-                console.error('Error fetching library element:', error);
-            });
-    };
+    // const fetchLibraryElement = () => {
 
-    if (id) {
-        fetchLibraryElement();
-    }
-}, [id]);
+    //         id.map((elementId) => {
+    //             fetch(`/api/library?id=${elementId}`)
+    //                 .then(response => response.json())
+    //                 .then(data => {
+    //                     console.log('Fetched library element:', data);
+    //                     setLibraryElements(prevElements => [...prevElements, data]);
+    //                 })
+    //                 .catch(error => {
+    //                     console.error('Error fetching library element:', error);
+    //                 });
+    //         })
+    //     }
+    
+
+    // if (id) {
+    //     fetchLibraryElement();
+    // }
+}, [id, type]);
 
 return (
     <div>
