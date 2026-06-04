625cb2b0a618b620dca845fbf1f660f492e21232
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-04 20:30:06 +0300

    updated groupe library element page

--- app/[locale]/media/library/group/page.tsx
+++ app/[locale]/media/library/group/page.tsx
@@ -1,12 +1,20 @@
 import LibraryElement from "@/app/components/library/libraryElement";
 import { LIBRARY_TYPE } from "@/app/globalConsts/globalEnum";
 
+interface GroupPageProps {
+  searchParams: Promise<{
+    ids?: string;
+  }>;
+}
 
-export default function GroupPage() {
+export default async function GroupPage({ searchParams }: GroupPageProps) {
+  const params = await searchParams;
+  const ids = params.ids
+
   return (
     <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
         <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
-            <LibraryElement id={['1', '2']} type={LIBRARY_TYPE.GROUP} />
+            <LibraryElement id={ids} type={LIBRARY_TYPE.GROUP} />
         </div>
     </div>
   )
