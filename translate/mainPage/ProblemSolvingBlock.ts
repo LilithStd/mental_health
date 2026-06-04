b2264b114ecc538168b49767e8dbbc8a2c47b496
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-04 10:26:14 +0300

    change problem solving block content ( before -> id:string - after->id string[], for correctly view in library component

--- translate/mainPage/ProblemSolvingBlock.ts
+++ translate/mainPage/ProblemSolvingBlock.ts
@@ -3,49 +3,49 @@ export const ProblemSolvingBlockContent = {
         title:'What problems do i work with and solve?',
         problems:[
             {
-                id: 'anxiety-and-stress',
+                id: ['anxiety','stress'],
                 problem:'Anxiety and Stress',
                 color:'bg-green-300',
                 description:'Anxiety and stress are common mental health issues that can affect anyone. They can manifest as excessive worry, restlessness, difficulty concentrating, and physical symptoms like a racing heart or muscle tension. Managing anxiety and stress often involves a combination of lifestyle changes, therapy, and sometimes medication. Techniques such as mindfulness, relaxation exercises, and cognitive-behavioral therapy (CBT) can be effective in reducing symptoms and improving overall well-being.'
             },
             {
-                id: 'depression-and-mood-disorders',
+                id: ['depression','mood-disorders'],
                 problem:'Depression and Mood Disorders',
                 color:'bg-amber-400',
                 description:'Depression and mood disorders are characterized by persistent feelings of sadness, hopelessness, and a lack of interest or pleasure in activities. They can also include changes in appetite, sleep disturbances, and difficulty concentrating. Treatment for depression often involves a combination of psychotherapy, medication, and lifestyle changes. Cognitive-behavioral therapy (CBT) and interpersonal therapy (IPT) are commonly used therapeutic approaches that can help individuals manage their symptoms and improve their quality of life.'
             },
             {
-                id: 'relationship-issues',
+                id: ['relationship-issues'],
                 problem:'Relationship Issues',
                 color:'bg-red-200',
                 description:'Relationship issues can arise in various forms, including communication problems, conflicts, trust issues, and difficulties with intimacy. These issues can occur in romantic relationships, friendships, or family dynamics. Couples therapy or family therapy can be beneficial in addressing relationship issues by improving communication skills, fostering empathy, and helping individuals understand each others perspectives. Additionally, individual therapy can also be helpful for those experiencing relationship difficulties, as it can provide a safe space to explore personal feelings and develop coping strategies.'
             },
             {
-                id: 'self-esteem-and-confidence',
+                id: ['self-esteem','confidence'],
                 problem:'Self-esteem and Confidence',
                 color:'bg-purple-300',
                 description:'Low self-esteem and confidence can impact various aspects of life, including relationships, work, and overall well-being. Building self-esteem often involves challenging negative self-talk, setting realistic goals, and celebrating achievements. Therapy can also be beneficial in addressing underlying issues that contribute to low self-esteem, such as past trauma or negative experiences. Techniques such as cognitive-behavioral therapy (CBT) can help individuals identify and change negative thought patterns, while also developing healthier coping strategies.'
             },
             {
-                id: 'grief-and-loss',
+                id: ['grief','loss'],
                 problem:'Grief and Loss',
                 color:'bg-blue-300',
                 description:'Grief and loss are natural responses to significant life changes, such as the death of a loved one, the end of a relationship, or major life transitions. Grieving is a unique process that can involve a range of emotions, including sadness, anger, guilt, and confusion. Support from friends, family, or a therapist can be crucial in navigating the grieving process. Therapy can provide a safe space to express feelings, develop coping strategies, and find meaning in the experience of loss.'
             },
             {
-                id: 'trauma-and-ptsd',
+                id: ['trauma','ptsd'],
                 problem:'Trauma and PTSD',
                 color:'bg-violet-200',
                 description:'Trauma can result from experiencing or witnessing a distressing event, such as physical or emotional abuse, accidents, natural disasters, or combat. Post-traumatic stress disorder (PTSD) is a mental health condition that can develop after experiencing trauma, characterized by symptoms such as flashbacks, nightmares, hypervigilance, and avoidance of reminders of the traumatic event. Treatment for trauma and PTSD often involves therapy, such as cognitive-behavioral therapy (CBT), eye movement desensitization and reprocessing (EMDR), and sometimes medication to manage symptoms.'
             },
             {
-                id: 'work-related-stress',
+                id: ['work-related-stress'],
                 problem:'Work-related Stress',
                 color:'bg-orange-200',
                 description:'Work-related stress can arise from various factors, including high job demands, lack of control, poor work-life balance, and interpersonal conflicts in the workplace. Chronic work-related stress can lead to burnout, decreased productivity, and negative impacts on mental health. Strategies for managing work-related stress include setting boundaries, practicing time management, seeking support from colleagues or supervisors, and engaging in self-care activities outside of work. Therapy can also be beneficial in developing coping strategies and addressing any underlying issues contributing to work-related stress.'
             },
             {
-                id: 'anxiety-and-depression',
+                id: ['anxiety','depression'],
                 problem:'Anxiety and Depression',
                 color:'bg-pink-200',
                 description:'Anxiety and depression are common mental health conditions that can significantly impact daily life. Anxiety is characterized by excessive worry, restlessness, and physical symptoms such as increased heart rate and muscle tension. Depression involves persistent feelings of sadness, loss of interest in activities, and changes in sleep and appetite. Treatment for anxiety and depression often includes therapy, such as cognitive-behavioral therapy (CBT), medication, and lifestyle changes to improve overall well-being.'
@@ -56,49 +56,49 @@ export const ProblemSolvingBlockContent = {
         title:'Ar kādām problēmām es strādāju un risinu?',
         problems:[
             {
-                id: 'anxiety-and-stress',
+                id: ['anxiety','stress'],
                 problem:'Anksiozitāte un stress',
                 color:'bg-green-200',
                 description:'Anksiozitāte un stress ir izplatītas garīgās veselības problēmas, kas var skart ikvienu. Tie var izpausties kā pārmērīga uztraukšanās, nemiers, grūtības koncentrēties un fiziskas simptomi, piemēram, sirdsklauves vai muskuļu sasprindzinājums. Anksiozitātes un stresa pārvaldīšana bieži ietver dzīvesveida izmaiņas, terapiju un dažreiz medikamentus. Tehnikas, piemēram, apzinātība, relaksācijas vingrinājumi un kognitīvi biheiviorālā terapija (CBT), var būt efektīvas simptomu mazināšanā un vispārējās labklājības uzlabošanā.'
             },
             {
-                id: 'depression-and-mood-disorders',
+                id: ['depression','mood-disorders'],
                 problem:'Depresija un garastāvokļa traucējumi',
                 color:'bg-blue-200',
                 description:'Depresija un garastāvokļa traucējumi raksturojas ar pastāvīgām skumjām, bezcerību un interešu zudumu aktivitātēs. Tie var ietvert arī izmaiņas apetītē, miega traucējumus un grūtības koncentrēties. Depresijas ārstēšana bieži ietver psihoterapiju, medikamentus un dzīvesveida izmaiņas. Kognitīvi biheiviorālā terapija (CBT) un starppersonu terapija (IPT) ir bieži izmantotas terapeitiskās pieejas, kas var palīdzēt cilvēkiem pārvaldīt simptomus un uzlabot dzīves kvalitāti.'
             },
             {
-                id: 'relationship-issues',
+                id: ['relationship-issues'],
                 problem:'Attiecību problēmas',
                 color:'bg-yellow-200',
                 description:'Attiecību problēmas var rasties dažādās formās, tostarp komunikācijas problēmas, konflikti, uzticības jautājumi un intimitātes grūtības. Šīs problēmas var rasties romantiskās attiecībās, draudzībā vai ģimenes dinamikā. Pāru terapija vai ģimenes terapija var būt noderīga attiecību problēmu risināšanā, uzlabojot komunikācijas prasmes, veicinot empātiju un palīdzot cilvēkiem saprast viens otra perspektīvas. Turklāt individuālā terapija var arī būt noderīga tiem, kas piedzīvo attiecību grūtības, jo tā var nodrošināt drošu telpu personīgo jūtu izpētei un tikt galā ar stratēģijām.'
             },
             {
-                id: 'self-esteem-and-confidence',
+                id: ['self-esteem','confidence'],
                 problem:'Pašvērtējums un pārliecība',
                 color:'bg-purple-200',
                 description:'Zems pašvērtējums un pārliecība var ietekmēt dažādas dzīves jomas, tostarp attiecības, darbu un vispārējo labklājību. Pašvērtējuma veidošana bieži ietver negatīvas pašrunas izaicināšanu, reālistisku mērķu izvirzīšanu un sasniegumu svinēšanu. Terapija var arī būt noderīga, lai risinātu pamatproblēmas, kas veicina zemu pašvērtējumu, piemēram, pagātnes trauma vai negatīvas pieredzes. Tehnikas, piemēram, kognitīvi biheiviorālā terapija (CBT), var palīdzēt cilvēkiem identificēt un mainīt negatīvus domāšanas modeļus, vienlaikus attīstot veselīgākas tikt galā ar stratēģijām.'
             },
             {
-                id: 'grief-and-loss',
+                id: ['grief','loss'],
                 problem:'Bēdas un zaudējums',
                 color:'bg-red-200',
                 description:'Bēdas un zaudējums ir dabiskas reakcijas uz nozīmīgām dzīves pārmaiņām, piemēram, mīļotā nāvi, attiecību beigas vai lielas dzīves pārejas. Bēdāšanās ir unikāls process, kas var ietvert dažādas emocijas, tostarp skumjas, dusmas, vainu un apjukumu. Atbalsts no draugiem, ģimenes vai terapeita var būt izšķirošs bēdāšanās procesa navigācijā. Terapija var nodrošināt drošu telpu jūtu izteikšanai, tikt galā ar stratēģijām un atrast nozīmi zaudējuma pieredzē.'
             },
             {
-                id: 'trauma-and-ptsd',
+                id: ['trauma','ptsd'],
                 problem:'Trauma un PTSD',
                 color:'bg-teal-200',
                 description:'Trauma var rasties pēc satraucoša notikuma pieredzes vai liecin    āšanas, piemēram, fiziska vai emocionāla vardarbība, negadījumi, dabas katastrofas vai karš. Posttraumatiskā stresa traucējumi (PTSD) ir garīgās veselības stāvoklis, kas var attīstīties pēc traumas pieredzes, raksturojoties ar simptomiem, piemēram, atmiņu atkārtošanos, murgiem, hiperuzmanību un izvairīšanos no atgādinājumiem par traumatisko notikumu. Traumas un PTSD ārstēšana bieži ietver terapiju, piemēram, kognitīvi biheiviorālo terapiju (CBT), acu kustību desensibilizāciju un pārstrādi (EMDR) un dažreiz medikamentus simptomu pārvaldīšanai.'
             },
             {
-                id: 'work-related-stress',
+                id: ['work-related-stress'],
                 problem:'Darba stress',
                 color:'bg-orange-200',
                 description:'Darba stress var rasties no dažādiem faktoriem, tostarp augstām darba prasībām, kontroles trūkuma, sliktas darba un privātās dzīves līdzsvara un starppersonu konfliktiem darbavietā. Hronisks darba stress var novest pie izdegšanas, samazinātas produktivitātes un negatīvas ietekmes uz garīgo veselību. Stratēģijas darba stresa pārvaldīšanai ietver robežu noteikšanu, laika pārvaldības praksi, atbalsta meklēšanu no kolēģiem vai vadītājiem un iesaistīšanos pašaprūpes aktivitātēs ārpus darba. Terapija var arī būt noderīga, lai izstrādātu tikt galā ar stratēģijām un risinātu jebkādas pamatproblēmas, kas veicina darba stresu.'
             },
             {
-                id: 'anxiety-and-depression',
+                id: ['anxiety','depression'],
                 problem:'Anksiozitāte un depresija',
                 color:'bg-pink-200',
                 description:'Anksiozitāte un depresija ir izplatītas garīgās veselības problēmas, kas var būtiski ietekmēt ikdienas dzīvi. Anksiozitāte raksturo pārmērīga uztraukšanās, nemiers un fiziski simptomi, piemēram, paātrināta sirdsdarbība un muskuļu sasprindzinājums. Depresija ietver pastāvīgas skumjas, interešu zudumu aktivitātēs un miega un apetītes izmaiņas. Anksiozitātes un depresijas ārstēšana bieži ietver terapiju, piemēram, kognitīvi biheiviorālo terapiju (CBT), medikamentus un dzīvesveida izmaiņas vispārējās labklājības uzlabošanai.'
@@ -109,49 +109,49 @@ export const ProblemSolvingBlockContent = {
         title:'С какими проблемами я работаю и решаю?',
         problems:[
             {
-                id: 'anxiety-and-stress',
+                id: ['anxiety','stress'],
                 problem:'Тревога и стресс',
                 color:'bg-yellow-200',
                 description:'Тревога и стресс являются распространенными проблемами психического здоровья, которые могут затронуть любого человека. Они могут проявляться в виде чрезмерного беспокойства, беспокойства, трудностей с концентрацией и физических симптомов, таких как учащенное сердцебиение или мышечное напряжение. Управление тревогой и стрессом часто включает в себя сочетание изменений образа жизни, терапии и иногда медикаментов. Техники, такие как осознанность, упражнения на расслабление и когнитивно-поведенческая терапия (CBT), могут быть эффективными в снижении симптомов и улучшении общего благополучия.'
             },
             {
-                id: 'depression-and-mood-disorders',
+                id: ['depression','mood-disorders'],
                 problem:'Депрессия и расстройства настроения',
                 color:'bg-pink-200',
                 description:'Депрессия и расстройства настроения характеризуются постоянными чувствами грусти, безнадежности и отсутствием интереса или удовольствия от деятельности. Они также могут включать изменения в аппетите, нарушения сна и трудности с концентрацией. Лечение депрессии часто включает в себя сочетание психотерапии, медикаментов и изменений образа жизни. Когнитивно-поведенческая терапия (CBT) и межличностная терапия (IPT) являются часто используемыми терапевтическими подходами, которые могут помочь людям справиться с симптомами и улучшить качество жизни.'
             },
             {
-                id: 'relationship-issues',
+                id: ['relationship-issues'],
                 problem:'Проблемы в отношениях',
                 color:'bg-green-200',
                 description:'Проблемы в отношениях могут возникать в различных формах, включая проблемы с коммуникацией, конфликты, проблемы с доверием и трудности с интимностью. Эти проблемы могут возникать в романтических отношениях, дружбе или семейной динамике. Терапия для пар или семейная терапия может быть полезной для решения проблем в отношениях, улучшая навыки общения, способствуя эмпатии и помогая людям понять перспективы друг друга. Кроме того, индивидуальная терапия также может быть полезной для тех, кто испытывает трудности в отношениях, так как она может предоставить безопасное пространство для исследования личных чувств и разработки стратегий преодоления.'
             },
             {
-                id: 'self-esteem-and-confidence',
+                id: ['self-esteem','confidence'],
                 problem:'Самооценка и уверенность',
                 color:'bg-blue-200',
                 description:'Низкая самооценка и уверенность могут влиять на различные аспекты жизни, включая отношения, работу и общее благополучие. Построение самооценки часто включает в себя вызов негативного внутреннего диалога, установление реалистичных целей и празднование достижений. Терапия также может быть полезной для решения основных проблем, способствующих низкой самооценке, таких как прошлые травмы или негативные переживания. Техники, такие как когнитивно-поведенческая терапия (CBT), могут помочь людям идентифицировать и изменить негативные шаблоны мышления, а также развить более здоровые стратегии преодоления.'
             },
             {
-                id: 'grief-and-loss',
+                id: ['grief','loss'],
                 problem:'Горе и потеря',
                 color:'bg-yellow-200',
                 description:'Горе и потеря являются естественными реакциями на значительные жизненные изменения, такие как смерть близкого человека, конец отношений или крупные жизненные переходы. Процесс горевания уникален и может включать в себя широкий спектр эмоций, включая грусть, гнев, вину и замешательство. Поддержка от друзей, семьи или терапевта может быть решающей в навигации по процессу горевания. Терапия может предоставить безопасное пространство для выражения чувств, разработки стратегий преодоления и нахождения смысла в опыте потери.'
             },
             {
-                id: 'trauma-and-ptsd',
+                id: ['trauma','ptsd'],
                 problem:'Травма и ПТСР',
                 color:'bg-purple-200',
                 description:'Травма может возникать в результате переживания или свидетеля травмирующего события, такого как физическое или эмоциональное насилие, аварии, природные катастрофы или боевые действия. Посттравматическое стрессовое расстройство (ПТСР) является состоянием психического здоровья, которое может развиться после переживания травмы, характеризующееся симптомами, такими как повторяющиеся воспоминания, кошмары, гипервозбуждение и избегание напоминаний о травмирующем событии. Лечение травмы и ПТСР часто включает в себя терапию, такую как когнитивно-поведенческая терапия (CBT), десенсибилизация и переработка движением глаз (EMDR) и иногда медикаменты для управления симптомами.'
             },
             {
-                id: 'work-related-stress',
+                id: ['work-related-stress'],
                 problem:'Стресс, связанный с работой',
                 color:'bg-red-200',
                 description:'Стресс, связанный с работой, может возникать из-за различных факторов, включая высокие требования к работе, отсутствие контроля, плохой баланс между работой и личной жизнью и межличностные конфликты на рабочем месте. Хронический стресс, связанный с работой, может привести к выгоранию, снижению продуктивности и негативному влиянию на психическое здоровье. Стратегии управления стрессом, связанным с работой, включают установление границ, практику управления временем, поиск поддержки у коллег или руководителей и участие в мероприятиях по уходу за собой вне работы. Терапия также может быть полезной для разработки стратегий преодоления и решения любых основных проблем, способствующих стрессу, связанному с работой.'
             },
             {
-                id: 'anxiety-and-depression',
+                id: ['anxiety','depression'],
                 problem:'Тревога и депрессия',
                 color:'bg-teal-200',
                 description:'Тревога и депрессия являются распространенными проблемами психического здоровья, которые могут значительно повлиять на повседневную жизнь. Тревога характеризуется чрезмерным беспокойством, беспокойством и физическими симптомами, такими как учащенное сердцебиение и мышечное напряжение. Депрессия включает в себя постоянные чувства грусти, потерю интереса к деятельности и изменения в сне и аппетите. Лечение тревоги и депрессии часто включает в себя терапию, такую как когнитивно-поведенческая терапия (CBT), медикаменты и изменения образа жизни для улучшения общего благополучия.'
