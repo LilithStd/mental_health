01031d5633df250cc8b1c76eb2e7faf5b4aada5b
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-08 21:16:32 +0300

    created footer content

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -2,6 +2,7 @@ import { getLocale } from "@/app/hooks/server/getLocale"
 import { LocaleType } from "@/app/types/types"
 import InstagramIcon from "@/public/icons/instagram.svg"
 import FacebookIcon from "@/public/icons/facebook.svg"
+import { FooterContent } from "@/translate/shared/footer"
 
 
 export default async function Footer() {
@@ -24,14 +25,14 @@ export default async function Footer() {
                     <h3 className={`text-lg font-medium `}>Terms of Service</h3>
                 </div>
                 <div className={`flex flex-col gap-2 p-4`}>
-                    <h2 className={`text-2xl font-bold `}>Follow Us</h2>
+                    <h2 className={`text-2xl font-bold `}>{FooterContent[locale].followUs}</h2>
                     <div className={`flex gap-2`}>
                         <InstagramIcon className={`w-6 h-6`} />
-                        <h3 className={`text-lg font-medium `}>Instagram</h3>
+                        <h3 className={`text-lg font-medium `}>{FooterContent[locale].instagram}</h3>
                     </div>
                     <div className={`flex gap-2`}>
                         <FacebookIcon className={`w-6 h-6`} />
-                        <h3 className={`text-lg font-medium `}>Facebook</h3>
+                        <h3 className={`text-lg font-medium `}>{FooterContent[locale].facebook}</h3>
                     </div>
                 </div>
             </div>
---
+++ translate/shared/footer.ts
@@ -0,0 +1,17 @@
+export const FooterContent = {
+    en: {
+        followUs: 'Follow Us',
+        instagram: 'Instagram',
+        facebook: 'Facebook'
+    },
+    lv: {
+        followUs: 'Seko mums',
+        instagram: 'Instagram',
+        facebook: 'Facebook'
+    },
+    ru: {
+        followUs: 'Следите за нами',
+        instagram: 'Инстаграм',
+        facebook: 'Фейсбук'
+    }
+}
