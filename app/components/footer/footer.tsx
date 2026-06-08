5aa2b4da2191b8cdbfac1b67cad0e1c21b37dff0
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-08 21:09:30 +0300

    added facebook icon

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,5 +1,6 @@
 import { getLocale } from "@/app/hooks/server/getLocale"
 import { LocaleType } from "@/app/types/types"
+import InstagramIcon from "@/public/icons/instagram.svg"
 
 
 export default async function Footer() {
@@ -23,7 +24,10 @@ export default async function Footer() {
                 </div>
                 <div className={`flex flex-col gap-2 p-4`}>
                     <h2 className={`text-2xl font-bold `}>Follow Us</h2>
-                    <h3 className={`text-lg font-medium `}>Instagram</h3>
+                    <div className={`flex gap-2`}>
+                        <InstagramIcon className={`w-6 h-6`} />
+                        <h3 className={`text-lg font-medium `}>Instagram</h3>
+                    </div>
                     <h3 className={`text-lg font-medium `}>Facebook</h3>
                 </div>
             </div>
