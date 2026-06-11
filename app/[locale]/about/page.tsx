a221dc588dc8544c8ff03ea79fe70d5cfc4534a9
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-11 22:50:34 +0300

    updated about page content

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -7,14 +7,16 @@ export default async function AboutPage() {
   return  (
         <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
             <div className={`flex flex-col flex-1 max-w-6xl border border-primary-color/30 rounded-large bg-primary-color/20 p-4 shadow-lg w-full`}>
-            <div className={`flex w-full flex-col flex-1  rounded-large`}>
-                <h2 className={`text-5xl justify-center text-center font-bold`}>{ABOUT_PAGE_CONTENT[locale].title}</h2>
-                <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].description}</p>
-            </div>
-            <div className={`flex w-full flex-col flex-1 mt-6 rounded-large`}>
-                <h2 className={`text-3xl justify-center text-center font-bold`}>{ABOUT_PAGE_CONTENT[locale].contact}</h2>
-                <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].contact}</p>
-            </div>
+                <div className={`flex w-full flex-col flex-1  rounded-large`}>
+                    <h2 className={`text-5xl justify-center text-center font-bold`}>{ABOUT_PAGE_CONTENT[locale].title}</h2>
+                    <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].description}</p>
+                </div>
+                <div className={`flex w-full flex-col flex-1 mt-6 rounded-large`}>
+                    <h2 className={`text-3xl justify-center text-center font-bold`}>{ABOUT_PAGE_CONTENT[locale].contact.title}</h2>
+                    <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].contact.phone}</p>
+                    <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].contact.email}</p>
+                    <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].contact.address}</p>
+                </div>
             </div>
             
         </div>
--- translate/aboutPage/aboutContent.ts
+++ translate/aboutPage/aboutContent.ts
@@ -2,17 +2,32 @@ export const ABOUT_PAGE_CONTENT = {
     en: {
         title: 'About us',
         description: 'We are a team of passionate individuals dedicated to improving mental health through innovative solutions. Our mission is to provide accessible and effective tools for mental well-being, empowering individuals to take control of their mental health journey.',
-        contact: 'Contact us',
+        contact: {
+            title:'Contact us',
+            phone: 'Phone: +1 (123) 456-7890',
+            email: 'Email:email@gmail.com',
+            address: 'Address: 123 Main St, Anytown, USA',
+        },
     },
     lv: {
         title: 'Par mums',
         description: 'Mēs esam komanda ar aizrautīgiem indivīdiem, kas ir veltīti garīgās veselības uzlabošanai ar inovatīviem risinājumiem. Mūsu misija ir nodrošināt pieejamus un efektīvus rīkus garīgajai labklājībai, ļaujot indivīdiem pārņemt kontroli pār savu garīgās veselības ceļojumu.',
-        contact: 'Sazinieties ar mums',
+        contact: {
+            title: 'Sazinieties ar mums',
+            phone: 'Telefons: +371 123 4567',
+            email: 'E-pasts: email@gmail.com',
+            address: 'Adrese: 123 Galvenā iela, Jebkura pilsēta, LV',
+        },
     },
     ru: {
         title: 'О нас',
         description: 'Мы - команда увлеченных людей, посвятивших себя улучшению психического здоровья с помощью инновационных решений. Наша миссия - предоставлять доступные и эффективные инструменты для психического благополучия, позволяя людям взять под контроль свое путешествие по психическому здоровью.',
-        contact: 'Свяжитесь с нами',
+        contact: {
+            title: 'Свяжитесь с нами',
+            phone: 'Телефон: +7 (123) 456-7890',
+            email: 'Электронная почта: email@gmail.com',
+            address: 'Адрес: 123 Главная ул., Любой город, РФ',
+        },
     },
 } as const
 
