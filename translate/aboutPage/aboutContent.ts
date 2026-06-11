ca3645c77e1cd6d779b3ea1ac4e0e6a38e5003c3
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-11 22:37:28 +0300

    created about page content

---
+++ translate/aboutPage/aboutContent.ts
@@ -0,0 +1,29 @@
+export const ABOUT_PAGE_CONTENT = {
+    en: {
+        title: 'About us',
+        description: 'We are a team of passionate individuals dedicated to improving mental health through innovative solutions. Our mission is to provide accessible and effective tools for mental well-being, empowering individuals to take control of their mental health journey.',
+        contact: 'Contact us',
+    },
+    lv: {
+        title: 'Par mums',
+        description: 'Mēs esam komanda ar aizrautīgiem indivīdiem, kas ir veltīti garīgās veselības uzlabošanai ar inovatīviem risinājumiem. Mūsu misija ir nodrošināt pieejamus un efektīvus rīkus garīgajai labklājībai, ļaujot indivīdiem pārņemt kontroli pār savu garīgās veselības ceļojumu.',
+        contact: 'Sazinieties ar mums',
+    },
+    ru: {
+        title: 'О нас',
+        description: 'Мы - команда увлеченных людей, посвятивших себя улучшению психического здоровья с помощью инновационных решений. Наша миссия - предоставлять доступные и эффективные инструменты для психического благополучия, позволяя людям взять под контроль свое путешествие по психическому здоровью.',
+        contact: 'Свяжитесь с нами',
+    },
+} as const
+
+export const ABOUT_PAGE_BUTTONS = {
+    en: 'Read more',
+    lv: 'Lasīt vairāk',
+    ru: 'Читать далее',
+} as const
+
+export const ABOUT_PAGE_SECTIONS = {
+    en: 'Our Team',
+    lv: 'Mūsu komanda',
+    ru: 'Наша команда',
+} as const
