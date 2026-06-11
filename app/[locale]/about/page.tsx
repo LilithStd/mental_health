559e3bb28b2764b30b3ed6d644bf0cf436da3609
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-11 22:43:52 +0300

    updated styles in about us page

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,11 +1,22 @@
+import { getLocale } from "@/app/hooks/server/getLocale";
+import { LocaleType } from "@/app/types/types";
+import { ABOUT_PAGE_CONTENT } from "@/translate/aboutPage/aboutContent";
 
-export default function AboutPage() {
+export default async function AboutPage() {
+    const locale = await getLocale() as LocaleType;
   return  (
         <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
-            <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 border border-primary-color/20 p-4 shadow-md`}>
-                <h2 className={`text-5xl justify-center text-center font-bold`}>About us</h2>
-                <p className={`text-center mt-2`}>We are a team of passionate individuals dedicated to providing comprehensive mental health support and resources. Our mission is to create a safe and inclusive space where individuals can access the help they need to improve their mental well-being. We believe in the power of community and strive to foster connections among our users, allowing them to share their experiences and support one another on their mental health journeys.</p>
+            <div className={`flex flex-col flex-1 max-w-6xl border border-primary-color/30 rounded-large bg-primary-color/20 p-4 shadow-lg w-full`}>
+            <div className={`flex w-full flex-col flex-1  rounded-large`}>
+                <h2 className={`text-5xl justify-center text-center font-bold`}>{ABOUT_PAGE_CONTENT[locale].title}</h2>
+                <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].description}</p>
             </div>
+            <div className={`flex w-full flex-col flex-1 mt-6 rounded-large`}>
+                <h2 className={`text-3xl justify-center text-center font-bold`}>{ABOUT_PAGE_CONTENT[locale].contact}</h2>
+                <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].contact}</p>
+            </div>
+            </div>
+            
         </div>
   )
 }
