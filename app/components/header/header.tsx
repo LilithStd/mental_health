007e0ecfcd7a71f91f827f7c9c31ae7f139f8f08
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-17 21:36:10 +0300

    updated styles in nav link component

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -9,14 +9,14 @@ export default function Header() {
     return (
         <nav className={`flex  justify-center sticky pt-8 pl-8 pr-8 top-0 left-0 w-full z-50`}>
             <HeaderBackground>
-                <div className="grid grid-cols-[1fr_2fr_1fr] gap-10 items-center w-full">
+                <div className="grid grid-cols-[1fr_2fr_0.5fr] gap-10 items-center w-full">
                     <div className="flex justify-start">
                         <Logo />
                     </div>
-                    <div className="flex justify-center">
+                    <div className="flex justify-center items-center">
                         <ListLinks />
                     </div>
-                    <div className="flex justify-end rounded-large p-4">
+                    <div className="flex justify-end items-center">
                         <AdditionalPanel />
                         {/* <AuthorizationMain /> */}
                     </div>
