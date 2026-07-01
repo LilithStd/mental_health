ae1f2f9a648b478220b9ba9ba598d459bde30092
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-01 19:26:17 +0300

    updated styles in search page

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -1,22 +1,24 @@
 import { SEARCH_TYPE } from "@/app/globalConsts/globalEnum";
 
 interface SearchPageProps {
-  params: Promise<{
+  searchParams: Promise<{
     type: SEARCH_TYPE
     query: string
     locale: string
   }>
 }
 
-export default async function SearchPage({ params }: SearchPageProps) {
-  const { type, query, locale } = await params;
-  console.log('SearchPage params:', await params); // Log the params to the console for debugging
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const { type, query, locale } = await searchParams;
+  console.log('SearchPage params:', await searchParams); // Log the params to the console for debugging
   return (
-    <div>
+    <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
+      <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
         <h1>Search Page</h1>
         <p>Type: {type}</p>
         <p>Query: {query}</p>
         <p>Locale: {locale}</p>
+      </div>  
     </div>
   )
 }
