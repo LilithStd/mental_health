9ad9ea911fdca268db84cfda8e91271b76b9f009
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-01 18:53:22 +0300

    updated hash tags component (add redirect to search page after click on element)

--- app/components/shared/hashTags.tsx
+++ app/components/shared/hashTags.tsx
@@ -4,7 +4,8 @@ import { useLocale } from "@/app/hooks/useLocale";
 import { LocaleType } from "@/app/types/types";
 import { HASH_TAGS } from "@/translate/shared/hashTags";
 import HashTagIcon from "@/public/icons/Tag.svg";
-import { SIZE_ELEMENT } from "@/app/globalConsts/globalEnum";
+import { SEARCH_TYPE, SIZE_ELEMENT } from "@/app/globalConsts/globalEnum";
+import { routes } from "@/app/helpers/helpersFunctions";
 
 interface HashTagsProps {
     hashTags: string[],
@@ -13,17 +14,16 @@ interface HashTagsProps {
 
 export default function HashTags({ hashTags, type }: HashTagsProps) {
     const locale = useLocale() as LocaleType;
-  return (
-    <div>
-        {/* <span className={`text-xs`}>#{HASH_TAGS[locale].hashTags}</span> */}
+    const routesAdaptive = routes(locale)
+
+    return (
         <div className={`flex w-full ${type === SIZE_ELEMENT.FULL ? 'text-md' : 'text-xs'} flex-wrap gap-2`}>
             {hashTags.map((tag, index) => (
-                <span key={index} className={`flex items-center gap-1 pl-2 pr-2 bg-buttonContainer/50  rounded-large`}>
-                    <HashTagIcon width={ type === SIZE_ELEMENT.FULL ? 14 : 8} height={ type === SIZE_ELEMENT.FULL ? 14 : 8} /> 
+                <span key={index} className={`cursor-pointer flex items-center gap-1 pl-2 pr-2 bg-buttonContainer/50  rounded-large`} onClick={() => routesAdaptive.search.root + `?type=${SEARCH_TYPE.HASHTAG}&query=${tag}`}>
+                <HashTagIcon width={ type === SIZE_ELEMENT.FULL ? 14 : 8} height={ type === SIZE_ELEMENT.FULL ? 14 : 8} /> 
                     {tag}
                 </span>
             ))}
         </div>
-    </div>
   )
 }
