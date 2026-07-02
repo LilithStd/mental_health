50f56d827ea57731e208e4e8afb447bfb4c089f7
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-02 20:45:44 +0300

    updated styles in search page

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -16,10 +16,12 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
   return (
     <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
       <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
-        <h1>Search Page</h1>
-        <p>Type: {type}</p>
-        <p>Query: {query}</p>
-        <p>Locale: {locale}</p>
+        <div className={`flex justify-start items-center w-full flex-col flex-1 gap-4`}>
+          <h1>Search Page</h1>
+          <p>Type: {type}</p>
+          <p>Query: {query}</p>
+          <p>Locale: {locale}</p>
+        </div>
       </div>  
     </div>
   )
