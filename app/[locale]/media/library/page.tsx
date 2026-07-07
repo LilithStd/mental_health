f1cd278dcc6a53170a707f3dba8bfb8ec56cfbb7
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-07 20:16:08 +0300

    replaced prev library component  from page -> articleComponent

--- app/[locale]/media/library/page.tsx
+++ app/[locale]/media/library/page.tsx
@@ -1,11 +1,13 @@
 
 import CreateElementLibrary from "@/app/components/library/createElementLibrary"
+import LibraryClient from "@/app/components/library/libraryClient"
 import ReturnButton from "@/app/components/returnButton"
 import HashTags from "@/app/components/shared/hashTags"
 import Search from "@/app/components/shared/search"
 import { SEARCH_REQUEST_TYPE, SIZE_ELEMENT } from "@/app/globalConsts/globalEnum"
 import { routes } from "@/app/helpers/helpersFunctions"
 import { getLocale } from "@/app/hooks/server/getLocale"
+import { Library } from "@/app/models/library"
 import { getAllLibrary } from "@/app/service/libraryService"
 import { LocaleType } from "@/app/types/types"
 import { LibraryContent } from "@/translate/mediaPage/libraryContent/libraryContent"
@@ -18,37 +20,6 @@ export default async function LibraryPage() {
     const routesAdaptive = routes(locale)
 
   return (
-    <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
-        <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
-            <div className={`flex w-full justify-start mb-4`}>
-                <ReturnButton pathToReturn={routesAdaptive.media.root} />
-            </div>
-            <div className={`flex w-full justify-center gap-4 mb-4`}>
-                <span>{LibraryContent[locale].title}</span>
-            </div>
-            <div className={`flex w-full justify-center gap-4 mb-4`}>
-                <Search requestType={SEARCH_REQUEST_TYPE.TITLE} query={""} callBackResultAfterSearch={() => {}} arrayForSearch={library} locale={locale} />
-            </div>
-            <div className={`flex w-full justify-center pr-4 mb-4`}>
-                <CreateElementLibrary />
-            </div>
-            <div className={`flex flex-col gap-4 `}>
-                    {library.map(item => (
-                        <div key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
-                            <span className={`text-2xl`}>{item.title[locale]}</span>                        
-                            <span className={`text-sm italic`}>{LibraryContent[locale].type}:{item.type}</span>   
-                            <HashTags hashTags={item.hashTags.length > 0 ? item.hashTags : ['depression']} type={SIZE_ELEMENT.SMALL} />
-                                <details key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
-                                
-                                <summary className={`font-bold cursor-pointer`}>Description</summary>
-                                <p>{item.content[locale]}</p>
-                            </details>
-                        </div>
-            
-                        
-                    ))}
-                </div>
-        </div>
-    </div>
+    <LibraryClient libraryData={library} />
   )
 }
--- app/components/library/libraryClient.tsx
+++ app/components/library/libraryClient.tsx
@@ -1,11 +1,54 @@
+'use client'
+import { SEARCH_REQUEST_TYPE, SIZE_ELEMENT } from "@/app/globalConsts/globalEnum"
+import { useLocale } from "@/app/hooks/useLocale"
 import { LibraryTypes } from "@/app/models/library"
+import { LibraryType, LocaleType } from "@/app/types/types"
+import { LibraryContent } from "@/translate/mediaPage/libraryContent/libraryContent"
+import ReturnButton from "../returnButton"
+import HashTags from "../shared/hashTags"
+import Search from "../shared/search"
+import CreateElementLibrary from "./createElementLibrary"
+import { routes } from "@/app/helpers/helpersFunctions"
 
 interface LibraryClientProps {
-  libraryData: LibraryTypes[]
+  libraryData: LibraryType[]
 }
 
 export default function LibraryClient({ libraryData }: LibraryClientProps) {
+    const locale = useLocale() as LocaleType
+    const routesAdaptive = routes(locale)
   return (
-    <div>LibraryClient</div>
+    <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
+        <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
+            <div className={`flex w-full justify-start mb-4`}>
+                <ReturnButton pathToReturn={routesAdaptive.media.root} />
+            </div>
+            <div className={`flex w-full justify-center gap-4 mb-4`}>
+                <span>{LibraryContent[locale].title}</span>
+            </div>
+            <div className={`flex w-full justify-center gap-4 mb-4`}>
+                <Search requestType={SEARCH_REQUEST_TYPE.TITLE} query={""} callBackResultAfterSearch={() => {}} arrayForSearch={libraryData} locale={locale} />
+            </div>
+            <div className={`flex w-full justify-center pr-4 mb-4`}>
+                <CreateElementLibrary />
+            </div>
+            <div className={`flex flex-col gap-4 `}>
+                    {libraryData.map(item => (
+                        <div key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
+                            <span className={`text-2xl`}>{item.title[locale]}</span>                        
+                            <span className={`text-sm italic`}>{LibraryContent[locale].type}:{item.type}</span>   
+                            <HashTags hashTags={item.hashTags.length > 0 ? item.hashTags : ['depression']} type={SIZE_ELEMENT.SMALL} />
+                                <details key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
+                                
+                                <summary className={`font-bold cursor-pointer`}>Description</summary>
+                                <p>{item.content[locale]}</p>
+                            </details>
+                        </div>
+            
+                        
+                    ))}
+                </div>
+        </div>
+    </div>
   )
 }
