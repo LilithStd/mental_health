97dcd1fc263d3705708df7b637b94a18baea7cd2
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-07 20:30:04 +0300

    updated library component

--- app/components/library/libraryClient.tsx
+++ app/components/library/libraryClient.tsx
@@ -9,6 +9,7 @@ import HashTags from "../shared/hashTags"
 import Search from "../shared/search"
 import CreateElementLibrary from "./createElementLibrary"
 import { routes } from "@/app/helpers/helpersFunctions"
+import { useState } from "react"
 
 interface LibraryClientProps {
   libraryData: LibraryType[]
@@ -17,38 +18,49 @@ interface LibraryClientProps {
 export default function LibraryClient({ libraryData }: LibraryClientProps) {
     const locale = useLocale() as LocaleType
     const routesAdaptive = routes(locale)
-  return (
-    <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
-        <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
-            <div className={`flex w-full justify-start mb-4`}>
-                <ReturnButton pathToReturn={routesAdaptive.media.root} />
-            </div>
-            <div className={`flex w-full justify-center gap-4 mb-4`}>
-                <span>{LibraryContent[locale].title}</span>
-            </div>
-            <div className={`flex w-full justify-center gap-4 mb-4`}>
-                <Search requestType={SEARCH_REQUEST_TYPE.TITLE} query={""} callBackResultAfterSearch={() => {}} arrayForSearch={libraryData} locale={locale} />
-            </div>
-            <div className={`flex w-full justify-center pr-4 mb-4`}>
-                <CreateElementLibrary />
-            </div>
-            <div className={`flex flex-col gap-4 `}>
-                    {libraryData.map(item => (
-                        <div key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
-                            <span className={`text-2xl`}>{item.title[locale]}</span>                        
-                            <span className={`text-sm italic`}>{LibraryContent[locale].type}:{item.type}</span>   
-                            <HashTags hashTags={item.hashTags.length > 0 ? item.hashTags : ['depression']} type={SIZE_ELEMENT.SMALL} />
-                                <details key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
-                                
-                                <summary className={`font-bold cursor-pointer`}>Description</summary>
-                                <p>{item.content[locale]}</p>
-                            </details>
-                        </div>
-            
-                        
-                    ))}
+    const [library, setLibrary] = useState<LibraryType[]>(libraryData);
+    const [searchResults, setSearchResults] = useState<LibraryType[]>([]);
+    console.log('Result', searchResults);
+    return (
+        <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
+            <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
+                <div className={`flex w-full justify-start mb-4`}>
+                    <ReturnButton pathToReturn={routesAdaptive.media.root} />
                 </div>
+                <div className={`flex w-full justify-center gap-4 mb-4`}>
+                    <span>{LibraryContent[locale].title}</span>
+                </div>
+                <div className={`flex w-full justify-center gap-4 mb-4`}>
+                    <Search requestType={SEARCH_REQUEST_TYPE.TITLE} query={""} callBackResultAfterSearch={setSearchResults} arrayForSearch={library} locale={locale} />
+                </div>
+                <div className={`flex w-full justify-center pr-4 mb-4`}>
+                    <CreateElementLibrary />
+                </div>
+                <div className={`flex flex-col gap-4 `}>
+                        {searchResults.length > 0 ? searchResults.map(item => (
+                            <div key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
+                                <span className={`text-2xl`}>{item.title[locale]}</span>                        
+                                <span className={`text-sm italic`}>{LibraryContent[locale].type}:{item.type}</span>   
+                                <HashTags hashTags={item.hashTags.length > 0 ? item.hashTags : ['depression']} type={SIZE_ELEMENT.SMALL} />
+                                    <details key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
+                                    
+                                    <summary className={`font-bold cursor-pointer`}>Description</summary>
+                                    <p>{item.content[locale]}</p>
+                                </details>
+                            </div>
+                        )) : library.map(item => (
+                            <div key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
+                                <span className={`text-2xl`}>{item.title[locale]}</span>
+                                <span className={`text-sm italic`}>{LibraryContent[locale].type}:{item.type}</span>   
+                                <HashTags hashTags={item.hashTags.length > 0 ? item.hashTags : ['depression']} type={SIZE_ELEMENT.SMALL} />
+                                <details key={item.id} className={`flex flex-col gap-2 p-4 bg-primary-color/50 rounded-large`}>
+                                    <summary className={`font-bold cursor-pointer`}>Description</summary>
+                                    <p>{item.content[locale]}</p>
+                                </details>
+                            </div>
+                        ))}
+                    </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
