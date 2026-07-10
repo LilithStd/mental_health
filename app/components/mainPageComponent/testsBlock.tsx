f6250aad6b0efc9566faab87d7e066e83c73a0f5
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-10 18:40:10 +0300

    updated styles in main page for test block

--- app/components/mainPageComponent/testsBlock.tsx
+++ app/components/mainPageComponent/testsBlock.tsx
@@ -21,14 +21,14 @@ export default function TestsBlock() {
         <div className={`flex gap-4 p-6 justify-around items-center bg-primary-color/20 backdrop-blur-md rounded-large text-center border border-primary-color/30 shadow-lg`}>
            
 
-            <Image src={LINK_RAW_PATH.butterflyBG} alt="Background Image" fill className="z-0  w-full h-full object-cover rounded-large opacity-70" />
+            
             <div className={`flex flex-col ml-4 gap-4 justify-center items-center text-center`}>
                 <h2 className={`text-5xl font-bold `}>{TestsBlockContent[locale].title}</h2>
                 <p className={`italic text-lg`}>{TestsBlockContent[locale].description}</p>
-                <Link href={routesAdaptive.tests.root} className={`mt-4 px-4 py-2 bg-buttonContainer rounded-large font-bold`}>{TestsBlockContent[locale].buttonText}</Link>
+                <Link href={routesAdaptive.tests.root} className={`mt-4 z-10 px-4 py-2 bg-buttonContainer rounded-large font-bold`}>{TestsBlockContent[locale].buttonText}</Link>
             </div>
             <AppImage width={300} height={300} imageName={UPLOAD_IMAGE_NAME.global.mainPage.tests.butterFlyViolet} alt="Test Butterfly" type={IMAGES_UPLOAD_PATH.GLOBAL}/>
-            
+            <Image src={LINK_RAW_PATH.butterflyBG} alt="Background Image" fill className="z-0  w-full h-full object-cover rounded-large opacity-70" />
         </div>
 
     )
