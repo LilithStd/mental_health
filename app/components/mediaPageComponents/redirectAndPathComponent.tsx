a40d5bb6725563b87ef5df4dd15203fc312f7f8f
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-19 19:29:52 +0300

    added arrow icon and implement in components

--- app/components/mediaPageComponents/redirectAndPathComponent.tsx
+++ app/components/mediaPageComponents/redirectAndPathComponent.tsx
@@ -1,5 +1,6 @@
 import ReturnButton from "../returnButton";
 import Link from "next/link";
+import ArrowIcon from '@/public/icons/ArrowRightCircle.svg'
 
 interface RedirectAndPathComponentProps {
   links: { name: string, href: string }[];
@@ -14,6 +15,7 @@ export default function RedirectAndPathComponent({ links, pathToRedirect }: Redi
             {links.map((link, index) => (
                 <div key={index} className={`bg-primary-color/50 cursor-pointer pb-2 pt-2 pl-4 pr-4 rounded-circle`}>
                     <Link href={link.href}>{link.name}</Link>
+                    {index < links.length - 1 && <ArrowIcon className="inline-block ml-2 w-6" />}
                 </div>
             ))}
         </div>
