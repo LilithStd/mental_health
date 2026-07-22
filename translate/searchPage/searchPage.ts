20daedbedb800a6989762de4aae95eb9dd738330
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-22 22:15:07 +0300

    created searchpage content

---
+++ translate/searchPage/searchPage.ts
@@ -0,0 +1,17 @@
+export const searchPageContent = {
+    en: {
+        title: "Search",
+        searchInputPlaceholder: "Search by request",
+        noResultsFound: "No results found",
+    },
+    lv: {
+        title: "Meklēt",
+        searchInputPlaceholder: "Meklēt pēc pieprasījuma",
+        noResultsFound: "Rezultāti nav atrasti",
+    },
+    ru: {
+        title: "Поиск",
+        searchInputPlaceholder: "Поиск по запросу",
+        noResultsFound: "Результаты не найдены",
+    },
+};
