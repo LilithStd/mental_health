cfb7126bd9daed2fe98ba6e3692eb4217303a2a6
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-22 22:21:51 +0300

    updated search page

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -2,6 +2,7 @@ import { SEARCH_TYPE } from "@/app/globalConsts/globalEnum";
 import { getLocale } from "@/app/hooks/server/getLocale";
 import { LocaleType } from "@/app/types/types";
 
+
 interface SearchPageProps {
   searchParams: Promise<{
     type: SEARCH_TYPE
@@ -11,16 +12,14 @@ interface SearchPageProps {
 }
 
 export default async function SearchPage({ searchParams }: SearchPageProps) {
-  const { type, query} = await searchParams;
+  const { query} = await searchParams;
   const locale = await getLocale() as LocaleType;
+
   return (
     <div className={`flex flex-col indents-main-container  flex-1 items-center`}>
       <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
         <div className={`flex justify-start items-center w-full flex-col flex-1 gap-4`}>
-          <h1>Search Page</h1>
-          <p>Type: {type}</p>
-          <p>Query: {query}</p>
-          <p>Locale: {locale}</p>
+          <h2 className={` justify-center text-center font-bold`}>{`Search results for "${query}"`}</h2>
         </div>
       </div>  
     </div>
