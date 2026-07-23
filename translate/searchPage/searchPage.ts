dd8b6bf9e2ccfe8a7cc9a14ab2298dd77468ed94
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-23 21:20:26 +0300

    updated search page content

--- translate/searchPage/searchPage.ts
+++ translate/searchPage/searchPage.ts
@@ -1,17 +1,17 @@
 export const searchPageContent = {
     en: {
         title: "Search",
-        searchInputPlaceholder: "Search by request",
+        searchByRequest: "Search by request",
         noResultsFound: "No results found",
     },
     lv: {
         title: "Meklēt",
-        searchInputPlaceholder: "Meklēt pēc pieprasījuma",
+        searchByRequest: "Meklēt pēc pieprasījuma",
         noResultsFound: "Rezultāti nav atrasti",
     },
     ru: {
         title: "Поиск",
-        searchInputPlaceholder: "Поиск по запросу",
+        searchByRequest: "Поиск по запросу",
         noResultsFound: "Результаты не найдены",
     },
 };
