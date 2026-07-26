fd77aae0c74e3f4b6e3a2520e4e0eaaf310f2345
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-26 19:54:08 +0300

    updated styles in media page

--- app/components/mediaPageComponents/redirectAndPathComponent.tsx
+++ app/components/mediaPageComponents/redirectAndPathComponent.tsx
@@ -9,9 +9,9 @@ interface RedirectAndPathComponentProps {
 
 export default function RedirectAndPathComponent({ links, pathToRedirect }: RedirectAndPathComponentProps) {
   return (
-    <div className={`flex w-full justify-start mb-4 z-10`}>
+    <div className={`flex gap-2 w-full justify-start mb-4 z-10`}>
         <ReturnButton pathToReturn={pathToRedirect} />
-        <div className={`flex  items-center`}>
+        <div className={`flex gap-2 items-center`}>
             {links.map((link, index) => (
                 <div key={index} className={`bg-primary-color/50 cursor-pointer border border-primary-color/30 pb-2 pt-2 pl-4 pr-4 rounded-circle`}>
                     <Link href={link.href}>{link.name}</Link>
