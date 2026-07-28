84783b6b1d5fef65ee5e2cecd66e22d13bb87299
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-28 15:14:51 +0300

    updated about page content

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
-import { LINK_RAW_PATH } from "@/app/globalConsts/globalConsts";
+import { LINK_RAW_PATH, UPLOAD_IMAGE_NAME } from "@/app/globalConsts/globalConsts";
 import { getLocale } from "@/app/hooks/server/getLocale";
 import { LocaleType } from "@/app/types/types";
 import { ABOUT_PAGE_CONTENT } from "@/translate/aboutPage/aboutContent";
+const DoctorImage = UPLOAD_IMAGE_NAME.bio.photo
 
 export default async function AboutPage() {
     const locale = await getLocale() as LocaleType;
-  return  (
+    return  (
         <div className={`flex flex-col indents-main-container  flex-1 items-center `}>
             <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
                 <div className={`flex w-full flex-col flex-1  rounded-large`}>
@@ -15,9 +16,8 @@ export default async function AboutPage() {
                 </div>
                 <div className={`flex w-full flex-col flex-1 mt-6 rounded-large`}>
                     <h2 className={`text-3xl justify-center text-center font-bold`}>{ABOUT_PAGE_CONTENT[locale].contact.title}</h2>
-                    <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].contact.phone}</p>
                     <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].contact.email}</p>
-                    <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].contact.address}</p>
+                    
                 </div>
                 <Image src={LINK_RAW_PATH.butterflyBG} alt="Background Image" fill className="  w-full h-full z-0 object-cover rounded-large opacity-20" />
             </div>
--- translate/aboutPage/aboutContent.ts
+++ translate/aboutPage/aboutContent.ts
@@ -1,32 +1,29 @@
 export const ABOUT_PAGE_CONTENT = {
     en: {
-        title: 'About us',
+        title: 'About me',
         description: 'We are a team of passionate individuals dedicated to improving mental health through innovative solutions. Our mission is to provide accessible and effective tools for mental well-being, empowering individuals to take control of their mental health journey.',
         contact: {
-            title:'Contact us',
-            phone: 'Phone: +1 (123) 456-7890',
+            title:'Contact me',
+            name: 'Aleksandra Konevnina',
             email: 'Email:email@gmail.com',
-            address: 'Address: 123 Main St, Anytown, USA',
         },
     },
     lv: {
-        title: 'Par mums',
+        title: 'Par mani',
         description: 'Mēs esam komanda ar aizrautīgiem indivīdiem, kas ir veltīti garīgās veselības uzlabošanai ar inovatīviem risinājumiem. Mūsu misija ir nodrošināt pieejamus un efektīvus rīkus garīgajai labklājībai, ļaujot indivīdiem pārņemt kontroli pār savu garīgās veselības ceļojumu.',
         contact: {
-            title: 'Sazinieties ar mums',
-            phone: 'Telefons: +371 123 4567',
+            title: 'Sazinieties ar mani',
+            name: 'Aleksandra Koņevnina',
             email: 'E-pasts: email@gmail.com',
-            address: 'Adrese: 123 Galvenā iela, Jebkura pilsēta, LV',
         },
     },
     ru: {
-        title: 'О нас',
+        title: 'Обо мне',
         description: 'Мы - команда увлеченных людей, посвятивших себя улучшению психического здоровья с помощью инновационных решений. Наша миссия - предоставлять доступные и эффективные инструменты для психического благополучия, позволяя людям взять под контроль свое путешествие по психическому здоровью.',
         contact: {
-            title: 'Свяжитесь с нами',
-            phone: 'Телефон: +7 (123) 456-7890',
+            title: 'Свяжитесь со мной',
+            name: 'Александра Коневнина',
             email: 'Электронная почта: email@gmail.com',
-            address: 'Адрес: 123 Главная ул., Любой город, РФ',
         },
     },
 } as const
