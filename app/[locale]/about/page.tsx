f1c39cff99fcc0dfd1624d80b79dee45fff8b498
Dmitrii <dmitrii.konevnin@gmail.com>
2026-07-28 15:55:40 +0300

    updated styles in about page

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -13,30 +13,20 @@ export default async function AboutPage() {
     return  (
         <div className={`flex flex-col indents-main-container  flex-1 items-center `}>
             <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
-                <div className="grid grid-cols-2 gap-4 justify-center">
-                                  <AppImage type={IMAGES_UPLOAD_PATH.GLOBAL} imageName={DoctorImage} width={500} height={800} alt="Doctor Image" className='rounded-large  z-100 float-left mr-4' />
-                <div>
-                    <div className={`flex w-full flex-col flex-1  rounded-large`}>
-                        <h2 className={`text-5xl justify-center text-center font-bold`}>{ABOUT_PAGE_CONTENT[locale].title}</h2>
-                        <p className={`mt-2`}>{ABOUT_PAGE_CONTENT[locale].description}</p>
-                    </div>
-                    <div>
-                        <p>
-                            {AboutDoctorContent[locale].DESCRIPTION_2}
-                        </p>
-                    </div>
-                        <div>
-                        <p>
-                            {AboutDoctorContent[locale].DESCRIPTION_3}
-                        </p>
-                    </div>
-                    <div className={`flex w-full flex-col flex-1 mt-6 rounded-large`}>
-                        <h2 className={`text-3xl justify-center text-center font-bold`}>{ABOUT_PAGE_CONTENT[locale].contact.title}</h2>
-                        <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].contact.email}</p>
-                        
-                    </div>
+                <div className={``}>
+                    <AppImage type={IMAGES_UPLOAD_PATH.GLOBAL} imageName={DoctorImage} width={500} height={800} alt="Doctor Image" className='rounded-large  z-100 float-left mr-4' />
+                    
+                        <h2 className={`text-5xl text-center font-bold`}>{ABOUT_PAGE_CONTENT[locale].title}</h2>
+                        <p className={``}>{ABOUT_PAGE_CONTENT[locale].description}</p>
+                        <p>{AboutDoctorContent[locale].DESCRIPTION_2}</p> 
+                        <p>{AboutDoctorContent[locale].DESCRIPTION_3}</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam perferendis asperiores aut iure itaque sapiente omnis incidunt culpa eos architecto voluptatibus, natus eum possimus maxime nobis amet magnam vero nisi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eaque molestias eligendi voluptatum. Culpa quos dolore accusantium, quia vitae illo aliquid officia corporis minus doloremque consectetur mollitia vel eius non?</p>
+                        <div className={`flex w-full flex-col flex-1 mt-6 rounded-large`}>
+                            <h2 className={`text-3xl justify-center text-center font-bold`}>{ABOUT_PAGE_CONTENT[locale].contact.title}</h2>
+                            <p className={`text-center mt-2`}>{ABOUT_PAGE_CONTENT[locale].contact.email}</p>
                 </div>
-                </div>
+                  
+            </div>
   
                 <Image src={LINK_RAW_PATH.butterflyBG} alt="Background Image" fill className="  w-full h-full z-0 object-cover rounded-large opacity-20" />
             </div>
