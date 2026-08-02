886ec06759a701578d5ae5d32460a6fd11b60b7a
Dmitrii <dmitrii.konevnin@gmail.com>
2026-08-02 17:39:31 +0300

    updated consultation form page

--- app/[locale]/consultation/form/page.tsx
+++ app/[locale]/consultation/form/page.tsx
@@ -1,6 +1,13 @@
+import ConsultationForm from "@/app/components/consultation/consultationForm";
 
 export default function ConsultationFormPage() {
   return (
-    <div>page</div>
+    <div className={`flex flex-col indents-main-container  rounded-medium flex-1 items-center`}>
+            <div className={`flex w-full flex-col flex-1 max-w-6xl  rounded-large bg-primary-color/20 shadow-lg backdrop-blur-md border border-primary-color/30 p-4`}>
+                <div>         
+                    <ConsultationForm />
+                </div>
+            </div>
+    </div>
   )
 }
