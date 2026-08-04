f35348aeedd8d30339d6798b3d4efe301b5f064d
Dmitrii <dmitrii.konevnin@gmail.com>
2026-08-04 19:42:24 +0300

    updated test result content

--- translate/testPage/testPage.ts
+++ translate/testPage/testPage.ts
@@ -30,12 +30,27 @@ export const LOGIN_TO_SAVE_RESULT_BUTTON = {
 
 export const TEST_RESULT = {
     en:{
-        title:'Test Result'
+        none:'You currently have no signs of depression, but there are some difficulties that definitely need attention.',
+        mild:'You have mild depression, which on its own causes difficulties and may progress. It is recommended to consult a psychiatrist.',
+        moderate:'You have moderate depression, which is a serious signal for your health. It is recommended to consult a psychiatrist in the near future.',
+        modereteSevere:'You have moderate-severe depression, which is a serious signal for your health. It is recommended to consult a psychiatrist in the near future.',
+        severe:'You have severe depression, such a condition can seriously affect your health. Therefore, it is recommended to consult a psychiatrist as soon as possible.',
+
     },
     lv:{
-        title:'Testa rezultāts'
+        title:'Testa rezultāts',
+        none:'Pašreiz Jums nav depresijas pazīmju, bet ir atsevišķas grūtības, kurām noteikti nepieciešams pievērst uzmanību.',
+        mild:'Jums ir vieglas depresijas pazīmes, kas pašas par sevi sagādā grūtības un var progresēt. Rekomendējams konsultēties pie psihiatra.',
+        moderate:'Jums ir vidējās depresijas pazīmes, kas ir nopietns signāls par veselību. Rekomendējams tuvākajā laikā konsultēties pie psihiatra.',
+        modereteSevere:'Jums ir vidēji smagas depresijas pazīmes, kas ir nopietns signāls par veselību. Rekomendējams tuvākajā laikā konsultēties pie psihiatra.',
+        severe:'Jums ir smagās depresijas pazīmes, šāda pašsajūta var nopietni ietekmēt Jūsu veselību. Sakarā ar ko rekomendējams pēc iespējas ātrāk konsultēties pie psihiatra.',
     },
     ru:{
-        title:'Результат теста'
+        title:'Результат теста',
+        none:'у Вас нет признаков депрессии, но есть отдельные трудности, которым определенно необходимо уделить внимание.',
+        mild:'у Вас легкая депрессия, которая сама по себе создает трудности и может прогрессировать. Рекомендуется проконсультироваться с психиатром.',
+        moderate:'у Вас умеренная депрессия, что является серьезным сигналом для здоровья. Рекомендуется в ближайшее время проконсультироваться с психиатром.',
+        modereteSevere:'у Вас умеренно-тяжелая депрессия, что является серьезным сигналом для здоровья. Рекомендуется в ближайшее время проконсультироваться с психиатром.',
+        severe:'у Вас тяжелая депрессия, такое состояние может серьезно повлиять на Ваше здоровье. В связи с этим рекомендуется как можно скорее проконсультироваться с психиатром.',
     },
 } as const
