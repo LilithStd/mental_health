f027218c493b6215ed90688e86b461c70762b04d
Dmitrii <dmitrii.konevnin@gmail.com>
2026-08-06 21:33:56 +0300

    updated styles on main page - welcome block

--- app/components/mainPageComponents/welcomeBlock.tsx
+++ app/components/mainPageComponents/welcomeBlock.tsx
@@ -51,15 +51,15 @@ export default function WelcomeBlock() {
         //         <Image src={LINK_RAW_PATH.butterflyBG} alt="Background Image" fill className="  w-full h-full z-0 object-cover rounded-large opacity-30" />
         // </div>
            <div className={`grid gap-6 w-full p-4  rounded-large items-center bg-primary-color/10  backdrop-blur-md border border-primary-color/12`}>
-            <div>
-                <h2 className={`text-5xl font-geistSans font-bold`}>{WelcomeBlockContent[localeAdapted].TITLE}</h2>
+            <div className={`flex flex-col gap-4 items-center justify-center`}>
+                <h2 className={`text-5xl text-center font-geistSans font-bold`}>{WelcomeBlockContent[localeAdapted].TITLE}</h2>
                 <p className={`font-geistSans italic  text-xl`}>{WelcomeBlockContent[localeAdapted].DESCRIPTION}</p>
-                <div className={`flex flex-col gap-4 mt-4 `}>
+                <div className={`flex items-center justify-center  gap-4 mt-4 `}>
                     <Link href={routesAdaptive.consultation.root} className={`p-4 w-fit bg-primary-color/40 backdrop-blur-md border border-primary-color/50 font-geistSans shadow-lg z-10 font-bold italic rounded-full hover:bg-accentElement hover:scale-105`}>{WelcomeBlockContent[localeAdapted].SIGN_UP_BUTTON}</Link>
                     <Link href={routesAdaptive.tests.root} className={`p-4 w-fit 
                          bg-primary-color/40 backdrop-blur-md border border-primary-color/50 font-geistSans font-bold italic z-10 shadow-lg rounded-full hover:bg-accentElement hover:scale-105`}>{WelcomeBlockContent[localeAdapted].TESTS_BUTTON}</Link>
                 </div>
-                <div className={`flex flex-col gap-2`}>
+                <div className={`flex justify-center gap-2`}>
                     <ul className={`list-none grid grid-cols-1 md:grid-cols-2 gap-6 mt-6`}>
                         {listPriviliges.map((privilege, index) => (
                             <li key={index} className={`flex items-center gap-2`}>
