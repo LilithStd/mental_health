69743d09490492997e9fd8ee1354ead5b228e0bc
Dmitrii <dmitrii.konevnin@gmail.com>
2026-08-08 20:25:24 +0300

    commited problem solving block (temp)

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -24,9 +24,9 @@ export default function Main() {
       <div className={`flex flex-col w-full  max-w-6xl mt-6 rounded-large shadow-lg backdrop-blur-md `}>
         <AboutDoctorBlock />
       </div>
-      <div className={`flex flex-col w-full  max-w-6xl mt-6 rounded-large  shadow-lg backdrop-blur-md `}>
+      {/* <div className={`flex flex-col w-full  max-w-6xl mt-6 rounded-large  shadow-lg backdrop-blur-md `}>
         <ProblemSolvingBlock />
-      </div>
+      </div> */}
       {/* <div className={`flex flex-col w-full  max-w-6xl mt-6 rounded-large  shadow-lg backdrop-blur-md `}>
         <AnalizUserProblemBlock />
       </div> */}
